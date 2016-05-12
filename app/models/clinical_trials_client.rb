fe9c95293020715ab2718d647663a2d4c89f0bd2
Garrett Martin <me@garrettqmartin.com>
2016-05-12 13:22:04 -0500

    Add #get_studies no-op method and #populate_studies.

--- app/models/clinical_trials_client.rb
+++ app/models/clinical_trials_client.rb
@@ -3,7 +3,25 @@ class ClinicalTrialsClient
 
   attr_reader :url
 
-  def initialize(search_term:)
-    @url = "#{BASE_URL}/search?term=#{search_term.split.join('+')}&resultsxml=true"
+  def initialize(search_term: nil)
+    @url = "#{BASE_URL}/search?term=#{search_term.try(:split).try(:join, '+')}&resultsxml=true"
+  end
+
+  def get_studies
+    
+  end
+
+  def populate_studies(studies)
+    studies.each do |study|
+      nct_id = extract_nct_id(study: study)
+
+      Study.new({ xml: study, nct_id: nct_id }).save
+    end
+  end
+
+  private
+
+  def extract_nct_id(study:)
+    Nokogiri::XML(study).xpath('//nct_id').text
   end
 end
--- spec/models/clinical_trials_client_spec.rb
+++ spec/models/clinical_trials_client_spec.rb
@@ -1,13 +1,43 @@
 require 'rails_helper'
 
 describe ClinicalTrialsClient do
+  let(:client) { ClinicalTrialsClient.new }
+  let(:studies) { [File.read(Rails.root.join('spec', 'support', 'xml_data', 'example_study.xml'))] }
+
   context 'initialization' do
     it 'should set the url based on the provided search term' do
       client = ClinicalTrialsClient.new(search_term: 'pancreatic cancer vaccine')
 
       expect(client.url).to eq('https://clinicaltrials.gov/search?term=pancreatic+cancer+vaccine&resultsxml=true')
     end
+  end
 
+  describe '#get_studies' do
+    # TODO use VCR
+    context 'success' do
+
+      before do
+        allow(client).to receive(:get_studies) { studies }
+      end
+
+      it 'should grab the xml' do
+        expect(client.get_studies).to eq(studies)
+      end
+
+    end
+
+    context 'failure'
+  end
+
+  describe '#populate_studies' do
+    context 'success' do
+      it 'should create a study record' do
+        client.populate_studies(studies)
+
+        expect(Study.last.nct_id).to eq('NCT00002475')
+      end
+    end
 
   end
+
 end
