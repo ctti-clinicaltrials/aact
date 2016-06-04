5769f93ece181b2224add7726fe5ddea07caf452
Garrett Martin <me@garrettqmartin.com>
2016-06-03 21:16:43 -0500

    Fix condition.

--- app/models/clinical_trials/client.rb
+++ app/models/clinical_trials/client.rb
@@ -32,9 +32,9 @@ module ClinicalTrials
         zipfile.each do |file|
           study_xml = file.get_input_stream.read
           nct_id = extract_nct_id_from_study(study_xml)
-          existing_study = Study.find_by(nct_id: nct_id)
+          existing_study_xml = StudyXmlRecord.find_by(nct_id: nct_id)
 
-          if !existing_study
+          if existing_study_xml.blank?
             StudyXmlRecord.create(content: study_xml, nct_id: nct_id)
             # report number of new records
           # elsif study_changed?(existing_study: existing_study, new_study_xml: study)
