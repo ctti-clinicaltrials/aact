6cc3ab84dba9caa3a5b6cc4ce5fe2ff137e993db
Chance Griffin <chance.griffin@gmail.com>
2016-06-22 23:15:38 -0400

    catching error on front end when no load event

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
     @studyCount = Study.count
-    @lastUpdate = ClinicalTrials::LoadEvent.last.updated_at.strftime("%B %d, %Y")
+    @lastUpdate = ClinicalTrials::LoadEvent.last.updated_at
   end
 end
