0b50f45858710f00b647b31ecd3389fcdb1d6749
Garrett Martin <me@garrettqmartin.com>
2016-07-04 10:27:21 -0500

    Remove keyword argument.

--- app/workers/table_export_worker.rb
+++ app/workers/table_export_worker.rb
@@ -2,7 +2,7 @@ class TableExportWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'table_export'
 
-  def perform(delimiter: ',')
+  def perform(delimiter)
     exporter = TableExporter.new
     exporter.run(delimiter: delimiter, should_upload_to_s3: true)
   end
