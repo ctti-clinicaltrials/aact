8584ea1777ad4cf9998972dc630f0e3687ff478a
tibbs001@duke.edu <tibbs001@duke.edu>
2016-07-17 15:56:48 -0400

    Minor change to study_content spec

--- spec/models/study_content_spec.rb
+++ spec/models/study_content_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Study do
-    it "should have expected values" do
-      xml=Nokogiri::XML(File.read('spec/support/xml_data/example_study.xml'))
-      study=Study.new({xml: xml, nct_id: 'NCT02260193'}).create
-      expect(study.first_received_results_disposition_date).to eq('December 1, 1999'.to_date)
+describe Study do
+  it "should have expected date values" do
+    xml=Nokogiri::XML(File.read('spec/support/xml_data/example_study.xml'))
+    study=Study.new({xml: xml, nct_id: 'NCT02260193'}).create
+    expect(study.first_received_results_disposition_date).to eq('December 1, 1999'.to_date)
   end
 end
-
