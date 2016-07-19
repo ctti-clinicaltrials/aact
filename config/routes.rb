c3b472e0c6e728ed94f60ec454a551054d8f0de3
Darin London <darin.london@duke.edu>
2016-07-19 11:41:57 -0400

    swagger is hosted by original /api_docs that the older apipie system used

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   mount AACT2::Base, at: '/'
   mount Sidekiq::Web => '/sidekiq'
-  get "apiexplorer" => "swaggerui#index"
+  get "api_docs" => "swaggerui#index"
   root "pages#home"
 
   get "/snapshot_archive" => "pages#snapshot_archive"
