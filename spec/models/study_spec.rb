2d69d5d1233ce29900efe43f3500815f9ddc1288
Darin London <darin.london@duke.edu>
2016-07-19 13:21:28 -0400

    merge changed the study out from under me. fixed to use the main subject

--- spec/models/study_spec.rb
+++ spec/models/study_spec.rb
@@ -59,14 +59,13 @@ describe Study do
   end
 
   describe 'with_related_records' do
-    subject { study }
     it { is_expected.to respond_to 'with_related_records'}
     it { is_expected.to respond_to 'with_related_records=' }
 
     it 'should allow the with_related_records attribute to be set' do
-      expect(study.with_related_records).not_to be true
-      study.with_related_records = true
-      expect(study.with_related_records).to be true
+      expect(subject.with_related_records).not_to be true
+      subject.with_related_records = true
+      expect(subject.with_related_records).to be true
     end
   end
 end
