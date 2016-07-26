91be063fed0d2b2b09908669f52063bfa844ce93
Chance Griffin <chance.griffin@gmail.com>
2016-07-26 07:57:05 -0400

    content

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get "api_docs" => "swaggerui#index"
   root "pages#home"
 
+  get "/points_to_consider" => "pages#points_to_consider"
   get "/snapshot_archive" => "pages#snapshot_archive"
 end
