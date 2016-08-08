cce568ab5e3f92d8e9344cdd8165d01e4db5f6ea
Chance Griffin <chance.griffin@gmail.com>
2016-08-08 13:59:05 -0500

    basic filtering

--- app/controllers/definitions_controller.rb
+++ app/controllers/definitions_controller.rb
@@ -2,14 +2,6 @@ class DefinitionsController < ApplicationController\
 
   def index
     dictionary = Roo::Spreadsheet.open('./public/dictionary.xlsx')
-    # xlsx = Roo::Excelx.new("./new_prices.xlsx")
-
-    # # Use the extension option if the extension is ambiguous.
-    # xlsx = Roo::Spreadsheet.open('./rails_temp_upload', extension: :xlsx)
-    #
-    # xlsx.info
-
-    # dataResult = dictionary.sheet(0).row(5)
 
     dataResult = []
 
@@ -22,13 +14,18 @@ class DefinitionsController < ApplicationController\
                               'FDAAA Req' => 'FDAAA Required',
                               'Max Length Used' => 'MAX LENGTH UTILIZED',
                               'PRS Label' => 'Variable Label'}) do |hash|
-      puts hash.inspect
-      # => { id: 1, name: 'John Smith' }
       dataResult << hash
 
     end
 
     dataResult.shift
+
+    if params["Table Name"].present?
+      dataResult = dataResult.select do |hash|
+        hash["Table Name"] == params["Table Name"]
+      end
+
+    end
 
     # filtering
 
