7fbc5b9ad201c8e89ca29c90c7d47c769f7cad94
tibbs001@duke.edu <tibbs001@duke.edu>
2016-08-09 20:17:02 -0400

    Have removed table Users, so the number of csv files created by table_exporter should be 48, not 49

--- spec/models/table_exporter_spec.rb
+++ spec/models/table_exporter_spec.rb
@@ -23,7 +23,7 @@ describe TableExporter do
           zipfile.entries.count
         end
 
-        expect(number_of_entries).to eq(49)
+        expect(number_of_entries).to eq(48)
       end
 
       it 'should have content in each csv' do
