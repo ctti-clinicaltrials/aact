c8f419dc032e62d5dd2301d09df50d8f3f922c3e
Chance Griffin <chance.griffin@gmail.com>
2016-08-15 16:59:29 -0400

    connect page reorg

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,15 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
-  get "/learn_more" => "pages#learn_more"
-  get "/connect" => "pages#connect"
-  get "/points_to_consider" => "pages#points_to_consider"
-  get "/download_aact" => "pages#download_aact"
-  get "/snapshot_archive" => "pages#snapshot_archive"
-  get "/sanity_check_report" => "pages#sanity_check", as: :sanity_check
+  get "/learn_more"           => "pages#learn_more"
+  get "/connect"              => "pages#connect"
+  get "/pgadmin"              => "pages#pgadmin"
+  get "/r"                    => "pages#r"
+  get "/sas"                  => "pages#sas"
+  get "/points_to_consider"   => "pages#points_to_consider"
+  get "/download_aact"        => "pages#download_aact"
+  get "/snapshot_archive"     => "pages#snapshot_archive"
+  get "/sanity_check_report"  => "pages#sanity_check", as: :sanity_check
 
   resources :definitions
 end
