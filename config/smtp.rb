9a15410fe1b7a5388aeff3db840e15784622a0aa
Garrett Martin <me@garrettqmartin.com>
2016-08-17 14:05:58 -0500

    Add option to use Mailgun addon keys.

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,9 +1,9 @@
 SMTP_SETTINGS = {
-  address: ENV.fetch("SMTP_ADDRESS"), # example: "smtp.sendgrid.net"
+  address: ENV["SMTP_ADDRESS"] || ENV["MAILGUN_SMTP_SERVER"], # example: "smtp.sendgrid.net"
   authentication: :plain,
-  domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
+  domain: ENV["SMTP_DOMAIN"] || ENV["MAILGUN_DOMAIN"], # example: "heroku.com"
   enable_starttls_auto: true,
-  password: ENV.fetch("SMTP_PASSWORD"),
+  password: ENV["SMTP_PASSWORD"] || ENV["MAILGUN_SMTP_PASSWORD"],
   port: "587",
-  user_name: ENV.fetch("SMTP_USERNAME")
+  user_name: ENV["SMTP_USERNAME"] || ENV["MAILGUN_SMTP_LOGIN"]
 }
