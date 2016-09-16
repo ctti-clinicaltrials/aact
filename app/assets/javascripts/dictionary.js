2c9bbe7d9d0d0a898f3af549fdf6498b47252ee2
tibbs001@duke.edu <tibbs001@duke.edu>
2016-09-15 22:23:08 -0400

    AACT-201:  Tweak data dictionary functionality.  Change appearance of the table and get the filtering feature basically working.

--- app/assets/javascripts/dictionary.js
+++ app/assets/javascripts/dictionary.js
@@ -7,9 +7,11 @@ $(function() {
         inserting: false,
         editing: false,
         sorting: true,
-        paging: false,
+        paging: true,
         autoload: true,
         noDataContent: "...Loading...",
+        searchButtonTooltip: "Search",
+        clearFilterButtonTooltip: "Clear filter",
         // pageSize: 10,
         // pageButtonCount: 5,
         controller: {
@@ -23,18 +25,19 @@ $(function() {
         },
         fields: [
             { type: 'control', deleteButton: false, editButton: false },
-            { name: 'nlm documentation', width: 22, align: "center" },
+            { name: 'nlm documentation', width: 22, align: 'center' },
+//            { name: "db section", type: "select", width: 75, items: ['','Results','Protocol'] },
             { name: 'db section',         type: "text", width: 75 },
             { name: 'table',              type: "text", width: 180 },
             { name: 'column',             type: "text" },
             { name: 'data type',          type: "text", width: 70 },
             { name: 'xml source',         type: "text", width: 260 },
             { name: 'AACT contribution',  type: "text" },
-            { name: 'CTTI Note',          type: "text", width: 260 },
+            { name: 'CTTI Note',          type: "textarea", width: 260 },
             { name: 'AACT1 Variable',     type: "text" },
             { name: 'PRS Label',          type: "text" },
-            { name: 'nlm required',       type: "text" },
-            { name: 'fdaaa required',     type: "text" }
+            { name: 'nlm required',       type: "checkbox" },
+            { name: 'fdaaa required',     type: "checkbox" }
         ]
     });
 });
