6b61ccae0cade42e25175ddd8317570bc252308e
tibbs001@duke.edu <tibbs001@duke.edu>
2016-09-30 11:27:41 -0400

    AACT-219:  Modify the way we retrieve study xml records that haven't yet been loaded

--- app/models/study_xml_record.rb
+++ app/models/study_xml_record.rb
@@ -2,7 +2,7 @@ class StudyXmlRecord < ActiveRecord::Base
   belongs_to :study, foreign_key: "nct_id"
 
   def self.not_yet_loaded
-     where(:created_study_at=>nil)
+    where('created_study_at is null')
   end
 
   def was_created
