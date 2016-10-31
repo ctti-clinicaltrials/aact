017d8976f7da8af0ea4c410a3f781634d6d958e7
tibbs001@duke.edu <tibbs001@duke.edu>
2016-10-31 11:55:42 -0400

    aact-237:  add indexes for API queries

---
+++ db/migrate/20161030000000_create_indices.rb
@@ -0,0 +1,23 @@
+class CreateIndices < ActiveRecord::Migration
+
+  def change
+
+    add_index :browse_conditions, :nct_id
+    add_index :browse_interventions, :nct_id
+    add_index :facilities, :nct_id
+    add_index :overall_officials, :nct_id
+    add_index :responsible_parties, :nct_id
+    add_index :studies, :nct_id
+    add_index :study_xml_records, :nct_id
+
+    add_index :browse_conditions, :mesh_term
+    add_index :browse_interventions, :mesh_term
+    add_index :facilities, :name
+    add_index :overall_officials, :affiliation
+    add_index :responsible_parties, :organization
+    add_index :studies, :source
+    add_index :study_xml_records, :created_study_at
+
+  end
+
+end
