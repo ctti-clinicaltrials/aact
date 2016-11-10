b08b05512c5d052d7e080e6a933b66f0e748205a
tibbs001@duke.edu <tibbs001@duke.edu>
2016-11-10 14:14:51 -0500

    aact-244:  FastCount is returning incorrect data.  Revert to doing regular database table counts

--- app/models/outcome_analysis_group.rb
+++ app/models/outcome_analysis_group.rb
@@ -1,5 +1,4 @@
 class OutcomeAnalysisGroup < StudyRelationship
-  extend FastCount
   belongs_to :outcome_analysis, inverse_of: :outcome_analysis_groups, autosave: true
   belongs_to :result_group,     inverse_of: :outcome_analysis_groups, autosave: true
 
