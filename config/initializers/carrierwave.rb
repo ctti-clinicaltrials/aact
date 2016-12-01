e0b0dcbac30afda22a16bffdbfc6fc14edf2e2df
tibbs001 <sheri.tibbs@gmail.com>
2016-11-30 22:41:48 -0500

    AACT-236:  use case image attachment works locally.  Trying to get it to work when pushed to heroku by storing files on AWS S3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
+    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]
+  }
+  config.fog_directory  = ENV["S3_BUCKET_NAME"]
+end
