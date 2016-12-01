71b94d1d3ee4bc3b1eb2ce9292b889accc6fde1e
tibbs001 <sheri.tibbs@gmail.com>
2016-12-01 09:07:15 -0500

    aact-236:  trying to get imagemagick/carrierwave to deploy to heroku.  Maybe cuz the file was carrierwave.rb instead of carrier_wave.rb?

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,10 +5,10 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
     :s3_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
-#    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
-#    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
   }
-  config.fog_directory  = ENV["S3_BUCKET_NAME"]
+  config.fog_directory  = ENV['S3_BUCKET_NAME']
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
