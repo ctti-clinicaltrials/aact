388a8ea09516c01e93ed3a77b81175a670656e87
tibbs001 <sheri.tibbs@gmail.com>
2017-01-09 10:32:54 -0500

    Include load status in notification subject line

--- app/mailers/load_mailer.rb
+++ app/mailers/load_mailer.rb
@@ -1,19 +1,14 @@
 class LoadMailer < ApplicationMailer
+
   def self.send_notifications(load_event)
     emails = ['sheri.tibbs@duke.edu']
-
     emails.each do |email|
       send_notification(email, load_event).deliver_now
     end
   end
 
   def send_notification(email, load_event)
-    puts "==============================="
-    puts @load_event.inspect
-    puts "==============================="
-    @load_event = load_event
-
-    subject_line="AACT #{ENV['RAILS_ENV']} load completed with #{@load_event.errors.size} errors"
-    mail(from: '<ctti.dcri@gmail.com>', to: email, subject: subject_line, body: @load_event.description)
+    subject_line="AACT #{ENV['RAILS_ENV']} load status: #{load_event.status}. Added: #{load_event.new_studies} Changed: #{load_event.changed_studies}"
+    mail(from: '<ctti.dcri@gmail.com>', to: email, subject: subject_line, body: load_event.description)
   end
 end
