6960e0c4f43bf7cec63d9bcd4ad1e0123814d321
tibbs001 <sheri.tibbs@gmail.com>
2017-01-18 16:09:50 -0500

    aact-299:  refined the list of enumerators. (these appear in the data dictionary on the website)

--- app/models/data_definition.rb
+++ app/models/data_definition.rb
@@ -9,15 +9,14 @@ class DataDefinition < ActiveRecord::Base
     puts "about to populate data definitions table..."
     (2..data.last_row).each do |i|
       row = Hash[[header, data.row(i)].transpose]
-      puts row
       if !row['table'].nil? and !row['column'].nil?
         new(:db_section=>row['db section'].downcase,
             :table_name=>row['table'].downcase,
             :column_name=>row['column'].downcase,
             :data_type=>row['data type'].downcase,
             :source=>row['source'].downcase,
-           :ctti_note=>row['CTTI note'],
-           :nlm_link=>row['nlm doc'],
+            :ctti_note=>row['CTTI note'],
+            :nlm_link=>row['nlm doc'],
            ).save!
       end
     end
@@ -66,11 +65,8 @@ class DataDefinition < ActiveRecord::Base
 
   def self.enums
     [
-      ['studies','study_type'],
-      ['studies','overall_status'],
-      ['studies','last_known_status'],
-      ['studies','phase'],
-      ['studies','enrollment_type'],
+      ['baseline_measurements','category'],
+      ['baseline_measurements','param_type'],
       ['calculated_values','sponsor_type'],
       ['central_contacts','contact_type'],
       ['design_groups','group_type'],
@@ -78,17 +74,43 @@ class DataDefinition < ActiveRecord::Base
       ['designs','observational_model'],
       ['designs','masking'],
       ['eligibilities','gender'],
+      ['eligibilities','gender_based'],
+      ['eligibilities','sampling_method'],
+      ['facilities','status'],
+      ['facility_investigators','role'],
       ['facility_contacts','contact_type'],
       ['id_information','id_type'],
       ['interventions','intervention_type'],
       ['responsible_parties','responsible_party_type'],
-      ['sponsors','agency_class'],
-      ['study_references','reference_type'],
-      ['result_groups','result_type'],
-      ['baseline_measurements','category'],
-      ['baseline_measurements','param_type'],
-      ['baseline_measurements','dispersion_type'],
+      ['outcome_analyses','ci_n_sides'],
+      ['outcome_analyses','non_inferiority_type'],
+      ['outcome_measurements','param_type'],
+      ['reported_events','assessment'],
+      ['reported_events','default_assessment'],
       ['reported_events','event_type'],
+      ['result_agreements','pi_employee'],
+      ['result_groups','result_type'],
+      ['sponsors','agency_class'],
+      ['sponsors','lead_or_collaborator'],
+      ['studies','biospec_retention'],
+      ['studies','completion_date_type'],
+      ['studies','enrollment_type'],
+      ['studies','expanded_access_type_individual'],
+      ['studies','expanded_access_type_intermediate'],
+      ['studies','expanded_access_type_treatment'],
+      ['studies','has_expanded_access'],
+      ['studies','has_dmc'],
+      ['studies','is_fda_regulated_device'],
+      ['studies','is_fda_regulated_drug'],
+      ['studies','is_ppsd'],
+      ['studies','is_unapproved_device'],
+      ['studies','is_us_export'],
+      ['studies','last_known_status'],
+      ['studies','overall_status'],
+      ['studies','phase'],
+      ['studies','start_date_type'],
+      ['studies','study_type'],
+      ['study_references','reference_type'],
     ]
   end
 
