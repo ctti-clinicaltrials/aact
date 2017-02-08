6af38069c07c94ecdfa88d86a1a0a0ddc54cd8a1
tibbs001 <sheri.tibbs@gmail.com>
2017-02-08 11:58:04 -0500

    aact-284:  use Proximo Heroku plug-in instead of QuotaGuard.  Proximo provides static IP for out-going requests, which is what we need to interact with AWS elasticsearch  (Seems QuotaGuard is for in-bound traffic)

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,12 +1,11 @@
 Rails.application.config.action_dispatch.cookies_serializer = :json
 config = {
   host: ENV['ELASTICSEARCH_URL'],
-  credentials: Aws::Credentials.new(ENV['AWS_ELASTICSEARCH_KEY'], ENV['AWS_ELASTICSEARCH_SECRET']),
-  url: ENV['QUOTAGUARDSTATIC_URL']
+  url: ENV['PROXIMO_URL']
 }
 
-if File.exists?("config/elasticsearch.yml")
-  config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].symbolize_keys)
-end
+#if File.exists?("config/elasticsearch.yml")
+#  config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].symbolize_keys)
+#end
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
