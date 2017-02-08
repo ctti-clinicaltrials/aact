bdb8b4e6fe5c48b3be74396f40cbc2b385181f98
tibbs001 <sheri.tibbs@gmail.com>
2017-02-08 16:07:41 -0500

    aact-284:  Configure elasticsearch to authenticate using AWS user credentials.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,11 +1,13 @@
+require 'faraday_middleware/aws_signers_v4'
 Rails.application.config.action_dispatch.cookies_serializer = :json
-config = {
-  host: ENV['ELASTICSEARCH_URL'],
-  url: ENV['PROXIMO_URL']
-}
 
-#if File.exists?("config/elasticsearch.yml")
-#  config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].symbolize_keys)
-#end
-
-Elasticsearch::Model.client = Elasticsearch::Client.new(config)
+credentials=Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+Elasticsearch::Model.client = Elasticsearch::Client.new(host: ENV['ELASTICSEARCH_URL']) do |f|
+  f.request :aws_signers_v4,
+    credentials: credentials,
+    service_name: 'es',
+    host:  ENV['ELASTICSEARCH_URL'],
+    region: ENV['AWS_REGION']
+  f.response :logger
+  f.adapter  Faraday.default_adapter
+end
