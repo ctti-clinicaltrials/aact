4e9d35fcfd38e61edd1f1e2e4b7ef2375cf53cad
tibbs001 <sheri.tibbs@gmail.com>
2017-02-16 14:56:04 -0500

    aact-236:  add use case validations and improve appearance of input screen

--- app/models/use_case.rb
+++ app/models/use_case.rb
@@ -1,6 +1,7 @@
 class UseCase < AdminBase
   has_many :use_case_attachments, :dependent => :destroy
   mount_uploader :image, ImageUploader
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
   def attachment
     attachments.first
