632afbb347919f3fcce4fbf657705a61150ba219
tibbs001 <sheri.tibbs@gmail.com>
2017-02-16 12:17:49 -0500

    fix bugs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,23 +8,24 @@ unless ENV['AWS_ACCESS_KEY_ID'].blank?
       :provider               => 'AWS',
       :region                 => ENV['AWS_REGION'],
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :endpoint               => ENV['FILESERVER_ENDPOINT']
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']
     config.fog_public     = false                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-    config.fog_host         = "#{ENV['FILESERVER_ENDPOINT']}/#{ENV['S3_BUCKET_NAME']}"
-  end
-end
 
-  # For testing, upload files to local `tmp` folder.
-  if Rails.env.test?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
+    # For testing, upload files to local `tmp` folder.
+    if Rails.env.test?
+      config.storage = :file
+      config.enable_processing = false
+      config.root = "#{Rails.root}/tmp"
+    else
+      config.storage = :fog
+    end
   end
+
+end
 
 module Carrierwave
   module MiniMagick
