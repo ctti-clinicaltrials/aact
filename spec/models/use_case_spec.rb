bd8d87dc27df3272d64f9ccec32c01a9c129a1fe
tibbs001 <sheri.tibbs@gmail.com>
2017-02-21 14:36:13 -0500

    aact-236:  test use case attachment

--- spec/models/use_case_spec.rb
+++ spec/models/use_case_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UseCase, type: :model do
 
+  before :each do
+    @file = Rack::Test::UploadedFile.new('spec/support/xml_data/example_study.xml', 'text/xml')
+  end
+
   it "should create use_case with multiple attachments" do
     status='proposed'
     title='use case title'
@@ -9,15 +13,14 @@ RSpec.describe UseCase, type: :model do
     url='www.duke.edu'
     email='joe.smith@duke.edu'
     expect(UseCase.count).to eq(0)
-    uc=UseCase.create({:status=>status,:title=>title,:brief_summary=>bs,:url=>url})
+    uc=UseCase.create({:status=>status,:title=>title,:brief_summary=>bs,:url=>url,:file=>@file})
     expect(UseCase.count).to eq(0)
     uc.email=email
     uc.save!
     expect(UseCase.count).to eq(1)
-    data = File.read("spec/support/xml_data/example_study.xml")
-#    a=UseCaseAttachment.create({:use_case=>uc, :file_name=>'test.xml', :payload=>data})
-#    expect(uc.attachments.size).to eq(1)
-#    expect(a.file_name).to eq('test.xml')
+    expect(uc.attachments.size).to eq(1)
+    expect(uc.attachment.file_name).to eq('example_study.xml')
+    expect(uc.attachment.content_type).to eq('text/xml')
   end
 
 end
