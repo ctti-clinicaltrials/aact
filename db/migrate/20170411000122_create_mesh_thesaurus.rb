1d9709503bd80adc48de55ba5d10a33719871a5f
tibbs001 <sheri.tibbs@gmail.com>
2017-04-11 09:06:28 -0400

    aact-348: add table of MeSH Headings provided by NLM from this site: https://www.nlm.nih.gov/mesh/2017/download/2017NewHeadingsByCategorySingleColumn508.txt

--- db/migrate/20170411000122_create_mesh_thesaurus.rb
+++ db/migrate/20170411000122_create_mesh_thesaurus.rb
@@ -1,10 +1,18 @@
 class CreateMeshThesaurus < ActiveRecord::Migration
   def change
+
     create_table :mesh_terms do |t|
       t.string  'qualifier'
       t.string  'tree_number'
       t.string  'description'
       t.string  'mesh_term'
     end
+
+    create_table :mesh_headings do |t|
+      t.string  'qualifier'
+      t.string  'heading'
+      t.string  'subcategory'
+    end
+
   end
 end
