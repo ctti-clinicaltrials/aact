cc432a713b67f4e5231883f8ac7f5c9311ec27b9
tibbs001 <sheri.tibbs@gmail.com>
2017-06-28 12:50:28 -0400

    aact-348: Start investigating ways to group studies by clinical domain.  Add a simple method to retrieve NCT IDs for studies that have a browse_condition that matches on a term

--- app/models/mesh_term.rb
+++ app/models/mesh_term.rb
@@ -16,4 +16,15 @@ class MeshTerm < ActiveRecord::Base
       end
     }
   end
+
+  def self.ids_possibly_related_to_condition(term)
+    ids=[]
+    searchable_term="%#{term}%"
+    terms=MeshTerm.where('mesh_term like ?',searchable_term).pluck("mesh_term").uniq
+    terms.each{|t| puts t}
+    terms.each{|t|
+      ids << BrowseCondition.where('mesh_term = ?',t).pluck(:nct_id).uniq if BrowseCondition.where('mesh_term = ?',t).count != 0
+    }
+    ids.flatten
+  end
 end
--- spec/models/mesh_term_spec.rb
+++ spec/models/mesh_term_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 describe MeshTerm do
-  it "should accurately load mesh terms from a file" do
+  before do
     MeshTerm.populate_from_file(Rails.root.join('spec', 'support', 'flat_data', 'mesh_terms.txt'))
+  end
+
+  it "should have accurately loaded mesh terms from a file." do
     expect(MeshTerm.count).to eq(82)
     term=MeshTerm.where('description = ?','D000071422').first
     expect(term.mesh_term).to eq('Inbreeding Depression')
   end
 
+  it "Searching for studies about 'Depression' should find study about 'Inbreeding Depression'." do
+    nct_id='NCT02586688'
+    xml=Nokogiri::XML(File.read("spec/support/xml_data/#{nct_id}.xml"))
+    study=Study.new({xml: xml, nct_id: nct_id}).create
+    expect(Study.all.size).to eq(1)
+    ids=MeshTerm.ids_possibly_related_to_condition('Depression')
+    expect(ids.size).to eq(1)
+  end
 end
 
