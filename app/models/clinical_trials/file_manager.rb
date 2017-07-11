0b49a927fc5ed468f14be18b8a00e1011dcec4c9
tibbs001 <sheri.tibbs@gmail.com>
2017-07-11 15:38:33 -0400

    aact-368:  There are 2 ways to access static files:  1) via a url which uses /static and 2) via ‘backend’ processes that use the actual location on the server: /var/local/share.  Update FileManager accordingly

--- app/models/clinical_trials/file_manager.rb
+++ app/models/clinical_trials/file_manager.rb
@@ -12,32 +12,36 @@ module ClinicalTrials
       "https://prsinfo.clinicaltrials.gov/results_definitions.html"
     end
 
-    def self.file_server
+    def self.static_url_base
       '/static'
     end
 
-    def file_server
-      '/static'
+    def self.backend_file_server
+      '/var/local/share'
+    end
+
+    def backend_file_server
+      '/var/local/share'
     end
 
     def self.dump_directory
-      "#{file_server}/tmp"
+      "#{backend_file_server}/tmp"
     end
 
     def dump_directory
-      "#{file_server}/tmp"
+      "#{backend_file_server}/tmp"
     end
 
     def self.pg_dump_file
-      "#{file_server}/tmp/postgres.dmp"
+      "#{backend_file_server}/tmp/postgres.dmp"
     end
 
     def self.static_copies_directory
-      "#{file_server}/static_db_copies"
+      "#{backend_file_server}/static_db_copies"
     end
 
     def self.flat_files_directory
-      "#{file_server}/exported_files"
+      "#{backend_file_server}/exported_files"
     end
 
     def self.snapshot_files
@@ -49,7 +53,7 @@ module ClinicalTrials
     end
 
     def self.documentation_directory
-      "#{file_server}/documentation"
+      "#{static_url_base}/documentation"
     end
 
     def self.admin_schema_diagram
@@ -65,7 +69,7 @@ module ClinicalTrials
     end
 
     def self.table_dictionary
-      "#{self.documentation_directory}/aact_tables.xlsx"
+      "/var/local/share/documentation/aact_tables.xlsx"
     end
 
     def self.default_data_definitions
@@ -82,11 +86,12 @@ module ClinicalTrials
 
     def self.files_in(sub_dir)
       entries=[]
-      dir="#{self.file_server}/#{sub_dir}"
+      dir="/var/local/share/#{sub_dir}"
       file_names=Dir.entries(dir) - ['.','..']
       file_names.each {|file_name|
-        file_url="#{dir}/#{file_name}"
-        size=File.open(file_url).size
+        file_location="#{dir}/#{file_name}"
+        file_url="#{static_url_base}/#{sub_dir}/#{file_name}"
+        size=File.open(file_location).size
         date_string=file_name.split('_').first
         date_created=(date_string.size==8 ? Date.parse(date_string).strftime("%m/%d/%Y") : date_string)
         entries << {:name=>file_name,:date_created=>date_created,:size=>number_to_human_size(size), :url=>file_url}
@@ -115,12 +120,12 @@ module ClinicalTrials
       dump_file_name=self.class.pg_dump_file
       File.delete(dump_file_name) if File.exist?(dump_file_name)
 
-      `PGPASSWORD=$RDS_DB_SUPER_PASSWORD pg_dump -h $RDS_DB_HOSTNAME -p 5432 -U $RDS_DB_SUPER_USERNAME --no-password --clean --exclude-table schema_migrations  -c -C -Fc -f  /var/www/aact/shared/tmp/postgres.dmp aact`
+      `PGPASSWORD=$RDS_DB_SUPER_PASSWORD pg_dump -h $RDS_DB_HOSTNAME -p 5432 -U $RDS_DB_SUPER_USERNAME --no-password --clean --exclude-table schema_migrations  -c -C -Fc -f  /var/local/share/tmp/postgres.dmp aact`
       return dump_file_name
     end
 
     def make_file_from_website(fname,url)
-      return_file="#{file_server}/tmp/#{fname}"
+      return_file="#{backend_file_server}/tmp/#{fname}"
       open(url) {|site|
         open(return_file, "wb"){|out_file|
             d=site.read
@@ -133,18 +138,18 @@ module ClinicalTrials
 
     def take_snapshot
       dump_database
-      schema_diagram_file=File.open("#{self.class.documentation_directory}/aact_schema.png")
-      admin_schema_diagram_file=File.open("#{self.class.documentation_directory}/aact_admin_schema.png")
-      data_dictionary_file=File.open("#{self.class.documentation_directory}/aact_data_definitions.xlsx")
+      schema_diagram_file=File.open("#{self.backend_file_server}/documentation/aact_schema.png")
+      admin_schema_diagram_file=File.open("#{self.backend_file_server}/documentation/aact_admin_schema.png")
+      data_dictionary_file=File.open("#{self.backend_file_server}/documentation/aact_data_definitions.xlsx")
       nlm_protocol_file=make_file_from_website('nlm_protocol_definitions.html',self.class.nlm_protocol_data_url)
       nlm_results_file=make_file_from_website('nlm_results_definitions.html',self.class.nlm_results_data_url)
 
       zip_file_name="#{self.class.static_copies_directory}/#{Time.now.strftime('%Y%m%d')}_clinical_trials.zip"
       File.delete(zip_file_name) if File.exist?(zip_file_name)
       Zip::File.open(zip_file_name, Zip::File::CREATE) {|zipfile|
-        zipfile.add('data_dictionary.xlsx',data_dictionary_file)
         zipfile.add('schema_diagram.png',schema_diagram_file)
         zipfile.add('admin_schema_diagram.png',admin_schema_diagram_file)
+        zipfile.add('data_dictionary.xlsx',data_dictionary_file)
         zipfile.add('postgres_data.dmp',self.class.pg_dump_file)
         zipfile.add('nlm_protocol_definitions.html',nlm_protocol_file)
         zipfile.add('nlm_results_definitions.html',nlm_results_file)
