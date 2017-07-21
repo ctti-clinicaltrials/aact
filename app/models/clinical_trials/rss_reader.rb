82ad711bb5d20cbd9ccbbbbe38f0ad1087db65f9
tibbs001 <sheri.tibbs@gmail.com>
2017-07-21 11:59:01 -0400

    aact-370: Use the 2 rss commands provided on the ct.gov website.

--- app/models/clinical_trials/rss_reader.rb
+++ app/models/clinical_trials/rss_reader.rb
@@ -9,8 +9,8 @@ module ClinicalTrials
     attr_reader :changed_url, :added_url
 
     def initialize(days_back: 1)
-      @changed_url = "#{BASE_URL}/ct2/results/rss.xml?lup_d=#{days_back}&recrs=a&count=10000"
-      @added_url   = "#{BASE_URL}/ct2/results/rss.xml?rcv_d=#{days_back}&show_rss=Y&count=10000"
+      @changed_url = "#{BASE_URL}/ct2/results/rss.xml?rcv_d=&lup_d=#{days_back}&sel_rss=mod#{days_back}&recrs=a&count=10000"
+      @added_url   = "#{BASE_URL}/ct2/results/rss.xml?rcv_d=#{days_back}&lup_d=&sel_rss=new#{days_back}&recrs=a&count=10000"
     end
 
     def get_changed_nct_ids(rss: @changed_url)
