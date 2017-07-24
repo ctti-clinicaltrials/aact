105f3be486e8b788fe668b0928413c5d9e129731
tibbs001 <sheri.tibbs@gmail.com>
2017-07-24 11:59:36 -0400

    aact-370:  If call to clinicaltrials.gov times out, try 5 times before giving up.

--- app/models/clinical_trials/rss_reader.rb
+++ app/models/clinical_trials/rss_reader.rb
@@ -13,14 +13,40 @@ module ClinicalTrials
       @added_url   = "#{BASE_URL}/ct2/results/rss.xml?rcv_d=#{days_back}&count=10000"
     end
 
-    def get_changed_nct_ids(rss: @changed_url)
-      feed = RSS::Parser.parse(rss, false)
-      feed.items.map(&:guid).map(&:content)
+    def get_changed_nct_ids
+      tries ||= 5
+      begin
+        feed = RSS::Parser.parse(@changed_url, false)
+        feed.items.map(&:guid).map(&:content)
+      rescue  Exception => e
+        if (tries -=1) > 0
+          puts "Failed: #{@added_url}.  trying again..."
+          puts "Error: #{e}"
+          retry
+        else #give up & return empty array
+          []
+        end
+      end
     end
 
-    def get_added_nct_ids(rss: @added_url)
-      feed = RSS::Parser.parse(rss, false)
-      feed.items.map(&:guid).map(&:content)
+    def get_added_nct_ids
+      tries ||= 5
+      begin
+        feed = RSS::Parser.parse(@added_url, false)
+        feed.items.map(&:guid).map(&:content)
+      rescue  Exception => e
+        if (tries -=1) > 0
+          puts "Failed: #{@added_url}.  trying again..."
+          puts "Error: #{e}"
+          retry
+        else #give up & return empty array
+          []
+        end
+      end
+    end
+
+    def set_changed_url(cmd)
+      @changed_url=cmd
     end
   end
 end
