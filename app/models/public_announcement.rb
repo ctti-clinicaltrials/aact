68dd8516966ef411ad838f868df9a8e5eb73a85a
tibbs001 <sheri.tibbs@gmail.com>
2017-07-27 14:40:39 -0400

    fix bug in PublicAnnouncement

--- app/models/public_announcement.rb
+++ app/models/public_announcement.rb
@@ -2,8 +2,7 @@ class PublicAnnouncement < AdminBase
 
   def self.populate(string)
     self.destroy_all
-    pa=new(:description=>string).save!
-    pa.save!
+    new(:description=>string).save!
   end
 
 end
