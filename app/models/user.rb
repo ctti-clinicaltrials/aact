dde597c7ed0b38211e0bc082ef0436e835495ffe
tibbs001 <sheri.tibbs@gmail.com>
2017-09-01 14:26:10 -0400

    Add methods to add and remove user that include adding/removing their database account

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,26 @@ class User < AdminBase
     false
   end
 
+  def add
+    con=ActiveRecord::Base.establish_connection(:public).connection
+    con.execute("create user #{db_username}")
+    con.execute("grant connect on database aact to #{db_username}")
+    con.execute("grant usage on schema public TO #{db_username}")
+    con.execute("grant select on all tables in schema public to #{db_username};")
+  end
+
+  def remove
+    con=ActiveRecord::Base.establish_connection(:public).connection
+    con.execute("drop owned by #{db_username};")
+    con.execute("revoke all on schema public from #{db_username};")
+    con.execute("drop user #{db_username};")
+    destroy
+  end
+
+  def self.remove(email)
+    where('email=?',email).first.try(:remove)
+  end
+
   def full_name
     first_name + ' ' + last_name
   end
