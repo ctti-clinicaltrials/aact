8130fa5dc5320db00596f981944658bbfd6a2e56
tibbs001 <sheri.tibbs@gmail.com>
2017-09-08 15:20:42 -0400

    aact-398:  fix typos in expected strings

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -7,11 +7,11 @@ describe Notifier, type: :mailer do
     let(:msg) { described_class.send_msg('test@gmail.com',event) }
 
     it 'renders the subject' do
-      expect(msg.subject).to eq('AACT Test  Load Notification. Nothing to load.')
+      expect(msg.subject).to eq('AACT Test Load Notification. Nothing to load.')
     end
 
     it 'sends to the admin email' do
-      expect(msg.to).to eq('test@duke.edu')
+      expect(msg.to.first).to eq('test@gmail.com')
     end
   end
 end
