6bb7a257e7b635204a4f290ed83fd1c91f9d2911
tibbs001 <sheri.tibbs@gmail.com>
2017-09-22 15:24:14 -0400

    aact-430:  SQL command needs to end with ;.  Error message contains the string we test for to see if error occurred, but isn’t the whole message.  Also, be consistent with using the word ‘create’ rather than ‘add’

--- app/models/util/db_manager.rb
+++ app/models/util/db_manager.rb
@@ -3,12 +3,12 @@ module Util
 
     attr_accessor :con
 
-    def self.add_user(user)
-      new.add_user(user)
+    def self.create_user(user)
+      new.create_user(user)
     end
 
-    def self.add_unconfirmed_user(user)
-      new.add_unconfirmed_user(user)
+    def self.create_unconfirmed_user(user)
+      new.create_unconfirmed_user(user)
     end
 
     def self.remove_user(user)
@@ -27,19 +27,19 @@ module Util
       new.revoke_db_privs
     end
 
-    def self.can_add_user?(user)
-      new.can_add_user?(user)
+    def self.can_create_user?(user)
+      new.can_create_user?(user)
     end
 
     # =============== instance methods
 
-    def add_unconfirmed_user(user)
+    def create_unconfirmed_user(user)
       # We add the unconfirmed user to the db to reserve the username - prevent others from
       # subsequently trying to create user with same name before user confirms the account
       # When user eventually confirms the account, we will set their password to what they defined
       begin
         dummy_pwd=ENV['UNCONFIRMED_USER_PASSWORD']
-        con.execute("create user \"#{user.username}\" password '#{dummy_pwd}';")
+        con.execute("create user \"#{user.username}\" password \"#{dummy_pwd}\";")
         con.execute("grant connect on database aact to \"#{user.username}\";")
         con.execute("grant usage on schema public TO \"#{user.username}\";")
         con.execute("grant select on all tables in schema public to \"#{user.username}\";")
@@ -48,7 +48,7 @@ module Util
       end
     end
 
-    def can_add_user?(user)
+    def can_create_user?(user)
       !user_account_exists?(user)
     end
 
@@ -56,7 +56,7 @@ module Util
       res=con.execute("SELECT * FROM pg_catalog.pg_user where usename = '#{user.username}'").count > 0
     end
 
-    def add_user(user)
+    def create_user(user)
       begin
         con.execute("create user \"#{user.username}\" password '#{user.unencrypted_password}'")
         con.execute("grant connect on database aact to \"#{user.username}\"")
@@ -73,13 +73,13 @@ module Util
         con.execute("revoke all on schema public from \"#{user.username}\";")
         con.execute("drop user \"#{user.username}\";")
       rescue => e
-        raise e unless e.message == "role \"#{user.username}\" does not exist"
+        raise e unless e.message.include? "role \"#{user.username}\" does not exist"
       end
     end
 
     def change_password(user,pwd)
       begin
-        con.execute("alter user \"#{user.username}\" password \"#{pwd}\"")
+        con.execute("alter user \"#{user.username}\" password \"#{pwd}\";")
       rescue => e
         user.errors.add(:base, e.message)
       end
