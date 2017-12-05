675b064d6d0c70411d1f4cce26674714100e48e8
tibbs001 <sheri.tibbs@duke.edu>
2017-12-05 15:29:04 -0500

    aact-492:  point production to the appropriate DigitalOcean server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+ server "192.241.161.127", user: "ctti", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
@@ -41,21 +42,20 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
+  set :ssh_options, {
+    keys: %w(/home/ctti/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(publickey)
+   }
+
 # The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+ server "192.241.161.127",
+   user: "ctti",
+   roles: %w{web db app},
+   ssh_options: {
+     user: "ctti", # overrides user setting above
+     keys: %w(/home/ctti/.ssh/id_rsa),
+     forward_agent: false,
+     auth_methods: %w(publickey)
+   }
+
