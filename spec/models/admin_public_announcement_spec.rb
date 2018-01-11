a6037d41dd4dd4ad1eab6df5725215176b1410ce
tibbs001 <sheri.tibbs@duke.edu>
2018-01-11 14:52:40 -0500

    aact-526:  Fix PublicAnnouncement tests

--- spec/models/admin_public_announcement_spec.rb
+++ spec/models/admin_public_announcement_spec.rb
@@ -6,23 +6,23 @@ describe Admin::PublicAnnouncement do
     pa_text='This is a loading announcement'
     pa_lt_text='This is a long term announcement'
 
-    subject.populate(pa_text)
-    PublicAnnouncement.populate_long_term(pa_lt_text)
-    expect(PublicAnnouncement.count).to eq(2)
-    pa=PublicAnnouncement.where('description=?',pa_text)
-    lt_pa=PublicAnnouncement.where('description=?',pa_lt_text)
+    Admin::PublicAnnouncement.populate(pa_text)
+    Admin::PublicAnnouncement.populate_long_term(pa_lt_text)
+    expect(Admin::PublicAnnouncement.count).to eq(2)
+    pa=Admin::PublicAnnouncement.where('description=?',pa_text)
+    lt_pa=Admin::PublicAnnouncement.where('description=?',pa_lt_text)
     expect(pa.size).to eq(1)
     expect(lt_pa.size).to eq(1)
     expect(pa.first.is_sticky).to eq(nil)
     expect(lt_pa.first.is_sticky).to eq(true)
 
-    PublicAnnouncement.clear_load_message
+    Admin::PublicAnnouncement.clear_load_message
     # Should not remove long term message
-    pa=PublicAnnouncement.where('description=?',pa_text)
+    pa=Admin::PublicAnnouncement.where('description=?',pa_text)
     expect(pa.size).to eq(0)
-    expect(PublicAnnouncement.count).to eq(1)
-    expect(PublicAnnouncement.first.is_sticky).to eq(true)
-    expect(PublicAnnouncement.first.description).to eq(pa_lt_text)
+    expect(Admin::PublicAnnouncement.count).to eq(1)
+    expect(Admin::PublicAnnouncement.first.is_sticky).to eq(true)
+    expect(Admin::PublicAnnouncement.first.description).to eq(pa_lt_text)
   end
 
 end
