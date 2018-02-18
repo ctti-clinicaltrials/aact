6196d0b2a45ec5be209d7901d46e4cb566d0525f
tibbs001 <sheri.tibbs@duke.edu>
2018-02-17 22:33:22 -0500

    Fix how problems get saved.  (had been overwriting previously recorded problems)

--- app/models/admin/load_event.rb
+++ app/models/admin/load_event.rb
@@ -6,7 +6,7 @@ module Admin
         return if self.completed_at.present?
         sc=params[:study_counts]
         self.status  = (params[:status] ?  params[:status] : 'complete')
-        self.problems = params[:problems]
+        self.problems = params[:problems] if params[:problems]
         self.completed_at = Time.now
         self.load_time = calculate_load_time
         if sc
@@ -17,14 +17,8 @@ module Admin
         self.save!
       end
 
-      def add_problem(errors={})
-        err="\n#{errors[:name]}"
-        desc="\n#{errors[:first_backtrace_line]}"
-        self.problems = "#{self.problems}#{err}"
-        self.problems = "#{self.problems}#{desc}"
-        $stdout.puts err
-        $stdout.puts desc
-        $stdout.flush
+      def add_problem(prob)
+        self.problems = "#{self.problems} \n#{prob}"
       end
 
       def calculate_load_time
