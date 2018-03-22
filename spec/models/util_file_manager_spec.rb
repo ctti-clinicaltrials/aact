5eb9e61fad63962aec61d29c5c17f9b45d7c5009
tibbs001 <sheri.tibbs@duke.edu>
2018-03-22 15:43:36 -0400

    aact-590: remove daily downloadable files when full load runs on the first of the month

--- spec/models/util_file_manager_spec.rb
+++ spec/models/util_file_manager_spec.rb
@@ -1,7 +1,95 @@
 require 'rails_helper'
 
 describe Util::FileManager do
-  describe 'create static db copy' do
+  context 'removes files that accumulated through the past month' do
+    it 'should remove flat pipe-delimited files created in previous month but not the one created on the first day of the month' do
+      cur_month=Date.today.strftime("%m")
+      cur_year=Date.today.year.to_s
+      prev_date=Time.now - 1.month
+      prev_month=prev_date.strftime("%m")
+      prev_year=prev_date.year.to_s
+
+      FileUtils.rm_rf(Dir['/aact-files/exported_files/*'])
+      # create dummy test flat files.
+      f1_should_keep="#{Util::FileManager.flat_files_directory}/20160901_clinical_trials.zip"
+      f2_should_keep="#{Util::FileManager.flat_files_directory}/20141003_clinical_trials.zip"
+      f3_should_keep="#{Util::FileManager.flat_files_directory}/#{cur_year}#{cur_month}01_clinical_trials.zip"
+      f4_should_keep="#{Util::FileManager.flat_files_directory}/#{prev_year}#{prev_month}01_clinical_trials.zip"
+
+      f5_should_remove="#{Util::FileManager.flat_files_directory}/#{prev_year}#{prev_month}03_clinical_trials.zip"
+      f6_should_remove="#{Util::FileManager.flat_files_directory}/#{prev_year}#{prev_month}05_clinical_trials.zip"
+      f7_should_remove="#{Util::FileManager.flat_files_directory}/#{prev_year}#{prev_month}25_clinical_trials.zip"
+
+      # these should be kept
+      f=File.new(f1_should_keep,'w')
+      f=File.new(f2_should_keep,'w')
+      f=File.new(f3_should_keep,'w')
+      f=File.new(f4_should_keep,'w')
+
+      # these should get removed
+      f=File.new(f5_should_remove,'w')
+      f=File.new(f6_should_remove,'w')
+      f=File.new(f7_should_remove,'w')
+
+      Util::FileManager.new.remove_daily_flat_files  #  <<<<<<  does all the work here
+
+      expect(File).to exist(File.new(f1_should_keep))
+      expect(File).to exist(File.new(f2_should_keep))
+      expect(File).to exist(File.new(f3_should_keep))
+      expect(File).to exist(File.new(f4_should_keep))
+
+      expect { File.new(f5_should_remove) }.to raise_error(StandardError, /No such file or directory @ rb_sysopen/)
+      expect { File.new(f6_should_remove) }.to raise_error(StandardError, /No such file or directory @ rb_sysopen/)
+      expect { File.new(f7_should_remove) }.to raise_error(StandardError, /No such file or directory @ rb_sysopen/)
+
+      FileUtils.rm_rf(Dir['/aact-files/exported_files/*'])
+    end
+
+    it 'should remove static db copies created in previous month but not the one created on the first day of the month' do
+      cur_month=Date.today.strftime("%m")
+      cur_year=Date.today.year.to_s
+      prev_date=Time.now - 1.month
+      prev_month=prev_date.strftime("%m")
+      prev_year=prev_date.year.to_s
+
+      FileUtils.rm_rf(Dir['/aact-files/static_db_copies/*'])
+      # create dummy test static db copy files.
+      f1_should_keep="#{Util::FileManager.static_copies_directory}/20160901_clinical_trials.zip"
+      f2_should_keep="#{Util::FileManager.static_copies_directory}/20141003_clinical_trials.zip"
+      f3_should_keep="#{Util::FileManager.static_copies_directory}/#{cur_year}#{cur_month}01_clinical_trials.zip"
+      f4_should_keep="#{Util::FileManager.static_copies_directory}/#{prev_year}#{prev_month}01_clinical_trials.zip"
+
+      f5_should_remove="#{Util::FileManager.static_copies_directory}/#{prev_year}#{prev_month}04_clinical_trials.zip"
+      f6_should_remove="#{Util::FileManager.static_copies_directory}/#{prev_year}#{prev_month}15_clinical_trials.zip"
+      f7_should_remove="#{Util::FileManager.static_copies_directory}/#{prev_year}#{prev_month}26_clinical_trials.zip"
+
+      # these should be kept
+      f=File.new(f1_should_keep,'w')
+      f=File.new(f2_should_keep,'w')
+      f=File.new(f3_should_keep,'w')
+      f=File.new(f4_should_keep,'w')
+
+      # these should get removed
+      f=File.new(f5_should_remove,'w')
+      f=File.new(f6_should_remove,'w')
+      f=File.new(f7_should_remove,'w')
+
+      Util::FileManager.new.remove_daily_snapshots  #  <<<<<<  does all the work here
+
+      expect(File).to exist(File.new(f1_should_keep))
+      expect(File).to exist(File.new(f2_should_keep))
+      expect(File).to exist(File.new(f3_should_keep))
+      expect(File).to exist(File.new(f4_should_keep))
+
+      expect { File.new(f5_should_remove) }.to raise_error(StandardError, /No such file or directory @ rb_sysopen/)
+      expect { File.new(f6_should_remove) }.to raise_error(StandardError, /No such file or directory @ rb_sysopen/)
+      expect { File.new(f7_should_remove) }.to raise_error(StandardError, /No such file or directory @ rb_sysopen/)
+
+      FileUtils.rm_rf(Dir['/aact-files/static_db_copies/*'])
+    end
+  end
+
+  context 'create static db copy' do
     it "should save db static copy to the appropriate directory" do
       allow_any_instance_of(Util::FileManager).to receive(:make_file_from_website).and_return(Util::FileManager.new.backend_schema_diagram)
       fm=Util::FileManager.new
