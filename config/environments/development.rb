f63ac9e22503bd06ada568c5f1389e2ba916ef6e
tibbs001 <sheri.tibbs@duke.edu>
2018-04-05 14:24:06 -0400

    display error information when exceptions raised

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,8 @@ Rails.application.configure do
   config.eager_load = false
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings =  {
     :address => '127.0.0.1',
