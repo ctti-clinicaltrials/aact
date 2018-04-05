6ca1fd6912753cda2c39721934115959757ec2f7
tibbs001 <sheri.tibbs@duke.edu>
2018-04-05 14:24:19 -0400

    Use the default Devise::Mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,8 +15,7 @@ Devise.setup do |config|
   config.mailer_sender = 'ctti-aact@duke.edu'
 
   # Configure the class responsible to send e-mails.
-  #config.mailer = 'Devise::Mailer'
-  config.mailer = 'Notifier'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   config.parent_mailer = 'ActionMailer::Base'
