b18ceee8b8a1b6a6ab14cfe2d537d42f9e815774
tibbs001 <sheri.tibbs@duke.edu>
2018-04-09 21:32:11 -0400

    gracefully handle invalid confirmation token

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -2,8 +2,14 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
   def show
     user=User.where('confirmation_token=?',params['confirmation_token']).first
-    user.confirm
-    redirect_to new_user_session_url
+    if user
+      user.confirm
+      flash[:notice] = "Your account has been confirmed. You now have access to the AACT database with the username #{user.username} and password you provided."
+      redirect_to new_user_session_url
+    else
+      flash[:notice] = "Invalid confirmation token: #{params['confirmation_token']}."
+      redirect_to new_user_session_url
+    end
   end
 
   private
