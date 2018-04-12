56a65a9fe565371d19d3acc035a0392420f391ef
tibbs001 <sheri.tibbs@duke.edu>
2018-04-12 19:57:15 -0400

    aact-610: do more to prevent non-downloadable files from appearing in the list of files on the Download page

--- spec/models/utils/util_file_manager_spec.rb
+++ spec/models/utils/util_file_manager_spec.rb
@@ -22,6 +22,27 @@ describe Util::FileManager do
       dump_file2=fm.get_dump_file_from(dump_file)
       expect(dump_file.name).to eq('postgres_data.dmp')
     end
+
+    it 'should not display files that are not downloadable zip files' do
+      dir_name="#{Rails.public_path}/static/tmp/static_copies"
+      FileUtils.remove_dir(dir_name) if File.exists?(dir_name)
+      FileUtils.mkdir_p(dir_name)
+      allow_any_instance_of(Util::FileManager).to receive(:static_copies_directory).and_return(dir_name)
+      fm=Util::FileManager.new
+      fpm=Util::FilePresentationManager.new
+
+      good_file_name="#{fm.static_copies_directory}/20180404_clinical_trials.zip"
+      good_file=File.new(good_file_name,"w")
+      expect(File).to exist(good_file)
+
+      bad_file_name="#{fm.static_copies_directory}/.nfs00000001c324f8c500000001"
+      bad_file=File.new(bad_file_name,"w")
+      expect(File).to exist(bad_file)
+      expect(fm.downloadable?(bad_file_name.split('/').last)).to eq(false)
+      expect(fm.downloadable?(good_file_name.split('/').last)).to eq(true)
+      FileUtils.remove_dir(dir_name)
+    end
+
   end
 
 end
