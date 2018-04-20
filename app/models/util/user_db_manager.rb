4a8d955d0a018933b2e9826f87041457fd6c2449
tibbs001 <sheri.tibbs@duke.edu>
2018-04-20 14:57:41 -0400

    user was not getting created. Rely on devise to create user.  Once created, create the db account

--- app/models/util/user_db_manager.rb
+++ app/models/util/user_db_manager.rb
@@ -1,29 +1,19 @@
 module Util
   class UserDbManager < DbManager
 
-    def self.change_password(user,pwd)
-      new.change_password(user,pwd)
+    def self.change_password(user, pwd)
+      new.change_password(user, pwd)
     end
 
     def create_user_account(user)
-      # We add the unconfirmed user to the db to reserve the username - prevent others from
-      # subsequently trying to create user with same name before user confirms the account
-      # When user eventually confirms the account, we will set their password to what they defined
       begin
-        if !can_create_user_account?(user)
-          return false
-        end
-        user.skip_password_validation=true  # don't validate user entered current password - they didn't have a chance!
-        user.unencrypted_password=user.password
-        dummy_pwd=ENV['UNCONFIRMED_USER_PASSWORD']
-        pub_con = PublicBase.establish_connection(ENV["AACT_PUBLIC_DATABASE_URL"]).connection
-
-        pub_con.execute("create user \"#{user.username}\" password '#{dummy_pwd}';")
-        pub_con.execute("grant connect on database aact to \"#{user.username}\";")
-        pub_con.execute("grant usage on schema ctgov TO \"#{user.username}\";")
-        pub_con.execute("grant select on all tables in schema ctgov to \"#{user.username}\";")
-        pub_con.disconnect!
-        @pub_con=nil
+        return false if !can_create_user_account?(user)
+        #user.skip_password_validation=true  # don't validate that user entered current password.  already validated
+        #pub_con = PublicBase.establish_connection(ENV["AACT_PUBLIC_DATABASE_URL"]).connection
+        pub_con.execute("create user \"#{user.username}\" password '#{user.password}';")
+        pub_con.execute("revoke connect on database aact from #{user.username};")
+        #pub_con.disconnect!
+        #@pub_con=nil
         return true
       rescue => e
         user.errors.add(:base, e.message)
@@ -47,19 +37,18 @@ module Util
     def remove_user(username)
       begin
         return false if !user_account_exists?(username)
-        pub_con.execute("reassign owned by #{username} to postgres;")
-        pub_con.execute("drop owned by #{username};")
-        pub_con.execute("revoke all on schema ctgov from #{username};")
-        pub_con.execute("revoke connect on database #{public_db_name} from #{username};")
+        revoke_db_privs(username)
         pub_con.execute("drop user #{username};")
         return true
       rescue => e
-        raise e unless e.message.include? " does not exist"
-        return false
+        raise e
       end
     end
 
     def change_password(user,pwd)
+      puts "=========== about to set password to #{pwd} ===================================================="
+      puts self.inspect
+      puts "==============================================================="
       begin
         pub_con.execute("alter user \"#{user.username}\" password '#{pwd}';")
       rescue => e
@@ -67,17 +56,20 @@ module Util
       end
     end
 
-    def grant_db_privs
-      revoke_db_privs # to avoid errors, ensure privs revoked first
-      con.execute("grant connect on database #{public_db_name} to public;")
-      con.execute("grant usage on schema ctgov TO public;")
-      con.execute('grant select on all tables in schema ctgov to public;')
+    def grant_db_privs(username)
+      pub_con.execute("grant connect on database aact to \"#{username}\";")
+      pub_con.execute("grant usage on schema ctgov TO \"#{username}\";")
+      pub_con.execute("grant select on all tables in schema ctgov to \"#{username}\";")
     end
 
-    def revoke_db_privs
-      con.execute("revoke connect on database #{public_db_name} from public;")
-      con.execute("revoke select on all tables in schema ctgov from public;")
-      con.execute("revoke all on schema ctgov from public;")
+    def revoke_db_privs(username)
+      pub_con = PublicBase.establish_connection(ENV["AACT_PUBLIC_DATABASE_URL"]).connection
+      pub_con.execute("reassign owned by #{username} to postgres;")
+      pub_con.execute("drop owned by #{username};")
+      pub_con.execute("revoke all on schema ctgov from #{username};")
+      pub_con.execute("revoke connect on database #{public_db_name} from #{username};")
+      pub_con.disconnect!
+      @pub_con=nil
     end
 
     def terminate_sessions_for(user)
