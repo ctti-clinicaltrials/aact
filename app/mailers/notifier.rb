2fd1291900362cab419cf5c31907ff1a060a256f
tibbs001 <sheri.tibbs@duke.edu>
2018-05-09 16:25:37 -0400

    aact-630:  have the User provide the subject line & it will display the Rails env.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -6,10 +6,12 @@ class Notifier < ApplicationMailer
     }
   end
 
-  def self.report_user_event(event, user)
-    admin_addresses.each { |email_addr|
-      send_msg(email_addr, "AACT user #{event}: #{user.full_name}", user.summary_info).deliver_now
-    }
+  def self.report_user_event(event_type, user)
+    admin_addresses.each { |email_addr| send_user_event_msg(email_addr, user, event_type).deliver_now }
+  end
+
+  def send_user_event_msg(email_addr, user, event_type)
+    send_msg(email_addr, user.notification_subject_line(event_type), user.summary_info)
   end
 
   def send_msg(email_addr, subject, body)
