90592ea7434220fc5831667149ffeec9700e279f
tibbs001 <sheri.tibbs@duke.edu>
2018-05-11 12:19:25 -0400

    aact-638:  Send email notification to AACT admins with user backup files.  (WIP - still need to attach the files to the email message)

--- app/models/admin/user_event.rb
+++ app/models/admin/user_event.rb
@@ -1,5 +1,27 @@
 module Admin
   class UserEvent < Admin::AdminBase
 
+    def subject_line
+      if event_type == 'backup'
+        "AACT #{Rails.env.capitalize} User Backups"
+      else
+        "AACT #{Rails.env.capitalize} user #{event_type}: #{self.description}"
+      end
+    end
+
+    def notification_message
+      "AACT User information is saved to the attached SQL files.  User info can be restored with the following commands:
+        First verify the schema defined in the file is correct (ctgov).
+        in aact_admin db:
+          truncate table users;
+          truncate table user_events;
+        psql -d aact_admin -a -f YYYYMMDD-aact-users_table.sql
+        psql -d aact_admin -a -f YYYYMMDD-aact-user_events.sql
+
+        On the public server that hosts the publicly accessible database:
+        psql -d aact -a -f YYYYMMDD-aact-user_accounts.sql
+        "
+    end
+
   end
 end
