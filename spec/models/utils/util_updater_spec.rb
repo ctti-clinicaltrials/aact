012090b65ac8485829aa5f40dd6390fa996d92b2
tibbs001 <sheri.tibbs@duke.edu>
2018-05-14 11:21:06 -0400

    aact-632:  update spec to use fresh data

--- spec/models/utils/util_updater_spec.rb
+++ spec/models/utils/util_updater_spec.rb
@@ -151,11 +151,11 @@ describe Util::Updater do
     expect(study.start_month_year).to eq('July 2001')
     expect(study.completion_month_year).to eq('November 2013')
     expect(study.primary_completion_month_year).to eq('January 2009')
-    expect(study.verification_month_year).to eq('November 2015')
+    expect(study.verification_month_year).to eq('November 2017')
 
     expect(study.study_first_submitted_date).to eq('September 13, 2001'.to_date)
     expect(study.results_first_submitted_date).to eq('February 12, 2014'.to_date)
-    expect(study.last_update_submitted_date).to eq('November 14, 2015'.to_date)
+    expect(study.last_update_submitted_date).to eq('November 27, 2017'.to_date)
 
     expect(study.start_date).to eq(study.start_month_year.to_date.end_of_month)
     expect(study.verification_date).to eq(study.verification_month_year.to_date.end_of_month)
