2b06cd60e3254823bd99d5261ae7776e899a0a5b
tibbs001 <sheri.tibbs@duke.edu>
2018-05-23 20:28:47 -0400

    aact-653: provide way to download list of users as csv or excel file

--- app/models/user.rb
+++ app/models/user.rb
@@ -151,4 +151,12 @@ class User < Admin::AdminBase
     return collection
   end
 
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |user|
+        csv << user.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
