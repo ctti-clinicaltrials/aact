c77687ee37585d1dc1267f55fa44095b25fec45a
tibbs001 <sheri.tibbs@duke.edu>
2018-06-06 11:40:49 -0400

    aact-668:  eliminate storing info to Removed_Users table (due to GDPR)

--- app/models/admin/removed_user.rb
+++
@@ -1,5 +0,0 @@
-require 'active_support/all'
-module Admin
-  class RemovedUser < Admin::AdminBase
-  end
-end
---
+++ db_admin/migrate/20180606153216_drop_removed_users.rb
@@ -0,0 +1,3 @@
+class DropRemovedUsers < ActiveRecord::Migration
+  drop_table :removed_users
+end
