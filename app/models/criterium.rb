b41ccf8a6a2d11ed857fad0f37a7ca41b4cf2249
tibbs001 <sheri.tibbs@duke.edu>
2019-04-17 20:12:54 -0400

    aact-949  Refactor to account for studies with only Inclusion Criteria or only Exclusion Criteria  (Still need to write tests for this.)

--- app/models/criterium.rb
+++ app/models/criterium.rb
@@ -3,12 +3,20 @@ class Criterium < StudyRelationship
 
   def self.create_all_from(opts)
     opts[:xml].xpath('//criteria').collect{|xml|
-      arr=xml.text.split('Exclusion criteria:')
-      arr=xml.text.split('Exclusion Criteria:') if arr.nil?
-      arr=xml.text.downcase.split('exclusion criteria') if arr.nil?
-      return if arr.nil?
-      incl = arr.first
-      excl = arr.last
+      test_input = xml.text
+      if (test_input.include?('Inclusion Criteria:') and test_input.include?('Exclusion Criteria:'))
+        incl=test_input.split('Exclusion Criteria:').first
+        excl=test_input.split('Exclusion Criteria:').last
+      elsif test_input.include?('Inclusion Criteria:')
+        incl=test_input
+        excl=''
+      elseif test_input.include?('Exclusion Criteria:')
+        incl=''
+        excl=test_input
+      else
+        puts "ERROR:  Unexpected criteria"
+        return
+      end
       create_each(incl, 'inclusion', opts)
       create_each(excl, 'exclusion', opts)
     }
@@ -18,7 +26,7 @@ class Criterium < StudyRelationship
     cntr = 1
     collection.split("\n\n").each{ |criterium|
       c=new.create_from({:xml=>criterium, :type=>type, :nct_id=>opts[:nct_id], :cntr => cntr})
-      if c.name and !c.name.include? "criteria:"
+      if !c.name.nil? and c.name.size > 1
         c.save!
         cntr = cntr + 1
       end
@@ -26,31 +34,36 @@ class Criterium < StudyRelationship
   end
 
   def attribs
-    nm    = clean_string(get_opt('xml'))
-    order = get_order_num(nm, get_opt('cntr'))
+    nm     = clean_string(get_opt('xml'))
+    return {} if nm.nil?
+    order  = get_order_num(nm, get_opt('cntr'))
+    dnm    = nm.downcase
     {
       :nct_id        => get_opt(:nct_id),
       :criteria_type => get_opt('type'),
-      :name          => nm.strip!,
-      :downcase_name => nm.downcase.strip!,
+      :name          => nm,
+      :downcase_name => dnm,
       :order_number  => order
     }
   end
 
   def clean_string(str)
+    return nil if str.downcase.include? 'inclusion criteria:' or str.downcase.include? 'exclusion criteria:'
     str.strip.gsub("\n",' ').gsub(/\s+/, ' ')
   end
 
   def get_order_num(nm, derived_cntr)
-    prefix = nm.split(' ').first
+    prefix = nm.split(' ').first if nm
     return derived_cntr if prefix.nil?
     order_defined_in_criterium = prefix.strip.gsub(".",'').gsub(":",'')
     is_a_num = Integer(order_defined_in_criterium) rescue false
     if !is_a_num
-      derived_cntr
+      return derived_cntr
     else
+      # cleanup the name - remove prefix and any leading spaces
       nm.slice! prefix
-      Integer(order_defined_in_criterium)
+      nm.strip!
+      return Integer(order_defined_in_criterium)
     end
   end
 
--- spec/models/criterium_spec.rb
+++ spec/models/criterium_spec.rb
@@ -12,12 +12,17 @@ RSpec.describe Criterium, type: :model do
 
       inclusion = described_class.where(criteria_type: 'inclusion')
       exclusion = described_class.where(criteria_type: 'exclusion')
-      expect(inclusion.size).to eq(12)
       expect(exclusion.size).to eq(23)
+      expect(inclusion.size).to eq(12)
       sample=inclusion.first
-      expect(sample.name).to eq('has histologically or cytologically documented adenocarcinoma nsclc')
+      expect(sample.name).to eq('Has histologically or cytologically documented adenocarcinoma NSCLC')
+      expect(sample.downcase_name).to eq('has histologically or cytologically documented adenocarcinoma nsclc')
       expect(sample.order_number).to eq(1)
+      sample=described_class.where('criteria_type=? and order_number=3','inclusion').first
+      expect(sample.name).to eq('Has acquired resistance to EGFR tyrosine kinase inhibitor (TKI) according to the Jackman criteria (PMID: 19949011):')
+      expect(sample.parent_criterium_id).to be(nil)
     end
+
   end
 
 end
