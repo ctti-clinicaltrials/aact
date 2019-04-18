14a306153c7eb1ba16b9ac52266578b7db2b1ded
tibbs001 <sheri.tibbs@duke.edu>
2019-04-18 08:36:00 -0400

    aact-949:  Add parent/child relationship to criteria

--- app/models/criterium.rb
+++ app/models/criterium.rb
@@ -1,5 +1,7 @@
 class Criterium < StudyRelationship
   self.table_name = 'criteria'
+  belongs_to :parent, class_name: 'Criterium'
+  has_many   :children, class_name: 'Criterium', foreign_key: 'parent_id'
 
   def self.create_all_from(opts)
     opts[:xml].xpath('//criteria').collect{|xml|
@@ -14,6 +16,7 @@ class Criterium < StudyRelationship
         incl=''
         excl=test_input
       else
+        # TODO.   Report this error properly
         puts "ERROR:  Unexpected criteria"
         return
       end
@@ -24,10 +27,31 @@ class Criterium < StudyRelationship
 
   def self.create_each(collection, type, opts)
     cntr = 1
+    previous_cntr = 1
+    previous_indent_size = 100000
+    previous_criterium = nil
+    parent_criterium = nil
     collection.split("\n\n").each{ |criterium|
-      c=new.create_from({:xml=>criterium, :type=>type, :nct_id=>opts[:nct_id], :cntr => cntr})
+      indent_size = criterium.count(' ') - criterium.lstrip.count(' ')
+      if indent_size == previous_indent_size
+        # this must be a sibling to the previous criterium
+      elsif indent_size > previous_indent_size
+        # this must be a child to the previous criterium
+        parent_criterium = previous_criterium
+        previous_indent_size = indent_size
+        previous_cntr = cntr
+        cntr = 1
+      elsif indent_size < previous_indent_size
+        # seems we've finished with the children of the previous criterium
+        parent_criterium = nil
+        previous_indent_size = indent_size
+        cntr = previous_cntr
+      end
+
+      c=new.create_from({:xml=>criterium, :type=>type, :nct_id=>opts[:nct_id], :cntr => cntr, :parent => parent_criterium})
       if !c.name.nil? and c.name.size > 1
         c.save!
+        previous_criterium = c
         cntr = cntr + 1
       end
     }
@@ -39,17 +63,19 @@ class Criterium < StudyRelationship
     order  = get_order_num(nm, get_opt('cntr'))
     dnm    = nm.downcase
     {
-      :nct_id        => get_opt(:nct_id),
-      :criteria_type => get_opt('type'),
-      :name          => nm,
-      :downcase_name => dnm,
-      :order_number  => order
+      :nct_id               => get_opt(:nct_id),
+      :parent               => get_opt(:parent),
+      :criteria_type        => get_opt('type'),
+      :name                 => nm,
+      :downcase_name        => dnm,
+      :order_number         => order
     }
   end
 
   def clean_string(str)
     return nil if str.downcase.include? 'inclusion criteria:' or str.downcase.include? 'exclusion criteria:'
-    str.strip.gsub("\n",' ').gsub(/\s+/, ' ')
+    val=str.strip.gsub("\n",' ').gsub(/\s+/, ' ')
+    val.gsub(/^[-] /, '')
   end
 
   def get_order_num(nm, derived_cntr)
--- spec/models/criterium_spec.rb
+++ spec/models/criterium_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Criterium, type: :model do
 
     it 'should handle studies that only have inclusion criteria' do
       described_class.destroy_all
+      sample1_name = "Women of childbearing potential must have a negative serum pregnancy test within 2 weeks prior to registration; patients that are pregnant or breast feeding are excluded; a female of childbearing potential is any woman, regardless of sexual orientation or whether they have undergone tubal ligation, who meets the following criteria:"
       nct_id= 'NCT02465060'
       xml=Nokogiri::XML(File.read("spec/support/xml_data/#{nct_id}.xml"))
       opts={xml: xml, nct_id: nct_id}
@@ -13,6 +14,19 @@ RSpec.describe Criterium, type: :model do
       exclusion = described_class.where(criteria_type: 'exclusion')
       expect(exclusion.size).to eq(0)
       expect(inclusion.size).to eq(82)
+      # should strip off leading ' - '
+      sample = described_class.where('order_number=1 and parent_id is null').first
+      expect(sample.name).to eq(sample1_name)
+      # should link children criteria to appropriate parent
+      children = described_class.where('parent_id = ?', sample.id)
+      expect(children.size).to eq(2)
+      # should link children criteria to another child criterium where appropriate
+      parent_sample = described_class.where('name=?',"Patients must have an electrocardiogram (ECG) within 8 weeks prior to registration to screening step and must meet the following cardiac criteria:").first
+      child_sample = described_class.where('name=?',"Resting corrected QT interval (QTc) =< 480 msec").first
+      expect(child_sample.criteria_type).to eq('inclusion')
+      grandchild_sample = described_class.where('name=?',"NOTE: If the first recorded QTc exceeds 480 msec, two additional, consecutive ECGs are required and must result in a mean resting QTc =< 480 msec; it is recommended that there are 10-minute (+/- 5 minutes) breaks between the ECGs").first
+      expect(child_sample.parent_id).to eq(parent_sample.id)
+      expect(grandchild_sample.parent_id).to eq(child_sample.id)
     end
 
     it 'should parse inclusion section and save each one' do
@@ -26,18 +40,28 @@ RSpec.describe Criterium, type: :model do
       exclusion = described_class.where(criteria_type: 'exclusion')
       expect(exclusion.size).to eq(23)
       expect(inclusion.size).to eq(12)
+      incl_with_parents=inclusion.select{|x| !x.parent_id.nil?}
+      excl_with_parents=exclusion.select{|x| !x.parent_id.nil?}
+      expect(incl_with_parents.size).to eq(2)
+      expect(excl_with_parents.size).to eq(8)
       sample=inclusion.first
       expect(sample.name).to eq('Has histologically or cytologically documented adenocarcinoma NSCLC')
       expect(sample.downcase_name).to eq('has histologically or cytologically documented adenocarcinoma nsclc')
       expect(sample.order_number).to eq(1)
       sample=described_class.where('criteria_type=? and order_number=3','inclusion').first
       expect(sample.name).to eq('Has acquired resistance to EGFR tyrosine kinase inhibitor (TKI) according to the Jackman criteria (PMID: 19949011):')
-      expect(sample.parent_criterium_id).to be(nil)
+      expect(sample.parent_id).to be(nil)
     end
 
     xit 'should handle studies that specify diff types of criteria' do
       'NCT01220531 Transplant Inclusion:'
       'NCT02260193  Key Inclusion & Key Exclusion Criteria'
+    end
+
+    xit 'should handle studies with criteria all on one line' do
+      "NCT03840122 Inclusion Criteria: 1. Patient is over the age of 21 2. Patient is scheduled to undergo a unilateral, primary TKA, secondary to osteoarthritis 3. Patient agrees to participate as a study subject and signs the Informed Consent and Research Authorization documents 4. Patient is able to read and speak English. Exclusion Criteria: 1. Patient is under the age of 21 2. Patient's primary diagnosis is not osteoarthritis (e.g. post-traumatic arthritis) 3. Patient is scheduled to undergo a bilateral TKA surgery 4. Patient is unable to read and speak English"
+      'NCT03720470 has both inclusion & exclusion all on one line'
+      "NCT02481830 doesn't specify inclusion or exclusion"
     end
 
   end
