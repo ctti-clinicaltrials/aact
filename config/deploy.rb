7dcbab0ee089ab8bf9d7b11bad39a10e074d0100
tibbs001 <sheri.tibbs@duke.edu>
2019-08-08 13:50:13 -0400

    Revert to using the execute rather than run command to create the symlink from public/static to /aact-files

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,11 +17,14 @@ task :finish_up do
   on roles(:app) do
     # create symlink to to the root directory containing aact static files
     # content of this directory can get big; we create this directory on a separate NAS drive
-    target = release_path.join('public/static')
     source = ENV.fetch('AACT_STATIC_FILE_DIR','/aact-files')
-    execute :ln, '-s', source, target
-    # restart the website
-    execute :touch, release_path.join('tmp/restart.txt')
+    target = release_path.join('public/static')
+    begin
+      execute :ln, '-s', source, target
+    rescue
+      # Don't fail if the source directory doesn't exist.
+      # the Util::FileManager will initialize public/static if necessary
+    end
   end
 end
 
@@ -46,12 +49,12 @@ set :format_options, command_output: true, log_file: "#{ENV.fetch('STATIC_FILE_D
 # Default value for default_env is {}
 
 set :default_env, {
-  'PATH' => ENV['AACT_PATH'] || "<server-path>/shared/bundle/ruby/2.4.0/bin:/opt/rh/rh-ruby24/root/usr/lib64",
-  'LD_LIBRARY_PATH' => ENV['LD_LIBRARY_PATH'] || "/opt/rh/rh-ruby24/root/usr/lib64",
-  'APPLICATION_HOST' => ENV['APPLICATION_HOST'] || 'localhost',
-  'RUBY_VERSION' =>  ENV['RUBY_VERSION'] || 'ruby 2.4.5',
-  'GEM_HOME' => ENV['GEM_HOME'] || '~/.gem/ruby',
-  'GEM_PATH' => ENV['GEM_PATH'] || '~/.gem/ruby/gems:/opt/rh/rh-ruby24/root/usr/share/gems:/opt/rh/rh-ruby24/root/usr/local/share/gems:/opt/rh/rh-ruby24/root/usr/lib64'
+  'PATH' => '/usr/local/pgsql/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/home/tibbs001/bin:/usr/local/share',
+  'LD_LIBRARY_PATH' => ENV['LD_LIBRARY_PATH'],
+  'APPLICATION_HOST' => 'localhost',
+  'RUBY_VERSION' =>  'ruby 2.4.5',
+  'GEM_HOME' => ENV['GEM_HOME'],
+  'GEM_PATH' => ENV['GEM_PATH']
 }
 
 # Default value for keep_releases is 5
