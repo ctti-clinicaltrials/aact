c217f07af3680d72a0d29e1d7f8b51973c61249b
tibbs001 <sheri.tibbs@duke.edu>
2019-10-28 10:09:20 -0400

    define env vars needed to deploy to server

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module AACT
     AACT_DB_SUPER_USERNAME = ENV['AACT_DB_SUPER_USERNAME'] || 'aact'   # Name of postgres superuser that has permission to create a database.
     AACT_OWNER_EMAIL       = ENV['AACT_OWNER_EMAIL']                   # Don't define this if your email service is not setup
     AACT_ADMIN_EMAILS      = ENV['AACT_ADMIN_EMAILS'] || "aact@your-org.org,admin@your-org.org" # Identifes who will receive load notifications
-    AACT_STATIC_FILE_DIR   = ENV['AACT_STATIC_FILE_DIR'] || '/aact-files'  # directory containing AACT static files such as the downloadable db snapshots
+    AACT_STATIC_FILE_DIR   = ENV['AACT_STATIC_FILE_DIR'] || '~/aact-files'  # directory containing AACT static files such as the downloadable db snapshots
     RACK_TIMEOUT           = ENV['RACK_TIMEOUT'] || 10
 
     APPLICATION_HOST          = 'localhost'
@@ -51,6 +51,18 @@ module AACT
     AACT_ADMIN_DATABASE_URL      = "postgres://#{AACT_DB_SUPER_USERNAME}@#{APPLICATION_HOST}:5432/#{AACT_ADMIN_DATABASE_NAME}"
     AACT_PUBLIC_DATABASE_URL     = "postgres://#{AACT_DB_SUPER_USERNAME}@#{AACT_PUBLIC_HOSTNAME}:5432/#{AACT_PUBLIC_DATABASE_NAME}"
     AACT_ALT_PUBLIC_DATABASE_URL = "postgres://#{AACT_DB_SUPER_USERNAME}@#{AACT_PUBLIC_HOSTNAME}:5432/#{AACT_ALT_PUBLIC_DATABASE_NAME}"
+
+    # If you deploy to a server, you need the following env variables defined for capistrano:
+    # AACT_DEPLOY_TO
+    AACT_PROD_REPO_URL="git@github.com:ctti-clinicaltrials/aact.git"
+    AACT_PROD_SERVER="ctti-web-01.oit.duke.edu"
+    AACT_DEV_REPO_URL="git@github.com:tibbs001/aact-1.git"
+    AACT_DEV_SERVER="ctti-web-dev-01.oit.duke.edu"
+    AACT_SERVER_USERNAME="ctti-aact"
+    #AACT_SSH_KEY_DIR="/#{AACT_SERVER_USER}/.ssh/id_rsa"
+
+    #Also create directory: /aact-files (either at system root or in home directory) and set AACT_STATIC_FILE_DIR to that directory.
+    #Under it, create sub directories:  documentation, logs, tmp
 
     #CORS
     cors_origins = '*'
--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ task :finish_up do
   on roles(:app) do
     # create symlink to to the root directory containing aact static files
     # content of this directory can get big; we create this directory on a separate NAS drive
-    source = ENV.fetch('AACT_STATIC_FILE_DIR','/aact-files')
+    source = '~/aact-files'
     target = release_path.join('public/static')
     begin
       execute :ln, '-s', source, target
@@ -35,7 +35,7 @@ end
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
-set :format_options, command_output: true, log_file: "#{ENV.fetch('STATIC_FILE_DIR','/aact-files')}/logs/capistrano_aact.log", color: :auto, truncate: :auto
+set :format_options, command_output: true, log_file: "~/aact-files/logs/capistrano_aact.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
@@ -52,6 +52,7 @@ set :default_env, {
   'PATH' => ENV['AACT_PATH'] || "<server-path>/shared/bundle/ruby/2.4.5/bin:/opt/rh/rh-ruby24/root/usr/lib64",
   'LD_LIBRARY_PATH' => ENV['LD_LIBRARY_PATH'] || "/opt/rh/rh-ruby24/root/usr/lib64",
   'APPLICATION_HOST' => ENV['APPLICATION_HOST'] || 'localhost',
+  'AACT_STATIC_FILE_DIR' => ENV['AACT_STATIC_FILE_DIR'] || '~/aact-files',
   'RUBY_VERSION' =>  ENV['RUBY_VERSION'] || 'ruby 2.4.5',
   'GEM_HOME' => ENV['GEM_HOME'] || '~/.gem/ruby',
   'GEM_PATH' => ENV['GEM_PATH'] || '~/.gem/ruby/gems:/opt/rh/rh-ruby24/root/usr/share/gems:/opt/rh/rh-ruby24/root/usr/local/share/gems:/opt/rh/rh-ruby24/root/usr/lib64'
