b5026a0c0f666ece7cc9aef4edb1fd7dca854a7d
Crystal Williams-Brown <crystal.williams.brown@gmail.com>
2020-04-22 15:42:38 -0400

    updated files for rails 5

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,18 @@
-require File.expand_path('../boot', __FILE__)
-require "rails"
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
+require_relative 'boot'
+
+require 'rails/all'
 require 'zip'
 require 'csv'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 module AACT
   class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Eastern Time (US & Canada)'
     config.quiet_assets = true
     config.generators do |generate|
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
-require File.expand_path('../application', __FILE__)
-# force Rails into production mode
-# We don't control web/app server & can't set it that way
-ENV['RAILS_ENV'] ||= 'production'
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
 Rails.application.initialize!
