3f253195e05d14ca6d46d9f45d4728b62d62c7ea
Crystal Williams-Brown <crystal.williams.brown@gmail.com>
2020-04-24 11:54:25 -0400

    added back the forcing into a production environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+# force Rails into production mode
+# We don't control web/app server & can't set it that way
+ENV['RAILS_ENV'] ||= 'production'
