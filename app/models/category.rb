2ebe87e3d843c40ffa91cf05341d909774eef60e
Crystal Williams-Brown <crystal.williams.brown@gmail.com>
2020-04-29 17:46:09 -0400

    setup almost all fields for covid-19 download

--- app/models/category.rb
+++ app/models/category.rb
@@ -46,6 +46,7 @@ class Category < ActiveRecord::Base
   end
 
   def self.study_values(study)
+    # sleep 3
     study_nct_id = study.nct_id
     id_values = study.id_information.pluck(:id_value).join('|')
     sponsors = study.sponsors
@@ -55,11 +56,67 @@ class Category < ActiveRecord::Base
     collaborators = grouped['collaborator']
     collab_names = collaborators.map{|collab| "#{collab.name}[#{collab.agency_class}]"}.join('|') if collaborators
     interventions = study.interventions
-    intervention_name_type = interventions.map{|intervention| "#{intervention.intervention_type}: #{intervention.name}"}.join('|') if interventions
-    intervention_details = interventions.map{|intervention| "#{intervention.intervention_type}:#{intervention.name}:#{intervention.description}"}.join('|') if interventions
-    design_groups = study.design_groups
-    arm_details = design_groups.map{|design_group| "#{design_group.group_type}:#{design_group.title}:#{design_group.description}"}.join('|') if design_groups
+    intervention_name_type = []
+    intervention_details = []
 
+    interventions.each do |intervention| 
+      intervention_name_type << "#{intervention.intervention_type || 'N/A'}: #{intervention.name}"
+      intervention_details << "#{intervention.intervention_type || 'N/A'}:#{intervention.name}:#{intervention.description}"
+    end
+    intervention_name_type = intervention_name_type.join('|')
+    intervention_details = intervention_details.join('|')
+
+    design_groups = study.design_groups
+    arm_details = []
+    arm_intervention_details = []
+    design_groups.each do |design_group| 
+      arm_details << "#{design_group.group_type || 'N/A'}:#{design_group.title}:#{design_group.description}"
+      interventions = design_group.interventions
+      interventions.each do |intervention|
+        arm_intervention_details << "#{design_group.group_type || 'N/A'}[#{design_group.title}]:#{intervention.intervention_type}[#{intervention.name}]"
+      end
+    end
+      
+    arm_details = arm_details.join('|')
+    arm_intervention_details = arm_intervention_details.join('|')
+
+    facilities = study.facilities
+    us_facility = facilities.find_by(country: ['USA', 'US', 'United States of America', 'United States', 'America'])
+
+    design = study.design
+    if design
+      primary_purpose = design.primary_purpose
+      intervention_model = design.intervention_model
+      observational_model = design.observational_model 
+      allocation = design.allocation 
+      masking = design.masking
+      subject_masked = design.subject_masked ? 1 : 0 
+      caregiver_masked = design.caregiver_masked ? 1 : 0
+      investigator_masked = design.investigator_masked ? 1 : 0 
+      outcomes_assessor_masked = design.outcomes_assessor_masked ? 1 : 0
+    end
+
+    eligibility = study.eligibility
+    if eligibility
+      minimum_age = eligibility.minimum_age
+      maximum_age = eligibility.maximum_age
+      gender = eligibility.gender
+      gender_based = eligibility.gender_based
+      gender_description = eligibility.gender_description
+      healthy_volunteers = eligibility.healthy_volunteers
+      population = eligibility.population
+      @criteria = eligibility.criteria
+    end
+
+    adaptive_protocol = single_term_query('adaptive', study) ? 1 : 0
+    master_protocol = single_term_query('master', study) ? 1 : 0
+    platform_protocol = single_term_query('platform', study) ? 1 : 0
+    umbrella_protocol = single_term_query('umbrella', study) ? 1 : 0
+    basket_protocol = single_term_query('basket', study) ? 1 : 0
+
+    
+    # "11Apr2016"
+    # byebug 
     [
       study_nct_id, #nct_id
       study.brief_title, #title
@@ -68,8 +125,8 @@ class Category < ActiveRecord::Base
       "https://ClinicalTrials.gov/show/#{study_nct_id}", #url
       study.overall_status, #status
       study.why_stopped, #why_stopped
-      hqc_query(study), #hqc
-      study.has_dmc, #has_dmc
+      hqc_query(study) ? 1 : 0, #hqc
+      study.has_dmc ? 1 : 0, #has_dmc
       sponsors.pluck(:agency_class).uniq.join('|'), #funded_bys
       sponsors.pluck(:name).join('|'), #sponsor_collaborators
       lead ? "#{lead.name}[#{lead.agency_class}]" : nil, #lead_sponsor
@@ -84,7 +141,50 @@ class Category < ActiveRecord::Base
       intervention_name_type, #interventions
       intervention_details, #intervention_details
       arm_details, #arm_datails
-
+      arm_intervention_details, #arm_intervention_details
+      study.design_outcomes.pluck(:measure).join('|'), #outcome_measures
+      study.start_date, #start_date
+      study.primary_completion_date, #primary_completion_date
+      study.completion_date, #completion_date 
+      study.study_first_posted_date, #first_posted
+      study.results_first_posted_date, #results_first_posted
+      study.last_update_posted_date, #last_update_posted
+      study.nlm_download_date_description, #nlm_download_date
+      study.study_first_submitted_date, #study_first_submitted_date
+      study.has_expanded_access ? 1 : 0, #has_expanded_access
+      study.is_fda_regulated_drug ? 1 : 0, #is_fda_regulated_drug
+      study.is_fda_regulated_device ? 1 : 0, #is_fda_regulated_device
+      study.is_unapproved_device ? 1 : 0, #is_unapproved_device
+      locations(facilities), #locations
+      facilities.count, #number_of_facilities
+      us_facility ? 1 : 0, #has_us_facility
+      facilities.count == 1 ? 1 : 0, #has_single_facility
+      study_design(design), #study_design
+      study.number_of_arms, #number_of_arms
+      study.number_of_groups, #number_of_groups
+      primary_purpose, #primary_purpose
+      intervention_model, #intervention_model
+      observational_model, #observational_model
+      allocation, #allocation
+      masking, #masking
+      subject_masked, #subject_masked
+      caregiver_masked, #caregiver_masked
+      investigator_masked, #investigator_masked
+      outcomes_assessor_masked, #outcomes_assessor_masked
+      adaptive_protocol, #adaptive_protocol
+      master_protocol, #master_protocol
+      platform_protocol, #platform_protocol
+      umbrella_protocol, #umbrella_protocol
+      basket_protocol, #basket_protocol
+      minimum_age, #minimum_agey
+      maximum_age, #maximum_agey
+      gender, #gender
+      gender_based, #gender_based
+      gender_description, #gender_description
+      healthy_volunteers, #healthy_volunteers
+      population, #population
+      @criteria, #criteria
+      study_documents(study), #study_documents
     ]
     
   end
@@ -98,22 +198,82 @@ class Category < ActiveRecord::Base
 
   def self.hqc_query(study)
     terms = %w[ hydroxychloroquine plaquenil hidroxicloroquina quineprox ]
-    official_title = study.official_title =~ /#{terms.join('|')}/
+    official_title = study.official_title =~ /#{terms.join('|')}/i
     return true if official_title
 
-    brief_title = study.brief_title =~ /#{terms.join('|')}/
+    brief_title = study.brief_title =~ /#{terms.join('|')}/i
     return true if brief_title
 
-    keywords = study.keywords.where(name: terms)
+    sql = terms.map{ 'name ILIKE ?'}.join(' OR ')
+    keywords = study.keywords.where(sql, *terms)
     return true unless keywords.empty?
 
-    interventions = study.interventions.where(name: terms)
+    interventions = study.interventions.where(sql, *terms)
     return true unless interventions.empty?
 
-    design_groups = study.design_groups.where(title: terms)
+    sql = terms.map{ 'title ILIKE ?'}.join(' OR ')
+    design_groups = study.design_groups.where(sql, *terms)
     return true unless design_groups.empty?
 
     false
+  end
+
+  def self.locations(facilities)
+    locations = []
+    facilities.each do |facility|
+      string = "#{facility.name}"
+      string += ", #{facility.city}" unless facility.city.empty? || facility.city.nil?
+      string += ", #{facility.state}" unless facility.state.empty? || facility.state.nil?
+      string += ", #{facility.country}" unless facility.country.empty? || facility.country.nil?
+
+      locations << string
+    end
+      # "#{facility.name}, #{facility.city}, #{facility.state}, #{facility.country}"
+    locations.join('|')
+  end
+
+  def self.study_design(design)
+    return unless design
+
+    who_masked = []
+    who_masked << 'Participant' if design.subject_masked
+    who_masked << 'Caregiver' if design.caregiver_masked
+    who_masked << 'Investigator' if design.investigator_masked
+    who_masked << 'Outcomes Assessor' if design.outcomes_assessor_masked
+
+    who_masked = who_masked.empty? ? '' : "(#{who_masked.join(', ')})"
+
+    study_design = []
+    study_design << "Allocation: #{design.allocation}" if design.allocation
+    study_design << "Intervention Model: #{design.intervention_model}" if design.intervention_model
+    study_design << "Observational Model: #{design.observational_model}" if design.observational_model
+    study_design << "Primary Purpose: #{design.primary_purpose}" if design.primary_purpose
+    study_design << "Time Perspective: #{design.time_perspective}" if design.time_perspective
+    study_design << "Masked: #{design.masking} #{who_masked}".squish if design.masking
+
+    study_design.join('|')
+  end
+
+  def self.single_term_query(term, study)
+    official_title = study.official_title =~ /#{term}/i
+    return true if official_title
+
+    brief_title = study.brief_title =~ /#{term}/
+    return true if brief_title
+
+    brief_summary = study.brief_summary.try(:description) =~ /#{term}/i
+    return true if brief_summary
+
+    detailed_description = study.detailed_description.try(:description) =~ /#{term}/i
+    return true if detailed_description
+
+    eligibility_criteria = @criteria =~ /#{term}/i if @criteria
+    return true if eligibility_criteria
+  end
+
+  def self.study_documents(study)
+    provided_documents = study.provided_documents
+    provided_documents.map{|provided_document| "#{provided_document.document_type}, #{provided_document.url}"}.join('|')
   end
 end
 
