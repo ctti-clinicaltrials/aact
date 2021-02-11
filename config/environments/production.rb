e9816148b05b4ca95c2d3c2a0168d14326c1bc72
Ramiro Rodriguez <ramiro.rodriguez@gmail.com>
2021-02-11 14:33:25 -0800

    use gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,12 @@ Rails.application.configure do
   config.log_level = :debug
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings =  {
-    :address => '127.0.0.1',
-    :port    => '25',
-    :domain  => host
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['EMAIL_USERNAME'],
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
