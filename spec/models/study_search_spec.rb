a0a83c81fa931b5957f1a5da81df1423272194f7
Crystal Williams-Brown <crystal@codethedream.org>
2021-02-17 10:37:09 -0500

    added some tests for the creation of searches

--- spec/models/study_search_spec.rb
+++ spec/models/study_search_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe StudySearch, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ':populate_database' do
+    it 'makes the correct number of searches' do
+      expect{ StudySearch.populate_database}.to change(StudySearch, :count).by 248
+    end
+  end
+  describe ':make_causes_of_death_search' do
+    it 'makes the correct number of searches' do
+      expect{ StudySearch.make_causes_of_death_search}.to change(StudySearch, :count).by 246
+    end
+    it 'makes searches that have the right data' do
+      StudySearch.make_causes_of_death_search
+      search = StudySearch.last
+      expect(search.save_tsv).to be false
+      expect(search.beta_api).to be false
+      expect(search.grouping).to_not eq nil
+      expect(search.grouping).to_not eq ''
+      expect(search.query).to_not eq nil
+      expect(search.query).to_not eq ''
+      expect(search.query).to eq search.name
+    end
+  end
+  describe ':make_covid_search' do
+    it 'makes the correct number of searches' do
+      expect{ StudySearch.make_covid_search}.to change(StudySearch, :count).by 1
+    end
+    it 'makes searches that have the right data' do
+      StudySearch.make_covid_search
+      search = StudySearch.find_by(name: 'covid-19')
+      expect(search.save_tsv).to be true
+      expect(search.beta_api).to be false
+      expect(search.grouping).to eq 'covid-19'
+      expect(search.query).to eq 'covid-19'
+    end
+  end
+  describe ':make_funder_search' do
+    it 'makes the correct number of searches' do
+      expect{ StudySearch.make_funder_search}.to change(StudySearch, :count).by 1
+    end
+    it 'makes searches that have the right data' do
+      # string = 'AREA[LocationCountry] EXPAND[None] COVER[FullMatch] "United States" AND AREA[LeadSponsorClass] EXPAND[None] COVER[FullMatch] "OTHER" AND AREA[FunderTypeSearch] EXPAND[None] NOT ( RANGE[AMBIG, NIH] OR RANGE[OTHER_GOV, UNKNOWN] )'
+    # find_or_create_by(save_tsv: false, grouping: 'funder_type', query: string, name: 'US no external funding', beta_api: true)
+      StudySearch.make_funder_search
+      search = StudySearch.find_by(grouping: 'covid-19')
+      expect(search.save_tsv).to be true
+      expect(search.beta_api).to be false
+      expect(search.grouping).to eq 'covid-19'
+      expect(search.query).to eq 'covid-19'
+      expect(search.name).to eq 'covid-19'
+    end
+  end
 end
