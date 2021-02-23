182615155b6df3612e45f8756cb91e35fdf93fd1
Crystal Williams-Brown <crystal@codethedream.org>
2021-02-23 18:27:22 -0500

    added a few more tests for study_search

--- spec/models/study_search_spec.rb
+++ spec/models/study_search_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe StudySearch, type: :model do
   let(:stub_request_headers) { {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'} }
+  let(:covid_batch) { File.read('spec/support/xml_data/covid_search.xml') }
+  let(:empty_batch) { File.read('spec/support/xml_data/empty_search.xml') }
+  let(:covid_url) { 'https://clinicaltrials.gov/ct2/results/rss.xml?cond=covid-19&count=1000&lup_d=2&start=0' }
+  let(:covid_stub) { stub_request(:get, covid_url).with(headers: stub_request_headers).to_return(:status => 200, :body => covid_batch, :headers => {}) }
+  let(:empty_search_stub) { stub_request(:get, covid_url).with(headers: stub_request_headers).to_return(:status => 200, :body => empty_batch, :headers => {}) }
   describe ':populate_database' do
     it 'makes the correct number of searches' do
       expect{ StudySearch.populate_database}.to change(StudySearch, :count).by 248
@@ -61,11 +66,7 @@ RSpec.describe StudySearch, type: :model do
       xml=Nokogiri::XML(File.read("spec/support/xml_data/NCT02798588.xml"))
       @etic_study=Study.new({xml: xml, nct_id: 'NCT02798588'}).create
       @covid_search = StudySearch.find_by(name: 'covid-19')
-      batch1 = File.read('spec/support/xml_data/covid_search_batch1.xml')
-      expected_url = 'https://clinicaltrials.gov/ct2/results/rss.xml?cond=covid-19&count=1000&lup_d=2&start=0'
-        stub_request(:get, expected_url).
-        with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-        to_return(:status => 200, :body => batch1, :headers => {})
+      covid_stub
     end
     after do
       Util::DbManager.new.remove_indexes_and_constraints
@@ -78,8 +79,25 @@ RSpec.describe StudySearch, type: :model do
       expect(SearchResult.find_by(nct_id: 'NCT04452435')).to be_truthy
       expect(SearchResult.find_by(nct_id: 'NCT02798588')).to be_nil
     end
-  end
-  describe ':execute' do
-
+    describe ':execute' do
+      it 'updates search_results' do
+        expect {StudySearch.execute}.to change(SearchResult, :count).by 1
+      end
+    end
+    describe ':fetch_study_ids' do
+      before do
+        @covid_search = StudySearch.make_covid_search
+      end
+      it 'returns the nct_ids' do
+        covid_stub
+        expect(@covid_search.fetch_study_ids.count).to eq 5
+      end
+      it 'returns an empty array if there are no nct_ids' do
+        empty_search_stub
+        expect(@covid_search.fetch_study_ids.count).to eq 0
+      end
+    end
+    describe ':json_data' do
+    end
   end
 end
