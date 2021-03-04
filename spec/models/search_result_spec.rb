6c52452beadf2b0b211aa394478b568717d45d5f
Crystal Williams-Brown <crystal@codethedream.org>
2021-03-03 21:36:56 -0500

    working on testing the tsv created

--- spec/models/search_result_spec.rb
+++ spec/models/search_result_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe SearchResult, type: :model do
+  let(:stub_request_headers) { {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'} }
+  let(:covid_batch) { File.read('spec/support/xml_data/covid_search.xml') }
+  let(:empty_batch) { File.read('spec/support/xml_data/empty_search.xml') }
+  let(:covid_url) { 'https://clinicaltrials.gov/ct2/results/rss.xml?cond=covid-19&count=1000&lup_d=2&start=0' }
+  let(:covid_stub) { stub_request(:get, covid_url).with(headers: stub_request_headers).to_return(:status => 200, :body => covid_batch, :headers => {}) }
+  let(:empty_search_stub) { stub_request(:get, covid_url).with(headers: stub_request_headers).to_return(:status => 200, :body => empty_batch, :headers => {}) }
+  
   context 'when there are search_results' do
     before do
+      covid_stub
       Util::DbManager.new.add_indexes_and_constraints
       covid_search = StudySearch.make_covid_search
       xml=Nokogiri::XML(File.read("spec/support/xml_data/NCT04452435_covid_19.xml"))
@@ -12,18 +20,20 @@ RSpec.describe SearchResult, type: :model do
     end
     after do
       `rm -r #{@folder}`
-      byebug
-      puts Dir.exists?(@folder)
-      byebug
     end
     describe ':make_tsv' do
       it 'makes a tsv file' do
         SearchResult.make_tsv
         expect(Dir.exists?(@folder)).to be true
         expect(Dir.empty?(@folder)).to be false
-      # xml=Nokogiri::XML(File.read("spec/support/xml_data/NCT02798588.xml"))
-      # @etic_study=Study.new({xml: xml, nct_id: 'NCT02798588'}).create
-      # @covid_search = StudySearch.find_by(name: 'covid-19')
+      end
+      it 'creates a tsv with the right content' do
+        SearchResult.make_tsv
+        filenames = Dir.entries(@folder)
+        filename = filenames.select{|name| name =~ /covid/}.first
+        content = File.open("#{@folder}/#{filename}").read
+        puts content
+        byebug
       end
     end
     describe ':study_values' do
