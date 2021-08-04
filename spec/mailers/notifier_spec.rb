6764363875f43e8802491cc0b2edf4b0f6f70613
Crystal Williams-Brown <crystal@codethedream.org>
2021-08-04 16:16:17 -0400

    added stub to fix failing notifier tests and fixed a couple of bugs in the tests

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -2,21 +2,26 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe Notifier, type: :mailer do
+  let(:stub_request_headers) { {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v1.1.0' } }
+  let(:study_statistics_body) {File.read('spec/support/json_data/study_statistics.json') }
+  let(:mailer_stub) { stub_request(:get, 'https://clinicaltrials.gov/api/info/study_statistics?fmt=json').with(headers: stub_request_headers).to_return(:status => 200, :body => study_statistics_body, :headers => {}) }
+  let(:msg) { described_class.send_msg('ctgov', 'test@gmail.com',event.subject_line, event.email_message) }
 
+  before do
+    mailer_stub
+  end
   describe 'load notification when nothing to load' do
     let!(:event) {Support::LoadEvent.new(:description=>'desc', :problems=>'')}
-    let(:msg) { described_class.send_msg('test@gmail.com',event.subject_line, event.email_message) }
 
     it 'msg has expected content' do
       expect(msg.subject).to eq('AACT Test Load Notification. Nothing to load.')
-      expect(msg.body).to eq('desc')
+      expect(msg.body).to include('desc')
       expect(msg.to.first).to eq('test@gmail.com')
     end
   end
 
   describe 'load notification when problems encountered' do
     let!(:event) {Support::LoadEvent.new(:description=>'desc', :problems=>'a problem',:should_add=>'1',:should_change=>'1', :processed=>'2')}
-    let(:msg) { described_class.send_msg('test@gmail.com',event.subject_line, event.email_message) }
 
     it 'msg has expected content' do
       expect(msg.subject).to eq('AACT Test Load - PROBLEMS ENCOUNTERED')
@@ -28,10 +33,9 @@ describe Notifier, type: :mailer do
 
   describe 'load notification when no problems encountered' do
     let!(:event) {Support::LoadEvent.new(:status=>'completed', :description=>'desc', :problems=>'',:should_add=>'1',:should_change=>'1', :processed=>'2')}
-    let(:msg) { described_class.send_msg('test@gmail.com',event.subject_line, event.email_message) }
 
     it 'msg has expected content' do
-      expect(msg.subject).to eq('AACT Test Load Notification. Status: completed. Added: 1 Updated: 1 Total: 2')
+      expect(msg.subject).to eq('AACT Test Load Notification. Status: completed. Added: 1 Updated: 1 Total: 2 Existing: 385281')
       expect(msg.body).to include('desc')
       expect(msg.body).not_to include('Problems encountered')
     end
