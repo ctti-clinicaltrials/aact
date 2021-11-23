4d17ca0231f3ea9553557e2330893171e711a903
Crystal Williams-Brown <crystal@codethedream.org>
2021-11-23 10:59:18 -0500

    added the removal of indexes and contraints for the full load

--- app/models/study_json_record.rb
+++ app/models/study_json_record.rb
@@ -75,6 +75,7 @@ class StudyJsonRecord < ActiveRecord::Base
   def self.full
     start_time = Time.current
     study_download = download_all_studies
+    remove_indexes_and_constraints
     nct_ids = Study.all.pluck(:nct_id).uniq
     clear_out_data_for(nct_ids)
     StudyJsonRecord.destroy_all
