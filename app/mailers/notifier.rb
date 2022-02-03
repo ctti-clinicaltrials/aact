78f843bddd77e8e5c66845585117dd8506e9b230
ievgend2 <ievgend2@gmail.com>
2022-02-03 13:20:43 -0500

    removed subject from the send_msg, removed schema from the self.report_load_event

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,17 +1,16 @@
 class Notifier < ApplicationMailer
-  def self.report_load_event(schema, event)
+  def self.report_load_event(event)
     admin_addresses.each { |email_addr|
-      send_msg(schema, email_addr, "#{schema} - #{event.subject_line}", event.email_message).deliver_now
+      send_msg(email_addr, "#{event.subject_line}", event.email_message).deliver_now
     }
   end
 
-     def send_msg(schema, email_addr, subject, event)
+  def send_msg(email_addr, event)
     db = Util::DbManager.new
     @public_study_count = db.public_study_count
     @ctgov_count = ClinicalTrialsApi.number_of_studies
     @event = event
-    @schema = schema
     @body = event.email_message
-    mail(to: email_addr, subject: subject, from: 'ctti@duke.edu')
+    mail(to: email_addr, subject: event.subject, from: 'ctti@duke.edu')
   end
 end
