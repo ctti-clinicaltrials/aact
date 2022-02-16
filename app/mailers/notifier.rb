024a478b78b483f77fe6f32d523e0bc9aafb5010
ievgend2 <ievgend2@gmail.com>
2022-02-16 11:32:36 -0500

    removed extra parameters from method calls #2

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,7 +1,7 @@
 class Notifier < ApplicationMailer
   def self.report_load_event(event)
     admin_addresses.each { |email_addr|
-      send_msg(email_addr, event.subject_line, event.email_message).deliver_now
+      send_msg(email_addr, event).deliver_now
     }
   end
 
