350c5ae5dd8f482989e5cdd5c192fad4d0561937
ievgend2 <ievgend2@gmail.com>
2022-06-08 11:21:07 -0400

    created a static method admin_emails

--- app/models/admin/user.rb
+++ app/models/admin/user.rb
@@ -1,12 +1,8 @@
-require 'active_support/all'
 module Admin
-    # do i need these above?
-
-    # class Admin::User < ApplicationRecord
-    class Admin::User < Admin::AdminBase
+    class User < Admin::AdminBase
+        self.table_name = "users"
         def self.admin_emails
-            # query goes here
+            where(admin:true).map {|user| user.email}
         end
     end
-
 end
--- spec/models/utils/util_updater_spec.rb
+++ spec/models/utils/util_updater_spec.rb
@@ -33,7 +33,7 @@ describe Util::Updater do
   it "continues on if there's a timeout error when attempting to retrieve data from clinicaltrials.gov for one of the studies"do
     updater=Util::Updater.new
     if AACT::Application::AACT_OWNER_EMAIL
-      expect { updater.send_notification }.to change { ActionMailer::Base.deliveries.count }.by(AACT::Application::User.admin_emails.split(',').size)
+      expect { updater.send_notification }.to change { ActionMailer::Base.deliveries.count }.by( Admin::User.admin_emails.size )
     end
     updater.run
   end
