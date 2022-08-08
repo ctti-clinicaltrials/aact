d169b1adee731fa308684d3a6f61d8a549517820
ievgend2 <ievgend2@gmail.com>
2022-08-08 11:24:45 -0400

    Added check to see if StudyJsonRecord exist and if no create new record

--- app/models/study_record.rb
+++ app/models/study_record.rb
@@ -22,7 +22,11 @@ class StudyRecord < ActiveRecord::Base
 
   def self.update_studies
     studies = studies_to_update
-    studies.each do |study|
+    studies.each do |study_id|
+      study = StudyJsonRecord.find_by(nct_id: study_id)
+      if study.nil?
+        study = StudyJsonRecord.create(nct_id: study_id)
+      end
       study.create_or_update_study
       # here need to run some more methods  (download file, extract the file, process all the xml files, process the studies that changed) to do a full load.
     end
