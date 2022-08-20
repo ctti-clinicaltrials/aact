bbe0075002b88879cdb3fac70bcf17bcd2ef3df4
ievgend2 <ievgend2@gmail.com>
2022-08-20 18:43:28 -0400

    хоть бы работало помоги кто знает

--- db/migrate/20220817000001_add_number_analayzed_and_number_analyzed_units_to_baseline_measurements.rb
+++ db/migrate/20220817000001_add_number_analayzed_and_number_analyzed_units_to_baseline_measurements.rb
@@ -2,5 +2,6 @@ class AddNumberAnalayzedAndNumberAnalyzedUnitsToBaselineMeasurements < ActiveRec
   def change
     add_column :baseline_measurements, :number_analayzed, :integer
     add_column :baseline_measurements, :number_analayzed_units, :string
+    # сомневаюсь что это правильно
   end
 end
