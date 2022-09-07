0d8f2c747145362a5243c0a360b9e1c0ef2601fe
Kassidy Holtzman <kassidy.holtzman@outlook.com>
2022-09-07 11:22:20 -0400

    Add data to milestone in StudyJsonRecord

--- app/models/study_json_record.rb
+++ app/models/study_json_record.rb
@@ -928,7 +928,9 @@ class StudyJsonRecord < ActiveRecord::Base
                           title: milestone['FlowMilestoneType'],
                           period: period['FlowPeriodTitle'],
                           description: achievement['FlowAchievementComment'],
-                          count: achievement['FlowAchievementNumSubjects']
+                          count: achievement['FlowAchievementNumSubjects'],
+                          milestones_descriptions: milestone['FlowMilestoneComment'],
+                          count_units: achievement['FlowAchievementNumUnits']
                         }
         end
       end
