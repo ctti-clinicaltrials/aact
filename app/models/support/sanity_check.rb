bc7b4087517656aaf160c056fc7c5f1873f32b4c
Ramiro Rodriguez <ramiro.rodriguez@gmail.com>
2022-12-01 10:47:06 -0800

    hotfix bug

--- app/models/support/sanity_check.rb
+++ app/models/support/sanity_check.rb
@@ -3,6 +3,7 @@
 module Support
   # Various quick sanity checks to make sure data is correct
   class SanityCheck < Support::SupportBase
+    self.table_name = 'support.sanity_checks'
     belongs_to :load_event
   end
 end
