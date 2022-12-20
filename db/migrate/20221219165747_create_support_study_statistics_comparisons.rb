88ecd06bc951b8fc210bbd37b87a211a2fc165a2
Ramiro Rodriguez <ramiro.rodriguez@gmail.com>
2022-12-19 18:02:47 -0800

    update the migration

--- db/migrate/20221219165747_create_support_study_statistics_comparisons.rb
+++ db/migrate/20221219165747_create_support_study_statistics_comparisons.rb
@@ -2,7 +2,12 @@ class CreateSupportStudyStatisticsComparisons < ActiveRecord::Migration[6.0]
   def change
     create_table 'support.study_statistics_comparisons' do |t|
       t.string :ctgov_selector
-      t.string :query
+      # we could add these columns to help us autogenerate the instances_query & the unique_query
+      # t.string :table
+      # t.string :column
+      # t.string :condition
+      t.string :instances_query
+      t.string :unique_query
     end
   end
 end
