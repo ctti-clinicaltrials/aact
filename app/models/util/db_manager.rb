35166ccfeb257f0a3d855523bb1f5a25f393f8f8
Ramiro Rodriguez <ramiro.rodriguez@gmail.com>
2023-01-02 12:46:01 -0800

    update db manager

--- app/models/util/db_manager.rb
+++ app/models/util/db_manager.rb
@@ -1,293 +1,299 @@
 # frozen_string_literal: true
 
+require 'open3'
 module Util
-  class Updater
-    attr_reader :params, :load_event, :client, :study_counts, :days_back, :full_featured, :schema, :search_days_back
+  class DbManager
+    attr_accessor :con, :public_con, :public_alt_con, :event, :migration_object, :fm
 
-    # days_back:     number of days
-    # full_featured: restore public db if true
-    # event_type:    type of load 'full' or 'incremental'
-    # restart:       restart an existing load
-    def initialize(params = {})
-      @full_featured = params[:full_featured] || false
-      @params = params
-      @type = (params[:event_type] || 'incremental')
-      @schema = params[:schema] || 'ctgov'
-      @search_days_back = params[:search_days_back]
-      ENV['load_type'] = @type
-      if params[:restart]
-        log("Starting the #{@type} load...")
-        @type = 'restart'
-      end
-      @days_back = (params[:days_back] || 4)
-      @study_counts = { should_add: 0, should_change: 0, processed: 0, count_down: 0 }
-      self
+    def initialize(params={})
+      # 'event' keeps track of what happened during a single load event & then saves to LoadEvent table in the admin db, so we have a log
+      # of all load events that have occurred.  If an event is passed in, use it; otherwise, create a new one.
+      @event = params[:event]
+      @fm = Util::FileManager.new
+      @config = YAML.safe_load(File.read("#{Rails.root}/config/connections.yml")).deep_symbolize_keys
     end
 
-    def run_main_loop
-      loop do
-        now = TZInfo::Timezone.get('America/New_York').now
-        if Support::LoadEvent.where('created_at > ?',now.beginning_of_day).count == 0
-          execute
-        else
-          ActiveRecord::Base.logger = nil
-          db_mgr.remove_constraints
-          update_old_studies
+    ##### connection management #####
+
+    def public_connection
+      db = @config[:public]
+      return unless db
+
+      connection = PublicBase.establish_connection(db).connection
+      connection.schema_search_path = 'ctgov'
+      connection
+    end
+
+    def staging_connection
+      db = @config[:staging]
+      return unless db
+
+      connection = PublicBase.establish_connection(db).connection
+      connection.schema_search_path = 'ctgov'
+      connection
+    end
+
+    def connection
+      Study.connection
+    end
+
+    ###### export/import methods ######
+
+    # generate a dump file
+    def dump_database
+      dump_file_location = fm.pg_dump_file
+      FileUtils.rm_f(dump_file_location)
+      config = Study.connection_config
+      host = config[:host]
+      port = config[:port]
+      username = config[:username]
+      database = config[:database]
+      host ||= 'localhost'
+      port ||= 5432
+
+      cmd = "
+        pg_dump  -v -h #{host} -p #{port} -U #{username} \
+        --clean --no-owner -b -c -C -Fc \
+        --exclude-table ar_internal_metadata \
+        --exclude-table schema_migrations \
+        --exclude-table study_records \
+        --schema 'ctgov'  \
+        -f #{dump_file_location} \
+        #{database} \
+      "
+      Rails.logger.debug cmd
+      run_command_line(cmd)
+
+      dump_file_location
+    end
+
+    # Restoring a database
+    # 1. prevent new connections and disconnect current connections
+    # 2. recreate the ctgov schema in the db
+    # 3. restore teh db from file
+    # 4. verify the study count (permissions are not granted again to prevent bad data from being used)
+    # 5. grant connection permissions again
+    def restore_database(connection, filename, schema = 'ctgov')
+      config = connection.instance_variable_get('@config')
+      host = config[:host]
+      port = config[:port]
+      username = config[:username]
+      database = config[:database]
+      password = config[:password]
+
+      # prevent new connections and drop current connections
+      connection.execute("ALTER DATABASE #{database} CONNECTION LIMIT 0;")
+      connection.execute("SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname ='#{database}' AND usename <> '#{username}'")
+
+      # recreate schema
+      log "  dropping in #{host}:#{port}/#{database} database..."
+      begin
+        connection.execute("DROP SCHEMA #{schema} CASCADE;")
+      rescue ActiveRecord::StatementInvalid => e
+        log(e.message)
+      end
+      connection.execute("CREATE SCHEMA #{schema};")
+      connection.execute("GRANT USAGE ON SCHEMA #{schema} TO read_only;")
+
+      # restore database
+      log "  restoring to #{host}:#{port}/#{database} database..."
+      cmd = "PGPASSWORD=#{password} pg_restore -c -j 5 -v -h #{host} -p #{port} -U #{username}  -d #{database} #{filename}"
+      run_restore_command_line(cmd)
+
+      # verify that the database was correctly restored
+      log "  verifying #{host}:#{port}/#{database} database..."
+      study_count = connection.execute('select count(*) from studies;').first['count'].to_i
+      if study_count != Study.count
+        raise "SOMETHING WENT WRONG! PROBLEM IN PRODUCTION DATABASE: #{host}:#{port}/#{database}.  Study count is #{study_count}. Should be #{Study.count}"
+      end
+
+      # allow users to access database again
+      connection.execute("ALTER DATABASE #{database} CONNECTION LIMIT 200;")
+      connection.execute("GRANT USAGE ON SCHEMA #{schema} TO read_only;")
+      connection.execute("GRANT SELECT ON ALL TABLES IN SCHEMA #{schema} TO READ_ONLY;")
+
+      true
+    end
+
+    def restore_from_file(path_to_file: "#{Rails.root}/tmp/postgres_data.dmp", database: 'aact')
+      Rails.logger.debug 'restoring the database...'
+      restore_database('normal', ActiveRecord::Base.connection, path_to_file)
+      Rails.logger.debug 'done'
+    end
+
+    def restore_from_url(params = {})
+      url = params[:url]
+      database_name = params[:database_name] || 'aact'
+      return unless url
+
+      tries ||= 5
+      file_path = "#{Rails.root}/tmp/snapshots"
+      FileUtils.rm_rf(file_path)
+      FileUtils.mkdir_p file_path
+      file_name = "#{file_path}/snapshot.zip"
+      file = File.new file_name, 'w'
+
+      Rails.logger.debug 'downloading file...'
+      begin
+        `curl -o #{file.path} #{url}`
+      rescue Errno::ECONNRESET
+        if (tries -= 1) > 0
+          retry
+        end
+      end
+      Rails.logger.debug 'done'
+
+      file.binmode
+
+      Rails.logger.debug 'extracting postgres_data.dmp...'
+      Zip::File.open(file) do |zip_file|
+        zip_file.each do |f|
+          if f.name == 'postgres_data.dmp'
+            fpath = File.join(file_path, f.name)
+            zip_file.extract(f, fpath) unless File.exist?(fpath)
+          end
+        end
+      end
+      Rails.logger.debug 'done'
+
+      restore_from_file({ path_to_file: "#{file_path}/postgres_data.dmp", database: database_name })
+
+      Rails.logger.debug 'removing temp folder...'
+      FileUtils.rm_rf(file_path)
+      Rails.logger.debug 'done'
+    end
+
+    # process for deploying database to digital ocean
+    # 1. try restoring to staging db
+    # 2. if successful restore to public db
+    def refresh_public_db(schema = 'ctgov')
+      success = restore_database(schema, staging_connection, fm.pg_dump_file)
+      return unless success
+
+      restore_database(schema, public_connection, fm.pg_dump_file)
+    end
+
+    def run_command_line(cmd)
+      _stdout, stderr, status = Open3.capture3(cmd)
+      return unless status.exitstatus != 0
+
+      log stderr
+      event.add_problem("#{Time.zone.now}: #{stderr}")
+      false
+    end
+
+    def run_restore_command_line(cmd)
+      _stdout, stderr, status = Open3.capture3(cmd)
+      return unless status.exitstatus != 0
+
+      # Errors that report a db object doesn't already exist aren't real errors. Ignore those.  Look for real errors.
+      real_errors = []
+      stderr_array = stderr.split('pg_restore:')
+      stderr_array.each { |line| real_errors << line if line.include?('ERROR') && line.exclude?('does not exist') }
+      return if real_errors.empty?
+
+      real_errors.each { |e| event.add_problem("#{Time.zone.now}: #{e}") }
+      false
+    end
+
+    ###### Constraint Management ######
+
+    def add_indexes_and_constraints
+      add_indexes
+      add_constraints
+    end
+
+    def remove_indexes_and_constraints
+      remove_constraints
+      remove_indexes
+    end
+
+    def add_indexes
+      indexes.each do |index|
+        unless migration.index_exists?(index[:table], index[:column])
+          migration.add_index(index[:table], index[:column], unique: index[:unique])
         end
       end
     end
 
-    # updates the oldest studies
-    def update_old_studies(count=100)
-      # ids = Study.where('updated_at < ?',Time.now-24.hours).order(updated_at: :asc).limit(count)
-      studies = Study.order(updated_at: :asc).limit(count)
-      puts "refreshing #{studies.count} studies"
-      studies.each do |study|
-        puts "refresh #{study.nct_id} #{study.updated_at}"
-        update_study(study.nct_id)
-      end
-    end
-
-    def execute
-      @load_event = Support::LoadEvent.create({ event_type: @type, status: 'running', description: '', problems: '' })
-      # TODO: need to extract this into a connection method
-      ActiveRecord::Base.logger = nil
-
-      # 1. remove constraings
-      log("removing constraints...")
-      db_mgr.remove_constraints
-      @load_event.log("1/11 removed constraints")
-
-      # 2. update studies
-      log("updating studies...")
-      update_studies
-      @load_event.log("2/11 updated studies")
-
-      # 3. add constraints
-      log("adding constraints...")
-      db_mgr.add_constraints
-      @load_event.log("3/11 added constraints")
-
-      # 4. comparing the counts from CT.gov to our database
-      log("comparing counts...")
-      begin
-        Verifier.refresh({schema: schema, load_event_id: @load_event.id})
-      rescue => e
-        Airbrake.notify(e)
-      end
-      @load_event.log("4/11 verification complete")
-
-      # 5. run study searches
-      log("execute study search...")
-      StudySearch.execute(search_days_back)
-      @load_event.log("5/11 executed study searches")
-
-      # 6. update calculated values
-      log("update calculated values...")
-      CalculatedValue.populate
-      @load_event.log("6/11 updated calculated values")
-
-      # 7. populate the meshterms and meshheadings
-      MeshTerm.populate_from_file
-      MeshHeading.populate_from_file
-      set_downcase_terms
-      @load_event.log("7/11 populated mesh terms")
-
-      # 8. run sanity checks
-      load_event.run_sanity_checks
-      @load_event.log("8/11 ran sanity checks")
-
-      if load_event.sanity_checks.count == 0
-        # 9. take snapshot
-        log("#{schema} take snapshot...")
-        take_snapshot
-        @load_event.log("9/11 db snapshot created")
-
-        # 10. refresh public db
-        log("#{schema} refresh public db...")
-        db_mgr.refresh_public_db
-        @load_event.log("10/11 refreshed public db")
-
-        # 10. create flat files
-        log("#{schema} creating flat files...") 
-        create_flat_files
-        @load_event.log("11/11 created flat files")
-      end
-
-      # refresh_data_definitions
-      
-      # 11. change the state of the load event from “running” to “complete”
-      @load_event.update({ status:'complete', completed_at: Time.now})
-
-      # 12. import study records
-      `rm -rf downloads`
-      StudyRecord.download
-      StudyRecord.unzip
-      StudyRecord.import_files
-      
-    rescue => e
-      # set the load event status to "error"
-      @load_event.update({ status: 'error'}) 
-      # set the load event problems to the exception message
-      @load_event.update({ problems: "#{e.message}\n\n#{e.backtrace.join("\n")}" }) 
-    end
-
-    def current_study_differences
-      studies = ClinicalTrialsApi.all
-      puts "aact  study count: #{Study.count}"
-      puts "ctgov study count: #{studies.count}"
-
-      # find all the studies that need to be updated
-      current = Hash[Study.pluck(:nct_id, :last_update_posted_date)]
-      ids = studies.select do |entry|
-        current_date = current[entry[:id]]
-        current_date.nil? || entry[:updated] > current_date
-      end
-      to_update = ids.map {|k| k[:id] }
-
-      # find all the studies that need to be removed
-      current = current.keys
-      studies = studies.map{|k| k[:id] }
-      to_remove = current - studies
-
-      return studies, to_update, to_remove
-    end
-
-    def update_studies
-      studies, to_update, to_remove = current_study_differences
-
-      log("#{schema} updating #{to_update.length} studies")
-      log("#{schema} removing #{to_remove.length} studies")
-
-      # update studies
-      total = to_update.length
-      total_time = 0
-      stime = Time.now
-      to_update.each_with_index do |id, idx|
-        t = update_study(id)
-        total_time += t
-        avg_time = total_time / (idx + 1)
-        remaining = (total - idx - 1) * avg_time
-        puts "#{total - idx} #{id} #{t} #{htime(total_time)} #{htime(remaining)}"
-      end
-      time = Time.now - stime
-      puts "Time: #{time} avg: #{time / total}"
-
-      # remove studies
-      raise "Removing too many studies #{to_remove.count}" if  Study.count <= to_remove.count
-      total = to_remove.length
-      total_time = 0
-      stime = Time.now
-      to_remove.each_with_index do |id, idx|
-        t = remove_study(id)
-        total_time += t
-        avg_time = total_time / (idx + 1)
-        remaining = (total - idx - 1) * avg_time
-        puts "#{total - idx} #{id} #{t} #{htime(total_time)} #{htime(remaining)}"
-      end
-    end
-
-    def htime(seconds)
-      seconds = seconds.to_i
-      hours = seconds / 3600
-      seconds -= hours * 3600
-      minutes = seconds / 60
-      seconds -= minutes * 60
-      "#{hours}:#{'%02i' % minutes}:#{'%02i' % seconds}"
-    end
-
-    def update_study(nct_id)
-      begin
-        stime = Time.now
-        record = StudyJsonRecord.find_by(nct_id: nct_id) || StudyJsonRecord.create(nct_id: nct_id, content: {})
-        changed = record.update_from_api
-
-        if record.blank? || record.content.blank?
-          record.destroy
-        else
-          record.create_or_update_study
+    def add_constraints
+      foreign_key_constraints.each do |constraint|
+        child_table = constraint[:child_table]
+        parent_table = constraint[:parent_table]
+        child_column = constraint[:child_column]
+        parent_column = constraint[:parent_column]
+        unless migration.foreign_key_exists?(child_table, parent_table, column: child_column)
+          migration.add_foreign_key child_table, parent_table, column: child_column, primary_key: parent_column,
+                                                              name: "#{child_table}_#{child_column}_fkey"
         end
-      rescue => e
-        ErrorLog.error(e)
-        Airbrake.notify(e)
       end
-      Time.now - stime
     end
 
-    def remove_study(id)
-      stime = Time.now
-      study = Study.find_by(nct_id: id)
-      study.remove_study_data if study
-      Time.now - stime
+    def remove_indexes
+      StudyRelationship.study_models.each do |model|
+        table_name = model.table_name
+        connection.indexes(table_name).each do |index|
+          migration.remove_index(index.table, index.columns) unless should_keep_index?(index)
+        end
+      end
     end
 
-    def load_study(study_id)
-      # 1. remove constraings
-      log("#{schema} remove constraints...")
-      db_mgr.remove_constraints
-      update_study(study_id)
-      # 2. add constraints
-      log("#{schema} adding constraints...")
-      db_mgr.add_constraints
+    def remove_constraints
+      foreign_key_constraints.each do |constraint|
+        table = constraint[:child_table]
+        column = constraint[:child_column]
+        if connection.foreign_keys(table).map(&:column).include?(column)
+          connection.remove_foreign_key(table, column: column)
+        end
+      end
     end
 
-    def load_multiple_studies(string_nct_ids)
-      # string_nct_ids looks like 'NCT00700336 NCT00772330 NCT00845871 NCT00852124 NCT01178814'
-      # here I'm turning the string into an array
-      array_nctids = string_nct_ids.split(' ')
-      # 1. remove constraings
-      log("#{schema} remove constraints...")
-      db_mgr.remove_constraints
-      array_nctids.each{|nctid|update_study(nctid)}
-      # 2. add constraints
-      log("#{schema} adding constraints...")
-      db_mgr.add_constraints
+    def should_keep_index?(index)
+      return true if (index.table == 'studies') && (index.columns == ['nct_id'])
+
+      false
     end
 
-    def set_downcase_terms
-      log('setting downcase mesh terms...')
-      con=ActiveRecord::Base.connection
-      #  save a lowercase version of MeSH terms so they can be found without worrying about case
-      con.execute("UPDATE browse_conditions SET downcase_mesh_term=lower(mesh_term);")
-      con.execute("UPDATE browse_interventions SET downcase_mesh_term=lower(mesh_term);")
-      con.execute("UPDATE keywords SET downcase_name=lower(name);")
-      con.execute("UPDATE conditions SET downcase_name=lower(name);")
+    def migration
+      @migration ||= ActiveRecord::Migration.new
+    end
+
+    ###### Information ######
+    def indexes
+      models = StudyRelationship.study_models.map do |m|
+        { table: m.table_name, column: 'nct_id', unique: one_to_one_related_tables.include?(m.table_name) }
+      end
+      extra = JSON.parse(File.read('db/indexes.json')).map { |k| { table: k.first, column: k.last } }
+      models + extra
+    end
+
+    def foreign_key_constraints
+      Util::DbManager.foreign_key_constraints
+    end
+
+    def self.foreign_key_constraints
+      models = StudyRelationship.study_models.map do |m|
+        { child_table: m.table_name, parent_table: 'studies', child_column: 'nct_id', parent_column: 'nct_id' }
+      end
+      extra = JSON.parse(File.read('db/foreign_keys.json')).map(&:deep_symbolize_keys)
+      models + extra
+    end
+
+    def one_to_one_related_tables
+      %w[brief_summaries designs detailed_descriptions eligibilities participant_flows
+         calculated_values]
+    end
+
+    def accessible?(connection)
+      config = connection.instance_variable_get('@config')
+      name = config[:database]
+      output = connection.execute("select datconnlimit from pg_database where datname='#{name}';")
+      limit = output.first['datconnlimit'].to_i
+      limit > 0 || limit == -1
     end
 
     def log(msg)
-      puts "#{Time.zone.now}: #{msg}"
-    end
-
-    def refresh_data_definitions(data = Util::FileManager.new.default_data_definitions)
-      return unless Admin::AdminBase.database_exists?
-
-      log('refreshing data definitions...')
-      Admin::DataDefinition.populate(data)
-    end
-
-    def take_snapshot
-      log('dumping database...')
-      db_mgr.dump_database
-
-      log('creating zipfile of database...')
-      Util::FileManager.new.save_static_copy
-      rescue StandardError => e
-        load_event.add_problem("#{e.message} (#{e.class} #{e.backtrace}")
-    end
-
-    def send_notification
-      log('sending email notification...')
-      Notifier.report_load_event(load_event)
-    end
-
-    def create_flat_files
-      log('exporting tables as flat files...')
-      Util::TableExporter.new([],'ctgov').run(delimiter: '|')
-    end
-
-    def db_mgr
-      # @db_mgr ||= Util::DbManager.new(event: load_event)
-      Util::DbManager.new(event: load_event)
+      Rails.logger.debug { "#{Time.zone.now}: #{msg}" } # log to STDOUT
     end
   end
 end
