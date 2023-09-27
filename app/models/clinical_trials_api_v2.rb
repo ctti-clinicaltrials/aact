5a08e849ba15434a40ce9f0212fb3c5bddc0ef7f
kostik <kostik@aact.myguest.virtualbox.org>
2023-09-27 12:17:03 -0400

    initial commit

--- app/models/clinical_trials_api_v2.rb
+++ app/models/clinical_trials_api_v2.rb
@@ -57,4 +57,40 @@ class ClinicalTrialsApiV2
     JSON.parse(body)
   end
 
+  # get all the studies from ctgov
+  def self.all(days_back: nil)
+    found = Float::INFINITY
+    offset = 1
+    items = []
+    while items.length < found
+      t = Time.now
+      url = "#{BASE_URL}/query/study_fields?fields=NCTId%2CStudyFirstPostDate%2CLastUpdatePostDate&min_rnk=#{offset}&max_rnk=#{offset + 999}&fmt=json"
+      # puts url
+      attempts = 0
+      begin
+        next if attempts > 3
+        json = JSON.parse(Faraday.get(url).body)
+      rescue Faraday::ConnectionFailed
+        attempts += 1
+        retry
+      rescue JSON::ParserError
+        attempts += 1
+        retry
+      end
+      found = json['StudyFieldsResponse']['NStudiesFound']
+      puts "current: #{items.length} found: #{found}, min: #{offset} max: #{offset + 1000}"
+      json['StudyFieldsResponse']["StudyFields"].each do |item|
+        items << { 
+          id: item["NCTId"].first,
+          posted: Date.parse(item["StudyFirstPostDate"].first),
+          updated: Date.parse(item["LastUpdatePostDate"].first)
+        }
+      end
+      items.uniq!
+      offset = offset < found ? offset + 1000 : 1
+      puts "loop #{Time.now - t}"
+    end
+    return items
+  end
+
 end
