8ca8a4b64fa4544fc993fca8bd8e7319107c4d17
kostik <kostik@aact.myguest.virtualbox.org>
2023-10-12 16:16:38 -0400

    download method finished

--- app/models/study_downloader.rb
+++ app/models/study_downloader.rb
@@ -1,5 +1,11 @@
 class StudyDownloader
     def self.download(nct_ids)
-      # code goes here to download the list of studies
+      
+      nct_ids.each do |nct_id|
+        record = StudyJsonRecord.find_by(nct_id: nct_id) || StudyJsonRecord.create(nct_id: nct_id, content: {})
+        record.update_from_api
+        puts "Study id: #{record.id}, study nct_id: #{record.nct_id}, version: #{record.version}"
+      end
+
     end
   end
