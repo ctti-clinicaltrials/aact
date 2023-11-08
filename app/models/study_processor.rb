2e6b8841306824ce9e45bb6c696e6858a4f5b665
kostik <kostik@aact.myguest.virtualbox.org>
2023-11-08 11:51:47 -0500

    prcessorV2 processing added

--- app/models/study_processor.rb
+++ app/models/study_processor.rb
@@ -5,7 +5,8 @@ class StudyProcessor
               if study.version == 1
                 study.create_or_update_study
               elsif study.version == 2 
-                
+                ProcessorV2.new(study.content).process
+                # StudyDownloader.update_from_apiV2(study, study.nct_id)
               end
             end  
         end
