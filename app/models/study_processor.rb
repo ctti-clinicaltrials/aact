571650abadfc4a1df33dab19306674a89e30524c
kostik <kostik@aact.myguest.virtualbox.org>
2023-11-09 16:51:27 -0500

    done

--- app/models/study_processor.rb
+++ app/models/study_processor.rb
@@ -1,14 +1,18 @@
 class StudyProcessor
     def self.process
-        StudyJsonRecord.all.each do |study| 
-            if study.updated_at > saved_study_at || saved_study_at == nil
-              if study.version == 1
-                study.create_or_update_study
-              elsif study.version == 2 
-                ProcessorV2.new(study.content).process
-                # StudyDownloader.update_from_apiV2(study, study.nct_id)
-              end
-            end  
+      saved_nil = []
+      StudyJsonRecord.all.each do |study|
+        if study.saved_study_at.nil? 
+          saved_nil.push(study)
+        elsif (study.updated_at > study.saved_study_at) && !study.updated_at.nil?
+          saved_nil.push(study)
         end
+      end
+
+      saved_nil.each do |record|
+          record.create_or_update_study if record.version == "1"
+          StudyJsonRecord::ProcessorV2.new(record.content).process if record.version == "2"
+      end
+        
     end
 end
