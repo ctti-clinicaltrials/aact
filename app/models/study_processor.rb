99d2593ae40b0075d4243f4287a31a5bb8d1909e
kostik <kostik@aact.myguest.virtualbox.org>
2023-12-04 14:04:12 -0500

    method optimized

--- app/models/study_processor.rb
+++ app/models/study_processor.rb
@@ -1,16 +1,9 @@
 class StudyProcessor
     def self.process
-        saved_nil = []
-        StudyJsonRecord.all.each do |study|
-        if (study.saved_study_at.nil?) || (study.updated_at > study.saved_study_at)
-            saved_nil.push(study)
-        end
-    end
-        
-        saved_nil.each do |record|
-        record.create_or_update_study if record.version == "1"
-        StudyJsonRecord::ProcessorV2.new(record.content).process if record.version == "2"
-        end
+      records = StudyJsonRecord.where('saved_study_at IS NULL OR updated_at > saved_study_at')
+      records.each do |record|
+      record.create_or_update_study if record.version == "1"
+      StudyJsonRecord::ProcessorV2.new(record.content).process if record.version == "2"
     end
 end
     
