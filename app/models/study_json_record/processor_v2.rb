c2720b76b4915e801634acf7b1fd187f4dd1130e
kostik <kostik@aact.myguest.virtualbox.org>
2023-12-05 11:35:54 -0500

    mapper created

--- app/models/study_json_record/processor_v2.rb
+++ app/models/study_json_record/processor_v2.rb
@@ -101,6 +101,24 @@ class StudyJsonRecord::ProcessorV2
   end
 
   def central_contacts_data
+    return unless @contacts_location_module
+
+    central_contacts = @contacts_location_module.dig('centralContacts')
+    return unless central_contacts
+
+    collection = []
+    central_contacts.each_with_index do |contact, index|
+      collection << {
+                      nct_id: nctId,
+                      contact_type: index == 0 ? 'primary' : 'backup',
+                      name: contact['name'],
+                      phone: contact['phone'],
+                      email: contact['email'],
+                      phone_extension: contact['phoneExt'],
+                      role: contact["role"]
+                     }
+    end
+    collection
   end
 
   def conditions_data
