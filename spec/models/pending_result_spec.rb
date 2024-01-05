0f10a1e87df055f81ae46ff111e18f62849e66dc
kholtzman5610 <kassidy.holtzman@outlook.com>
2024-01-05 11:39:33 -0500

    add-documents_data-mapper

--- spec/models/pending_result_spec.rb
+++ spec/models/pending_result_spec.rb
@@ -1,32 +1,18 @@
 require 'rails_helper'
 
 describe PendingResult do
-  it "study should have expected data in pending results fields" do
-    nct_id='NCT03446690'
-    xml=Nokogiri::XML(File.read("spec/support/xml_data/#{nct_id}.xml"))
-    study=Study.new({xml: xml, nct_id: nct_id}).create
+  it 'should test documents_data' do
+    expected_data = {
+      nct_id: 'NCT03453554',
+      event: 'RELEASE',
+      event_date_description: '2022-12-23',
+      event_date: Date.parse('2022-12-23')
+    }
 
-    expect(study.pending_results.size).to eq(3)
-    submitted=study.pending_results.select{|x| x.event == 'submitted'}
-    submitted1=submitted.select{|x| x.event_date_description == 'March 16, 2018'}
-    returned=study.pending_results.select{|x| x.event=='returned'}
-    expect(submitted.size).to eq(2)
-    expect(submitted1.size).to eq(1)
-    expect(returned.size).to eq(1)
-    expect(returned.first.event_date).to eq('April 11, 2018'.to_date)
-    expect(submitted1.first.event_date).to eq('March 16, 2018'.to_date)
+    hash = JSON.parse(File.read('spec/support/json_data/NCT03453554.json'))
+    json = StudyJsonRecord::ProcessorV2.new(hash)
+    result = PendingResult.mapper(json)
+
+    expect(result.first).to eq(expected_data)
   end
-
-  it "study should handle strings in the date field" do
-    nct_id='NCT03435185'
-    xml=Nokogiri::XML(File.read("spec/support/xml_data/#{nct_id}.xml"))
-    study=Study.new({xml: xml, nct_id: nct_id}).create
-    expect(study.pending_results.size).to eq(3)
-
-    unknown=study.pending_results.select{|x| x.event_date_description == 'Unknown'}
-    expect(unknown.size).to eq(1)
-    expect(unknown.first.event_date).to eq(nil)
-    expect(unknown.first.event).to eq('submission_canceled')
-  end
-
 end
