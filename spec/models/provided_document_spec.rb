a8e2ab4cf3fd32b5ece12038b3474f14f3b3d037
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-01-10 16:39:26 -0700

    Removed xml mapping in ProvidedDocument and RSpec test file.

--- spec/models/provided_document_spec.rb
+++ spec/models/provided_document_spec.rb
@@ -1,60 +1,31 @@
 require 'rails_helper'
 
-describe ProvidedDocument do
-  it "study should have expected provided_document info" do
-    nct_id='NCT01298141'
-    xml=Nokogiri::XML(File.read("spec/support/xml_data/#{nct_id}.xml"))
-    study=Study.new({xml: xml, nct_id: nct_id}).create
-    expect(study.provided_documents.size).to eq(7)
-    doc = study.provided_documents.select{|x| x.url == 'https://ClinicalTrials.gov/ProvidedDocs/41/NCT01298141/Prot_000.pdf'}.first
-    expect(doc.document_type).to eq('Study Protocol: Original Protocol')
-    expect(doc.document_date.strftime("%Y-%m-%d")).to eq('2010-09-30')
-    expect(doc.has_protocol).to be(true)
-    expect(doc.has_icf).to be(false)
-    expect(doc.has_sap).to be(false)
-  end
-
-end
-
 RSpec.describe ProvidedDocument do
   describe 'ProvidedDocument#mapper' do
     it 'study should have expected provided_document info' do
       expected_data = [
         { 
           nct_id: 'NCT03064438', 
-          document_type: 'United States', 
-          has_protocol: false
-          has_icf: 'NCT02552212', 
-          has_sap: 'Australia', 
-          document_date: false,
-          url: false
+          document_type: 'Study Protocol', 
+          has_protocol: true,
+          has_icf: false, 
+          has_sap: false, 
+          document_date: Date.parse('Sat, 05 Aug 2017'),
+          url: 'https://ClinicalTrials.gov/ProvidedDocs/38/NCT03064438/Prot_000.pdf'
         },
         { 
           nct_id: 'NCT03064438', 
-          document_type: 'United States', 
-          has_protocol: false
-          has_icf: 'NCT02552212', 
-          has_sap: 'Australia', 
-          document_date: false,
-          url: false
+          document_type: 'Statistical Analysis Plan', 
+          has_protocol: false,
+          has_icf: false, 
+          has_sap: true, 
+          document_date: Date.parse('Fri, 11 May 2018'),
+          url: 'https://ClinicalTrials.gov/ProvidedDocs/38/NCT03064438/SAP_001.pdf'
         }
       ]
-      hash = JSON.parse(File.read('spec/support/json_data/NCT02278341.json'))
+      hash = JSON.parse(File.read('spec/support/json_data/NCT03064438.json'))
       processor = StudyJsonRecord::ProcessorV2.new(hash)
-      expect(Country.mapper(processor)).to eq(expected_data)
+      expect(ProvidedDocument.mapper(processor)).to eq(expected_data)
     end
-
-    it 'when contact location country exist and removed country does not exist' do
-      expected_data = [
-        { 
-          nct_id: 'NCT04207047', 
-          name: 'United States', 
-          removed: false
-        }
-      ]  
-      hash = JSON.parse(File.read('spec/support/json_data/NCT04207047.json'))
-      processor = StudyJsonRecord::ProcessorV2.new(hash)
-      expect(Country.mapper(processor)).to eq(expected_data)
-    end    
   end  
 end
