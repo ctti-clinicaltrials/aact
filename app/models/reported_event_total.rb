b8c6414c57ad123d8cef0c62682112d09ebbffda
kostik <kostik@aact.myguest.virtualbox.org>
2024-01-11 13:44:18 -0500

    mapper modified

--- app/models/reported_event_total.rb
+++ app/models/reported_event_total.rb
@@ -1,5 +1,16 @@
 class ReportedEventTotal < ApplicationRecord
     def self.mapper(json)
-        
+        adverse_events_module = json.dig('adverseEventsModule')
+        return [] unless adverse_events_module
+        event_groups = adverse_events_module.dig('eventGroups')
+        return [] unless event_groups
+
+        event_groups.each do |event_group|
+            collection << event_totals('seriousEvents', event_group)
+            collection << event_totals('otherEvents', event_group)
+            # collection << event_totals('Deaths', event_group)
+        end
+        collection
+        # ask Ramiro about nct id, deaths, AACT-620 and common correctness
     end
 end
