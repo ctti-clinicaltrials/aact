8cbc6209bc62d22cfeb21e7e290594a30f487cbb
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-01-19 17:45:00 -0700

    Added RSpec test case.

--- spec/models/sponsor_spec.rb
+++ spec/models/sponsor_spec.rb
@@ -41,49 +41,19 @@ RSpec.describe Sponsor do
       expect(Sponsor.mapper(processor)).to eq(expected_data)
     end
 
-
-    # it 'should return expected Lead sponsor type data' do
-    #   expected_data = [
-    #     { 
-    #       nct_id: "NCT00763412", 
-    #       agency_class: "INDIV", 
-    #       lead_or_collaborator: "lead",
-    #       name: "Arbelaez, Ana Maria"
-    #     }
-    #   ]
-    #   hash = JSON.parse(File.read('spec/support/json_data/NCT00763412.json'))
-    #   processor = StudyJsonRecord::ProcessorV2.new(hash)
-    #   expect(Sponsor.mapper(processor)).to eq(expected_data)
-    # end
-
-    # it 'should return expected Collaborator sponsor type data' do
-    #   expected_data = [
-    #     { 
-    #       nct_id: "NCT00763412", 
-    #       agency_class: "INDIV", 
-    #       lead_or_collaborator: "lead",
-    #       name: "Arbelaez, Ana Maria"
-    #     },
-    #     { 
-    #       nct_id: "NCT00763412", 
-    #       agency_class: "INDIV", 
-    #       lead_or_collaborator: "lead",
-    #       name: "Arbelaez, Ana Maria"
-    #     },
-    #     { 
-    #       nct_id: "NCT03064438", 
-    #       outcome_type: "secondaryoutcomes", 
-    #       measure: "Percentage of Participants Who Were Treatment Responders at Week 12",
-    #       time_frame: "Baseline, Week 12", 
-    #       population: nil, 
-    #       }
-    #   ]
-
-    # it 'should return nil when  data is empty' do
-    #   hash = JSON.parse(File.read('spec/support/json_data/design_outcome_empty.json'))
-    #   processor = StudyJsonRecord::ProcessorV2.new(hash)
-    #   expect(DesignOutcome.mapper(processor)).to eq(nil)  
-    # end
-
-  end  
+    it 'should return Lead sponsor type expected data and no Collaborator data' do
+      expected_data = [
+        { 
+          nct_id: "NCT02552212", 
+          agency_class: "INDUSTRY", 
+          lead_or_collaborator: "lead",
+          name: "UCB BIOSCIENCES GmbH"
+        }
+      ]
+      hash = JSON.parse(File.read('spec/support/json_data/NCT02552212.json'))
+      processor = StudyJsonRecord::ProcessorV2.new(hash)
+      expect(Sponsor.mapper(processor)).to eq(expected_data)
+    end
+  end 
+   
 end
