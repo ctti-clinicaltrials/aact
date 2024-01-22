899025ef7841d4344a4279f6feb12c206301948b
kostik <kostik@aact.myguest.virtualbox.org>
2024-01-22 14:46:14 -0500

    mapper in progres2

--- app/models/id_information.rb
+++ app/models/id_information.rb
@@ -4,6 +4,51 @@ class IdInformation < StudyRelationship
     '//id_info'
   end
 
+  def self.mapper(json)
+    # return unless @protocol_section
+    protocol_sdection = json.dig('protocolSection')
+    return unless protocol_section
+
+    identification_module = protocol_section.dig('identificationModule')
+    return unless identification_module
+
+    nct_id_alias = identification_module.dig('nctIdAliases') || []
+    secondary_info = identification_module.dig('secondaryIdInfos', 'id') || []
+    org_study_info = identification_module['orgStudyIdInfo']
+    collection = []
+    collection << {
+      nct_id: nct_id,
+      id_source: 'org_study_id',
+      id_type: identification_module.dig('OrgStudyIdInfo', "OrgStudyIdType"),
+      id_type_description: identification_module.dig('OrgStudyIdInfo', "OrgStudyIdDomain"),
+      id_link: identification_module.dig('OrgStudyIdInfo', "OrgStudyIdLink"),
+      id_value: org_study_info['OrgStudyId'] 
+      } if org_study_info
+
+    nct_id_alias.each do |nct_alias|
+      collection << { 
+        nct_id: nct_id, 
+        id_source: 'nct_alias', 
+        id_type: nil,
+        id_type_description: nil,
+        id_link: nil,
+        id_value: nct_alias
+      }
+    end
+    secondary_info.each do |info|
+      collection << { 
+        nct_id: nct_id,
+        id_source: 'secondary_id',
+        id_type: info['SecondaryIdType'],
+        id_type_description: info['SecondaryIdDomain'],
+        id_link: info['SecondaryIdLink'],
+        id_value: info['SecondaryId'],
+      }
+    end
+    collection
+  end
+
+
 	def self.id_types
     ['org_study_id','secondary_id','nct_alias']
   end
