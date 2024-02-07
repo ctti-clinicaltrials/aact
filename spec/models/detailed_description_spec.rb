8a26730c459f88c5f5484331d899cdee4dbf7173
Ramiro Rodriguez <ramiro.rodriguez@gmail.com>
2024-02-06 19:17:16 -0800

    add detailed description

--- spec/models/detailed_description_spec.rb
+++ spec/models/detailed_description_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 describe DetailedDescription do
+  it "should create an instance of DetailedDescription" do
+    # load the json
+    content = JSON.parse(File.read('spec/support/json_data/detailed_description.json'))
+    StudyJsonRecord.create(nct_id: "NCT000001", version: '2', content: content) # create a brand new json record
+
+    # process the json
+    StudyJsonRecord::Worker.new.process # import the new json record
+
+    # load the database entries
+    imported = DetailedDescription.all.map{|x| x.attributes }
+    imported.each{|x| x.delete("id")}
+    expect(imported).to eq([{ "nct_id" => "NCT000001", "description" => "Brief Study Summary..." }])
+  end
 end
