7bc5e3d52c1bbda005378b552d267bfdf0406819
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-02-08 22:40:24 -0800

    Updated milestones mapper method to reflect v2 API data names.

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -3,32 +3,32 @@ class Milestone < ApplicationRecord
   def self.mapper(json)
     return unless json.results_section
 
-    flow_periods = json.results_section.dig('ParticipantFlowModule', 'FlowPeriodList', 'FlowPeriod')
-    flow_groups =  json.results_section.dig('ParticipantFlowModule')
+    flow_periods = json.results_section.dig('participantFlowModule', 'periods')
+    flow_groups =  json.results_section['participantFlowModule']
     result_groups = create_and_group_results(flow_groups, 'Flow', 'Participant Flow')
     return unless flow_periods
 
     collection = []
     flow_periods.each do |period|
 
-      flow_period = period['FlowPeriodTitle']
-      flow_milestones = period.dig('FlowMilestoneList', 'FlowMilestone')
+      flow_period = period['title']
+      flow_milestones = period['milestones']
       next unless flow_milestones
 
       flow_milestones.each do |milestone|
-        flow_achievements = milestone.dig('FlowAchievementList', 'FlowAchievement')
+        flow_achievements = milestone['achievements']
         next unless flow_achievements
 
         flow_achievements.each do |achievement|
-          ctgov_group_code = achievement['FlowAchievementGroupId']
+          ctgov_group_code = achievement['groupId']
           collection << {
                           nct_id: nct_id,
                           result_group_id: result_groups[ctgov_group_code].try(:id),
                           ctgov_group_code: ctgov_group_code,
-                          title: milestone['FlowMilestoneType'],
-                          period: period['FlowPeriodTitle'],
-                          description: achievement['FlowAchievementComment'],
-                          count: achievement['FlowAchievementNumSubjects'],
+                          title: milestone['type'],
+                          period: period['title'],
+                          description: achievement['comment'],
+                          count: achievement['numSubjects'],
                           milestone_description: milestone['FlowMilestoneComment'],
                           count_units: achievement['FlowAchievementNumUnits']
                         }
@@ -41,7 +41,7 @@ class Milestone < ApplicationRecord
   end
 
   def create_and_group_results(section, selector='Outcome', result_type='Outcome')
-    groups = (section || {}).dig("#{selector}GroupList", "#{selector}Group") || []
+    groups = (section || {}).["#{selector}groups"] || []
     groups_data = StudyJsonRecord.result_groups(groups, selector, result_type, nct_id)
     result_groups = {}
     groups_data.each do |group|
