ef9e8a80d042c544aa6a55318ace708cc03fcabe
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-02-12 21:31:40 -0800

    Initial commit. Removed documents_data method from ProcessorV2 class. Created add_mapping definition in Document class. Removed create_all_from and collect_docs methods from Document class.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,31 +1,36 @@
 class Document < StudyRelationship
 
-  def self.create_all_from(opts)
-    docs=opts[:xml].xpath('//study_docs')
-    return nil if docs.blank?
-    opts[:docs]=docs.children
-    collect_docs(opts)
+  def documents_data
+    return unless protocol_section
+    nct_id = protocol_section.dig('identificationModule', 'nctId')
+
+    avail_ipds = protocol_section.dig('referencesModule', 'availIpds')
+    return unless avail_ipds
+
+    collection = []
+    avail_ipds.each do |item|
+      collection << {
+                      nct_id: nct_id,
+                      document_id: item['id'],
+                      document_type: item['type'],
+                      url: item['url'],
+                      comment: item['comment']
+                    }
+    end
+    collection
   end
 
-  def self.collect_docs(opts)
-    opts[:docs].each{|doc|
-      id=doc.xpath('doc_id').text
-      type=doc.xpath('doc_type').text
-      url=doc.xpath('doc_url').text
-      comment=doc.xpath('doc_comment').text
-      id=nil if id.blank?
-      type=nil if type.blank?
-      url=nil if url.blank?
-      comment=nil if comment.blank?
-      if !type.blank?
-        create({
-          :nct_id => opts[:nct_id],
-          :document_id   => id,
-          :document_type => type,
-          :url           => url,
-          :comment       => comment,
-        })
-      end
+  add_mapping do
+    {
+      table: :,
+      root: [:, :, :],
+      columns: [
+        { name: :, value: : },
+        { name: :, value: : },
+        { name: :, value: : },
+        { name: :, value: : },
+        { name: :, value: : }
+      ]
     }
   end
 
--- app/models/study_json_record/processor_v2.rb
+++ app/models/study_json_record/processor_v2.rb
@@ -257,26 +257,6 @@ class StudyJsonRecord::ProcessorV2
     collection
   end
 
-  def documents_data
-    return unless protocol_section
-    nct_id = protocol_section.dig('identificationModule', 'nctId')
-
-    avail_ipds = protocol_section.dig('referencesModule', 'availIpds')
-    return unless avail_ipds
-
-    collection = []
-    avail_ipds.each do |item|
-      collection << {
-                      nct_id: nct_id,
-                      document_id: item['id'],
-                      document_type: item['type'],
-                      url: item['url'],
-                      comment: item['comment']
-                    }
-    end
-    collection
-  end
-
   def facilities_data
   end
 
