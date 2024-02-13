e5e346d10245eab44d924963a8df5131298dfff6
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-02-13 11:53:23 -0800

    Updated add_mapping definition.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,35 +1,14 @@
 class Document < StudyRelationship
 
-  def documents_data
-    return unless protocol_section
-    nct_id = protocol_section.dig('identificationModule', 'nctId')
-
-    avail_ipds = protocol_section.dig('referencesModule', 'availIpds')
-    return unless avail_ipds
-
-    collection = []
-    avail_ipds.each do |item|
-      collection << {
-                      nct_id: nct_id,
-                      document_id: item['id'],
-                      document_type: item['type'],
-                      url: item['url'],
-                      comment: item['comment']
-                    }
-    end
-    collection
-  end
-
   add_mapping do
     {
-      table: :,
-      root: [:, :, :],
+      table: :documents,
+      root: [:protocolSection, :referencesModule, :availIpds],
       columns: [
-        { name: :, value: : },
-        { name: :, value: : },
-        { name: :, value: : },
-        { name: :, value: : },
-        { name: :, value: : }
+        { name: :document_id, value: :id },
+        { name: :document_type, value: :type },
+        { name: :url, value: :url },
+        { name: :comment, value: :comment }
       ]
     }
   end
