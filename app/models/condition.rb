6725527979037dd07ff31b9e07b49a02b23832ab
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-02-16 12:14:54 -0800

    Updated add_mapping defintion in the Condition model. Created RSpec test files.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,21 +1,14 @@
 class Condition < StudyRelationship
 
-  def conditions_data
-    return unless protocol_section
-
-    nct_id = protocol_section.dig('identificationModule', 'nctId')
-
-    conditions_module = protocol_section['conditionsModule']
-    return unless conditions_module
-
-    conditions = conditions_module.dig('conditions')
-    return unless conditions
-
-    collection = []
-    conditions.each do |condition|
-      collection << { nct_id: nct_id, name: condition, downcase_name: condition.try(:downcase) }
-    end
-    collection
+  add_mapping do
+    {
+      table: :conditions,
+      root: [:protocolSection, :conditionsModule, :conditions],
+      columns: [
+        { name: :name, value: :conditions },
+        { name: :downcase_name, value: :conditions.try(:downcase) }
+      ]
+    }
   end
 
 end
--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -1,4 +1,26 @@
 require 'rails_helper'
 
 describe Condition do
+  it "should create an instance of Condition" do
+    expected_data = [
+      {
+        "nct_id" => "NCT000001",
+        "name" => ["Axial Spondyloarthritis", "Nonradiographic Axial Spondyloarthritis", "Nr-axSpA"],
+        "downcase_name" => ["axial spondyloarthritis", "nonradiographic axial spondyloarthritis", "nr-axspa"]
+      }
+    ]  
+
+    # load the json
+    content = JSON.parse(File.read('spec/support/json_data/condition.json'))
+    StudyJsonRecord.create(nct_id: "NCT000001", version: '2', content: content) # create a brand new json record
+
+    # process the json
+    StudyJsonRecord::Worker.new.process # import the new json record
+
+    # load the database entries
+    imported = Condition.all.map{|x| x.attributes }
+    imported.each{|x| x.delete("id")}
+    expect(imported).to eq(expected_data)
+  end
 end
+
