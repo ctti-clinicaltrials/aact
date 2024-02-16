3ceb4447ce0569e3ee6112da2005bb30434b8947
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-02-16 12:49:08 -0800

    Initial commit. Removed old code for mapping JSON, links_data method, from ProcessorV2 model. Created add_mapping definition in the Link model. Removed old code for importing using xml from Link model: methods top_level_label, create_all_from and attribs.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,17 +1,29 @@
 class Link < StudyRelationship
 
-  def self.top_level_label
-    '//link'
-  end
+  # def links_data
+  #   return unless protocol_section
 
-  def self.create_all_from(opts)
-    objects = super
-    import(objects)
-  end
+  #   nct_id = protocol_section.dig('identificationModule', 'nctId')
+  #   see_also_links = protocol_section.dig('referencesModule', 'seeAlsoLinks')
+  #   return unless see_also_links
 
-  def attribs
-    {:url=>get('url'),
-     :description => get('description')}
+  #   collection = []
+  #   see_also_links.each do |link|
+  #     collection << { nct_id: nct_id, url: link['url'], description: link['label'] }
+  #   end
+    
+  #   collection
+  # end
+
+  add_mapping do
+    {
+      table: ,
+      root: [],
+      columns: [
+        { name: , value:  },
+        { name: , value:  }
+      ]
+    }
   end
 
 end
--- app/models/study_json_record/processor_v2.rb
+++ app/models/study_json_record/processor_v2.rb
@@ -293,21 +293,6 @@ class StudyJsonRecord::ProcessorV2
     collection
   end
 
-  def links_data
-    return unless protocol_section
-
-    nct_id = protocol_section.dig('identificationModule', 'nctId')
-    see_also_links = protocol_section.dig('referencesModule', 'seeAlsoLinks')
-    return unless see_also_links
-
-    collection = []
-    see_also_links.each do |link|
-      collection << { nct_id: nct_id, url: link['url'], description: link['label'] }
-    end
-    
-    collection
-  end
-
   def milestones_data
   end
 
