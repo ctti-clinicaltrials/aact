7cf4f3cffc4d7dd8c89ba82ed40e6c162aae117e
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-02-22 16:56:39 -0800

    Updated RSpec test file with schema version 2 and added comment.

--- spec/models/ipd_information_type_spec.rb
+++ spec/models/ipd_information_type_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe IpdInformationType do
-  it "should create an instance of Document" do
+  it "should create an instance of Document", schema: :v2 do
     expected_data = [
       {
         "nct_id" => "NCT000001",
@@ -27,6 +27,8 @@ describe IpdInformationType do
     # load the database entries
     imported = IpdInformationType.all.map{|x| x.attributes }
     imported.each{|x| x.delete("id")}
+
+    # Compare the modified imported data with the expected data
     expect(imported).to eq(expected_data)
   end
 end
