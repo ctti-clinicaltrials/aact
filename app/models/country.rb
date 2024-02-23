7eee36dd43dd90f832308fd58af604e646d86cee
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-02-23 10:15:27 -0800

    Initial commit. Changed ApplicationRecord to StudyRelationship class in the Country model. Removed old JSON mapper method.

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,29 +1,32 @@
-class Country < ApplicationRecord
+class Country < StudyRelationship
 
-  def self.mapper(json)
-    return unless json.derived_section
+  # def self.mapper(json)
+  #   return unless json.derived_section
 
-    nct_id = json.protocol_section.dig('identificationModule', 'nctId')
-    removed_countries = json.derived_section.dig('miscInfoModule', 'removedCountries') || []
-    locations = json.locations_array || []
-    return if locations.empty? && removed_countries.empty?
+  #   nct_id = json.protocol_section.dig('identificationModule', 'nctId')
+  #   removed_countries = json.derived_section.dig('miscInfoModule', 'removedCountries') || []
+  #   locations = json.locations_array || []
+  #   return if locations.empty? && removed_countries.empty?
   
-    countries = []
-    collection = []
+  #   countries = []
+  #   collection = []
   
-    locations.each do |location|
-      countries << location['country']
-    end
+  #   locations.each do |location|
+  #     countries << location['country']
+  #   end
   
-    countries.uniq.each do |country|
-      collection << { nct_id: nct_id, name: country, removed: false }
-    end
+  #   countries.uniq.each do |country|
+  #     collection << { nct_id: nct_id, name: country, removed: false }
+  #   end
   
-    removed_countries.uniq.each do |country|
-      collection << { nct_id: nct_id, name: country, removed: true }
-    end
+  #   removed_countries.uniq.each do |country|
+  #     collection << { nct_id: nct_id, name: country, removed: true }
+  #   end
   
-    collection
-  end
+  #   collection
+  # end
+
+  add_mapping do
+  end  
 
 end
