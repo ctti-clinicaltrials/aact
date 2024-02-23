05fa3aac7181e207d0d5f24f9b70ebbceb55b954
Ievgen Dotsenko <66342665+ievgend2@users.noreply.github.com>
2024-02-23 12:15:49 -0500

    refactored responsible_party mapping

--- app/models/responsible_party.rb
+++ app/models/responsible_party.rb
@@ -1,43 +1,17 @@
-class ResponsibleParty < ApplicationRecord
+class ResponsibleParty < StudyRelationship
 
-  def self.top_level_label
-    '//responsible_party'
+  add_mapping do
+    {
+      table: :responsible_party,
+      root: [:protocolSection, :sponsorCollaboratorsModule, :responsibleParty],
+      columns: [
+        { name: :responsible_party_type, value: :type },
+        { name: :name, value: :investigatorFullName},
+        { name: :title, value: :investigatorTitle},
+        { name: :affiliation, value: :investigatorAffiliation },
+        { name: :old_name_title, value: :oldNameTitle },
+        { name: :organization, value: :oldOrganization},
+      ]
+    }
   end
-
-  def self.create_all_from(opts)
-    objects = super
-    import(objects)
-  end
-
-  def name_field(opts)
-    if opts[:xml].xpath('name_title').present?
-      return get('name_title')
-    else
-      return get('investigator_full_name')
-    end
-  end
-
-  def self.mapper(json)
-    # checks for protocol_section
-    return unless json.protocol_section
-    nct_id = json.protocol_section.dig('identificationModule', 'nctId')
-    responsible_party = json.protocol_section.dig('sponsorCollaboratorsModule', 'responsibleParty')
-    # checks for responsible party
-    return unless responsible_party
-
-      {
-        nct_id: nct_id,
-        responsible_party_type: responsible_party['type'],
-        name: responsible_party['investigatorFullName'],
-        title: responsible_party['investigatorTitle'],
-        organization: responsible_party['leadSponsor'],
-        affiliation: responsible_party['investigatorAffiliation']
-      }
-  end
-
-  # def get_name
-  #   n=get('investigator_full_name')
-  #   !n.blank? ? n : get('name_title')
-  # end
-
 end
