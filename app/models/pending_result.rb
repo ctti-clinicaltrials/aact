b7b0a273c388363b3ba61a6b3f21acca8de2c3f7
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-02-26 14:44:51 -0800

    Initial commit. Changed ApplicationRecord to StudyRelationship class in the PendingResult model. Removed old JSON mapper method.

--- app/models/pending_result.rb
+++ app/models/pending_result.rb
@@ -1,26 +1,36 @@
-class PendingResult < ApplicationRecord
-  enum unposted_event_type: {
-    RELEASE: 'RELEASE',
-    RESET: 'RESET',
-    UNRELEASE: 'UNRELEASE'
+class PendingResult < StudyRelationship
+# enum unposted_event_type: {
+#   RELEASE: 'RELEASE',
+#   RESET: 'RESET',
+#   UNRELEASE: 'UNRELEASE'
+# }
+
+#   def self.mapper(json)
+#     return unless json.annotation_section
+#     nct_id = json.protocol_section.dig('identificationModule', 'nctId')
+
+#     unposted_events = json.annotation_section.dig('annotationModule', 'unpostedAnnotation', 'unpostedEvents')
+#     return unless unposted_events
+
+#     collection = []
+#     unposted_events.each do |event|
+#       collection << {
+#         nct_id: nct_id,
+#         event: unposted_event_types[event['type']],
+#         event_date_description: event['date'],
+#         event_date: get_date(event['date'])
+#       }
+#     end
+#     collection
+#   end
+# end
+
+add_mapping do
+  {
+    table: :pending_results,
+    root: [:annotationSection, :, :],
+    columns: [
+      { name: :, value:  }
+    ]
   }
-
-  def self.mapper(json)
-    return unless json.annotation_section
-    nct_id = json.protocol_section.dig('identificationModule', 'nctId')
-
-    unposted_events = json.annotation_section.dig('annotationModule', 'unpostedAnnotation', 'unpostedEvents')
-    return unless unposted_events
-
-    collection = []
-    unposted_events.each do |event|
-      collection << {
-        nct_id: nct_id,
-        event: unposted_event_types[event['type']],
-        event_date_description: event['date'],
-        event_date: get_date(event['date'])
-      }
-    end
-    collection
-  end
 end
