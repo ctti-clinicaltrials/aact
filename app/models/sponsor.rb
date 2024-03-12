c480af1044e78282da495afe6c76617e921323d6
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-03-12 13:46:08 -0700

    Modified add_mapping definition and added RSpec JSON data file.

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -36,15 +36,26 @@ class Sponsor < ApplicationRecord
   # end
 
   add_mapping do
-    {
-      table: :sponsors,
-      root: [:protocolSection, :sponsorCollaboratorsModule],
-      columns: [
-        { name: :agency_class, value: :class },
-        { name: :lead_or_collaborator, value: :sponsor_type =~ /Lead/i ? 'lead' : 'collaborator' },
-        { name: :name, value: :name }
-      ]
-    }
+    [
+      {
+        table: :sponsors,
+        root: [:protocolSection, :sponsorCollaboratorsModule, :leadSponsor],
+        columns: [
+          { name: :agency_class, value: :class },
+          { name: :lead_or_collaborator, value: 'lead' },
+          { name: :name, value: :name }
+        ]
+      },
+      {
+        table: :sponsors,
+        root: [:protocolSection, :sponsorCollaboratorsModule, :collaborators],
+        columns: [
+          { name: :agency_class, value: :class },
+          { name: :lead_or_collaborator, value: 'collaborator' },
+          { name: :name, value: :name }
+        ]
+      }
+    ]
   end  
 
 end
