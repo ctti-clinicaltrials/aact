d03966bd893572377d1a9c4314cf1621149f4ba7
kholtzman5610 <kassidy.holtzman@outlook.com>
2024-04-03 11:14:32 -0400

    Pushing code for debug

--- app/models/study_reference.rb
+++ app/models/study_reference.rb
@@ -1,12 +1,23 @@
-class StudyReference < StudyRelationship  
-	add_mapping do
+class StudyReference < StudyRelationship
+  has_many :retractions, foreign_key: :reference_id 
+  add_mapping do
     {
       table: :study_references,
       root: [:protocolSection, :referencesModule, :references],
       columns: [
-        { name: :pmid, value: :pmid},
-        { name: :reference_type, value: :type},
-        { name: :citation, value: :citation}
+        { name: :pmid, value: :pmid },
+        { name: :reference_type, value: :type },
+        { name: :citation, value: :citation }
+      ],
+      children: [
+        {
+          table: :retractions,
+          root: [:retractions],
+          columns: [
+            { name: :pmid, value: :pmid },
+            { name: :source, value: :source }
+          ]
+        }
       ]
     }
   end
