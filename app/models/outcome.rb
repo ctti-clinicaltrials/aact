3cf10d37f80a6d270747ad9f7d904dd636fbf942
Javier-Jimenez-18 <javier.jimenez.engineering@gmail.com>
2024-04-10 18:18:09 -0700

    Added child mapping for table outcome_analyses to model Outcome.

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -19,6 +19,37 @@ class Outcome < StudyRelationship
         { name: :units_analyzed, value: :typeUnitsAnalyzed },
         { name: :dispersion_type, value: :dispersionType },
         { name: :param_type, value: :paramType },
+      ],
+      children: [
+        {
+          table: :outcome_analyses,
+          root: [:analyses],
+          columns: [
+            { name: :outcome_id, value: nil },
+            { name: :non_inferiority_type, value: nil },
+            { name: :non_inferiority_description, value: nil },
+            { name: :param_type, value: nil },
+            { name: :param_value, value: nil },
+            { name: :dispersion_type, value: nil },
+            { name: :dispersion_value, value: nil },
+            { name: :p_value_modifier, value: nil },
+            { name: :p_value, value: nil },
+            { name: :p_value_raw, value: nil },
+            { name: :p_value_description, value: nil },
+            { name: :ci_n_sides, value: nil },
+            { name: :ci_percent, value: nil },
+            { name: :ci_lower_limit, value: nil },
+            { name: :ci_upper_limit, value: nil },
+            { name: :ci_lower_limit_raw, value: nil },
+            { name: :ci_upper_limit_raw, value: nil },
+            { name: :ci_upper_limit_na_comment, value: nil },
+            { name: :method, value: nil },
+            { name: :method_description, value: nil },
+            { name: :estimate_description, value: nil },
+            { name: :groups_description, value: nil },
+            { name: :other_analysis_description, value: nil }
+          ]
+        }
       ]
     }
   end
