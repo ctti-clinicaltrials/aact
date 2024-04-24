c6dd3febce021218bd7ef2677a0bff2345ec229c
akabishau <aleksey.kabishau@icloud.com>
2024-04-23 23:43:45 -0400

    add mapping for "baseline" group type

--- app/models/result_group.rb
+++ app/models/result_group.rb
@@ -3,6 +3,7 @@ class ResultGroup < StudyRelationship
   has_many :reported_events, autosave: true
   has_many :milestones, autosave: true
   has_many :drop_withdrawals, autosave: true
+  has_many :baseline_counts, autosave: true
   has_many :baseline_measures, autosave: true
   has_many :outcome_counts, autosave: true
   has_many :outcome_measurements, autosave: true
@@ -10,6 +11,9 @@ class ResultGroup < StudyRelationship
   has_many :outcome_analyses, :through => :outcome_analysis_groups
 
   add_mapping do
+    # TODO: Update topological sort to handle array of mapping objects (study_relationship.rb line 265)
+
+=begin
     {
       table: :result_groups,
       root: [:resultsSection, :participantFlowModule, :groups],
@@ -17,6 +21,19 @@ class ResultGroup < StudyRelationship
       columns: [
         { name: :ctgov_group_code, value: :id },
         { name: :result_type, value: 'Participant Flow' },
+        { name: :title, value: :title },
+        { name: :description, value: :description },
+      ]
+    }
+=end
+
+    {
+      table: :result_groups,
+      root: [:resultsSection, :baselineCharacteristicsModule, :groups],
+      index: [:ctgov_group_code, :result_type], 
+      columns: [
+        { name: :ctgov_group_code, value: :id },
+        { name: :result_type, value: 'Baseline' },
         { name: :title, value: :title },
         { name: :description, value: :description },
       ]
