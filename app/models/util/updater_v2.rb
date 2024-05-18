e15382534dcbcc43dee212b1df50c59d89af6171
akabishau <aleksey.kabishau@icloud.com>
2024-05-17 22:44:59 -0400

    add logic for getting current studies from the ctgov_v2 schema

--- app/models/util/updater_v2.rb
+++ app/models/util/updater_v2.rb
@@ -5,11 +5,10 @@ module Util
     # TODO: add tests for refactored method
     def current_study_differences
       api_studies = ClinicalTrialsApiV2.all
-      puts "aact  study count: #{Study.count}"
+      result = ActiveRecord::Base.connection.execute("SELECT nct_id, last_update_posted_date FROM ctgov_v2.studies")
+      puts "aact study count: #{result.count}"
       puts "ctgov study count: #{api_studies.count}"
-
-      current_studies = Hash[Study.pluck(:nct_id, :last_update_posted_date)]
-
+      current_studies = Hash[result.map { |record| [record['nct_id'], record['last_update_posted_date']] }]
       studies_to_update = api_studies.select do |api_study|
         current_study_update_date = current_studies[api_study[:nct_id]]
         current_study_update_date.nil? || Date.parse(api_study[:updated]) > current_study_update_date
@@ -19,9 +18,11 @@ module Util
       studies_to_remove = current_studies.keys.to_set - api_studies.map { |study| study[:nct_id] }.to_set
       puts "update: #{studies_to_update.take(10)}"
       puts "remove: #{studies_to_remove.take(10)}"
-      byebug
+      puts "result: #{result.take(10)}"
       return [api_studies, studies_to_update, studies_to_remove]
+    rescue => e
+      puts "An error occurred: #{e.message}"
     end
-
   end
+
 end
