acb32be655f9f61c8888fcddb5a64de5e9c0493f
akabishau <aleksey.kabishau@icloud.com>
2024-05-30 23:08:51 -0400

    fix indentation; add placeholder for missing steps

--- app/models/util/updater_v2.rb
+++ app/models/util/updater_v2.rb
@@ -41,59 +41,72 @@ module Util
     def execute
       with_search_path('ctgov_v2, support, public') do
 
-      log("#{@schema}: EXECUTE started")
+        log("#{@schema}: EXECUTE started")
 
-      @load_event = Support::LoadEvent.create({ event_type: @type, status: 'running', description: "#{@schema}", problems: '' })
+        @load_event = Support::LoadEvent.create({ event_type: @type, status: 'running', description: "#{@schema}", problems: '' })
 
-      ActiveRecord::Base.logger = nil # why are we disabling logger here?
+        ActiveRecord::Base.logger = nil # why are we disabling logger here?
 
-      # 1. remove constraings
-      log("#{@schema}: removing constraints...")
-      db_mgr.remove_constraints
-      @load_event.log("1/11 removed constraints")
-
-
-      # 2. update studies
-      log("#{@schema}: updating studies...")
-      update_studies
-      @load_event.log("2/11 updated studies")
+        # 1. remove constraings
+        log("#{@schema}: removing constraints...")
+        db_mgr.remove_constraints
+        @load_event.log("1/11 removed constraints")
 
 
-      # 3. add constraints
-      log("#{@schema}: adding constraints...")
-      db_mgr.add_constraints
-      @load_event.log("3/11 added constraints")
+        # 2. update studies
+        log("#{@schema}: updating studies...")
+        update_studies
+        @load_event.log("2/11 updated studies")
 
 
-      # 4. comparing the counts from CT.gov to our database
-      log("#{@schema}: comparing counts...")
-      @load_event.log("4/11 skipped verification")
+        # 3. add constraints
+        log("#{@schema}: adding constraints...")
+        db_mgr.add_constraints
+        @load_event.log("3/11 added constraints")
 
 
-      # 5. run study searches
-      log("#{@schema}: execute study search...")
-      @load_event.log("5/11 skipped study searches")
+        # 4. comparing the counts from CT.gov to our database
+        log("#{@schema}: comparing counts...")
+        @load_event.log("4/11 skipped verification")
 
 
-      # 6. update calculated values
-      log("#{@schema}: update calculated values...")
-      CalculatedValue.populate(@schema)
-      @load_event.log("6/11 updated calculated values")
+        # 5. run study searches
+        log("#{@schema}: execute study search...")
+        @load_event.log("5/11 skipped study searches")
 
 
-      # 7. populate the meshterms and meshheadings
-      log("#{@schema}: update mesh terms and headings...")
-      MeshTerm.populate_from_file
-      MeshHeading.populate_from_file
-      set_downcase_terms
-      @load_event.log("7/11 populated mesh terms")
+        # 6. update calculated values
+        log("#{@schema}: update calculated values...")
+        CalculatedValue.populate(@schema)
+        @load_event.log("6/11 updated calculated values")
 
 
-      # 8. run sanity checks
-      log("#{@schema}: run sanity checks...")
-      @load_event.run_sanity_checks(@schema)
-      @load_event.log("8/11 ran sanity checks")
+        # 7. populate the meshterms and meshheadings
+        log("#{@schema}: update mesh terms and headings...")
+        MeshTerm.populate_from_file
+        MeshHeading.populate_from_file
+        set_downcase_terms
+        @load_event.log("7/11 populated mesh terms")
 
+
+        # 8. run sanity checks
+        log("#{@schema}: run sanity checks...")
+        @load_event.run_sanity_checks(@schema)
+        @load_event.log("8/11 ran sanity checks")
+
+        if load_even.sanity_checks.count == 0
+          puts "SANITY CHECKS PASSED"
+           # 9. take snapshot
+
+           # 10. refresh public db
+
+           # 10. create flat files
+        end
+
+        # 11. change the state of the load event from “running” to “complete”
+        @load_event.update({ status: "complete", completed_at: Time.now})
+
+        # 12. import study records
       end
     rescue => e
       # set the load event status to "error"
