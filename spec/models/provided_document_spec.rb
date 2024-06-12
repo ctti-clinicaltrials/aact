fe808f9ce4ae822f211d219d29f28768129cb576
akabishau <aleksey.kabishau@icloud.com>
2024-06-12 10:39:10 -0400

    refactor: improve readability and fix indentation

--- spec/models/provided_document_spec.rb
+++ spec/models/provided_document_spec.rb
@@ -1,25 +1,18 @@
 require "rails_helper"
 
-
 describe ProvidedDocument do
   include SchemaSwitcher
 
   NCT_ID = "NCT03064438"
-
-  json_files = [
-    "provided_document.json"
-  ]
+  json_files = ["provided_document.json"]
 
   json_files.each do |json_file|
     context "When importing data from #{json_file}" do
-      
       let(:content) { JSON.parse(File.read("spec/support/json_data/#{json_file}")) }
       let(:large_docs) { content["documentSection"]["largeDocumentModule"]["largeDocs"] }
       let(:expected_data) { get_expected_data(large_docs) }
 
-      # byebug
       before do
-        @large_docs = content["documentSection"]["largeDocumentModule"]["largeDocs"]
         StudyJsonRecord.create(nct_id: NCT_ID, version: "2", content: content)
         StudyJsonRecord::Worker.new.process_study(NCT_ID)
       end
@@ -31,22 +24,24 @@ describe ProvidedDocument do
           expect(subject).to eq(expected_data)
         end
       end
-
-
-      describe ".build_document_url" do
-        let(:nct_id) { NCT_ID }
-        let(:filename) { "SAP_001.pdf" }
-        let(:expected_url) { "https://ClinicalTrials.gov/ProvidedDocs/38/NCT03064438/SAP_001.pdf" }
-
-        it "constructs the document URL correctly" do
-          subject { ProvidedDocument.build_document_url(nct_id, filename) }
-          expect(subject).to eq(expected_url)
-        end
-      end
     end
   end
 
-  
+  context "Document URLs" do
+    let(:nct_id) { NCT_ID }
+    let(:filename) { "SAP_001.pdf" }
+    let(:expected_url) { "https://ClinicalTrials.gov/ProvidedDocs/38/NCT03064438/SAP_001.pdf" }
+
+    describe ".build_document_url" do
+      subject { ProvidedDocument.build_document_url(nct_id, filename) }
+
+      it "constructs the document URL correctly" do
+        expect(subject).to eq(expected_url)
+      end
+    end
+
+    # TODO: add test to make sure that the URL is accessible
+  end
 
   private
 
@@ -55,12 +50,11 @@ describe ProvidedDocument do
     documents.map do |document|
       {
         nct_id: NCT_ID,
-        document_type: document["label"], 
+        document_type: document["label"],
         has_protocol: document["hasProtocol"],
-        has_icf: document["hasIcf"], 
-        has_sap: document["hasSap"], 
+        has_icf: document["hasIcf"],
+        has_sap: document["hasSap"],
         document_date: Date.parse(document["date"]),
-        # url: "#{BASE_URL}#{NCT_ID[-2..-1]}/#{NCT_ID}/#{document["filename"]}"
         url: ProvidedDocument.build_document_url(NCT_ID, document["filename"])
       }
     end
