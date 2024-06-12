ee76d21749eb17a9adc64fa008e63db04641beb3
akabishau <aleksey.kabishau@icloud.com>
2024-06-12 09:10:36 -0400

    update tests to support new mapping and ctgov_v2 schema

--- app/models/provided_document.rb
+++ app/models/provided_document.rb
@@ -14,33 +14,4 @@ class ProvidedDocument < StudyRelationship
       ]
     }
   end
-
-
-  def self.mapper(json)
-    return unless json.document_section
-
-    large_docs = json.document_section.dig('largeDocumentModule', 'largeDocs')
-    return unless large_docs
-
-    nct_id = json.protocol_section.dig('identificationModule', 'nctId')
-    collection = []
-    large_docs.each do |doc|
-      base_url = 'https://ClinicalTrials.gov/ProvidedDocs/'
-      number = "#{nct_id[-2]}#{nct_id[-1]}/#{nct_id}"
-      full_url = base_url + number + "/#{doc['filename']}" if doc['filename']
-
-      collection << {
-                      nct_id: nct_id,
-                      document_type: doc['label'],
-                      has_protocol: get_boolean(doc['hasProtocol']),
-                      has_icf: get_boolean(doc['hasIcf']),
-                      has_sap: get_boolean(doc['hasSap']),
-                      document_date: convert_to_date(doc['date']),
-                      url: full_url
-                    }
-    end
-
-    collection
-  end
-
 end
--- spec/models/provided_document_spec.rb
+++ spec/models/provided_document_spec.rb
@@ -1,31 +1,63 @@
 require 'rails_helper'
+import SchemaSwitcher
 
-RSpec.describe ProvidedDocument do
-  describe 'ProvidedDocument#mapper' do
-    it 'study should have expected provided_document info' do
-      expected_data = [
-        { 
-          nct_id: 'NCT03064438', 
-          document_type: 'Study Protocol', 
-          has_protocol: true,
-          has_icf: false, 
-          has_sap: false, 
-          document_date: Date.parse('Sat, 05 Aug 2017'),
-          url: 'https://ClinicalTrials.gov/ProvidedDocs/38/NCT03064438/Prot_000.pdf'
-        },
-        { 
-          nct_id: 'NCT03064438', 
-          document_type: 'Statistical Analysis Plan', 
-          has_protocol: false,
-          has_icf: false, 
-          has_sap: true, 
-          document_date: Date.parse('Fri, 11 May 2018'),
-          url: 'https://ClinicalTrials.gov/ProvidedDocs/38/NCT03064438/SAP_001.pdf'
-        }
-      ]
-      hash = JSON.parse(File.read('spec/support/json_data/NCT03064438.json'))
-      processor = StudyJsonRecord::ProcessorV2.new(hash)
-      expect(ProvidedDocument.mapper(processor)).to eq(expected_data)
+describe ProvidedDocument do
+  
+
+  NCT_ID = "NCT03064438"
+  BASE_URL = "https://ClinicalTrials.gov/ProvidedDocs/"
+
+  json_files = [
+    # TODO: use full study data
+    "provided_document.json"
+  ]
+
+  json_files.each do |json_file|
+    context "When importing data from #{json_file}" do
+      
+      let(:content) { JSON.parse(File.read("spec/support/json_data/#{json_file}")) }
+
+      # byebug
+      before do
+        @large_docs = content["documentSection"]["largeDocumentModule"]["largeDocs"]
+        StudyJsonRecord.create(nct_id: NCT_ID, version: "2", content: content)
+        StudyJsonRecord::Worker.new.process_study(NCT_ID)
+      end
+
+      describe 'ProvidedDocument' do
+        it 'study should have expected provided_document info' do
+          puts "expected_data: #{expected_data}"
+          puts "import(ProvidedDocument): #{import(ProvidedDocument)}"
+          expect(expected_data).to eq(import(ProvidedDocument))
+        end
+      end
+      
     end
-  end  
+  end
+
+  
+
+  private
+
+  def expected_data
+    # byebug
+    return unless @large_docs
+    
+
+    @large_docs.map do |document|
+      {
+        nct_id: NCT_ID,
+        document_type: document["label"], 
+        has_protocol: document["hasProtocol"],
+        has_icf: document["hasIcf"], 
+        has_sap: document["hasSap"], 
+        document_date: Date.parse(document["date"]),
+        url: "#{BASE_URL}#{NCT_ID[-2..-1]}/#{NCT_ID}/#{document["filename"]}"
+      }
+    end
+  end
+
+  def import(model)
+    with_v2_schema { model.all.map { |x| x.attributes.except("id").symbolize_keys } }
+  end
 end
