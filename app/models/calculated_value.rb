0b4df578a525c1ff378aa72937bfd9bab0c5726f
akabishau <aleksey.kabishau@icloud.com>
2024-07-02 23:04:34 -0400

    refactor to use instance variables and add basic error handling

--- app/models/calculated_value.rb
+++ app/models/calculated_value.rb
@@ -2,19 +2,26 @@ class CalculatedValue < ActiveRecord::Base
   belongs_to :study, :foreign_key => 'nct_id'
 
   def self.populate_for(studies)
-    nct_ids = studies.map { |r| r[:nct_id] }
-    calculations = initialize_calculations(nct_ids)
-    perform_calculations(nct_ids, calculations)
-    remove_existing_records(nct_ids)
-    insert_calculated_values(calculations)
+    @nct_ids = studies.map { |r| r[:nct_id] }.freeze
+    @calculations = initialize_calculations
+
+    begin
+      ActiveRecord::Base.transaction do
+        perform_calculations
+        refresh_calculated_values
+      end
+    rescue StandardError => e
+      puts "Calculated Error: #{e.message}"
+      raise ActiveRecord::Rollback
+    end
   end
 
 
   private
 
   
-  def self.initialize_calculations(nct_ids)
-    nct_ids.each_with_object({}) do |nct_id, hash| 
+  def self.initialize_calculations
+    @nct_ids.each_with_object({}) do |nct_id, hash| 
       hash[nct_id] = {
         nct_id: nct_id,
         nlm_download_date: nil, # TODO: remove from table - not used
@@ -39,94 +46,77 @@ class CalculatedValue < ActiveRecord::Base
   end
 
 
-  def self.insert_calculated_values(calculations)
-    columns = [
-      :nct_id, :were_results_reported,
-      :number_of_facilities, :has_single_facility, :has_us_facility,
-      :minimum_age_num, :minimum_age_unit, :maximum_age_num, :maximum_age_unit,
-      :months_to_report_results, :actual_duration, :registered_in_calendar_year,
-      :number_of_sae_subjects, :number_of_nsae_subjects,
-      :number_of_primary_outcomes_to_measure, :number_of_secondary_outcomes_to_measure, :number_of_other_outcomes_to_measure,
-    ]
-    CalculatedValue.import columns, calculations.values
+  def self.refresh_calculated_values
+    CalculatedValue.where(nct_id: @nct_ids).delete_all
+    CalculatedValue.import @calculations.values.first.keys, @calculations.values
   end
 
 
-  # TODO: do calculations first and package this as one thing with insert
-  def self.remove_existing_records(nct_ids)
-    CalculatedValue.where(nct_id: nct_ids).delete_all
-  end
-
-
-  def self.perform_calculations(nct_ids, calculations)
-    start_time = Time.now
-    process_were_results_reported(nct_ids, calculations)
-    process_facility_info(nct_ids, calculations)
-    process_age_info(nct_ids, calculations)
-    process_outcome_design_counts(nct_ids, calculations)
-    process_event_subject_counts(nct_ids, calculations)
-    process_dates(nct_ids, calculations)
-    end_time = Time.now
-    duration = end_time - start_time
-    puts "#perform_calculations executed in #{duration} seconds".green
+  def self.perform_calculations
+    process_were_results_reported
+    process_facility_info
+    process_age_info
+    process_outcome_design_counts
+    process_event_subject_counts
+    process_dates
   end
 
 
   # TODO: use has_results api single property instead
-  def self.process_were_results_reported(nct_ids, calculations)
-    results = Outcome.where(nct_id: nct_ids).distinct.pluck(:nct_id)
-    results.each { |nct_id| calculations[nct_id][:were_results_reported] = true }
+  def self.process_were_results_reported
+    results = Outcome.where(nct_id: @nct_ids).distinct.pluck(:nct_id)
+    results.each { |nct_id| @calculations[nct_id][:were_results_reported] = true }
   end
 
 
-  def self.process_facility_info(nct_ids, calculations)
-    facility_counts = Facility.facility_counts(nct_ids)
-    us_facilities = Facility.us_facility_nct_ids(nct_ids)
+  def self.process_facility_info
+    facility_counts = Facility.facility_counts(@nct_ids)
+    us_facilities = Facility.us_facility_nct_ids(@nct_ids)
 
     facility_counts.each do | nct_id, count |
-      calculations[nct_id][:number_of_facilities] = count
-      calculations[nct_id][:has_single_facility] = (count == 1)
-      calculations[nct_id][:has_us_facility] = us_facilities.include?(nct_id)
+      @calculations[nct_id][:number_of_facilities] = count
+      @calculations[nct_id][:has_single_facility] = (count == 1)
+      @calculations[nct_id][:has_us_facility] = us_facilities.include?(nct_id)
     end
   end
 
 
-  def self.process_age_info(nct_ids, calculations)
-    results = Eligibility.age_values(nct_ids)
+  def self.process_age_info
+    results = Eligibility.age_values(@nct_ids)
     results.each do |study|
-      calculations[study.nct_id][:minimum_age_num] = study.minimum_age_num
-      calculations[study.nct_id][:minimum_age_unit] = study.minimum_age_unit
-      calculations[study.nct_id][:maximum_age_num] = study.maximum_age_num
-      calculations[study.nct_id][:maximum_age_unit] = study.maximum_age_unit
+      @calculations[study.nct_id][:minimum_age_num] = study.minimum_age_num
+      @calculations[study.nct_id][:minimum_age_unit] = study.minimum_age_unit
+      @calculations[study.nct_id][:maximum_age_num] = study.maximum_age_num
+      @calculations[study.nct_id][:maximum_age_unit] = study.maximum_age_unit
     end
   end
 
 
-  def self.process_dates(nct_ids, calculations)
-    results = Study.study_dates_for_calculations(nct_ids)
+  def self.process_dates
+    results = Study.study_dates_for_calculations(@nct_ids)
     results.each do |study|
-      calculations[study.nct_id][:months_to_report_results] = calculate_months_to_report_results(study)
-      calculations[study.nct_id][:actual_duration] = calculate_actual_duration(study)
-      calculations[study.nct_id][:registered_in_calendar_year] = calculate_registered_year(study)
+      @calculations[study.nct_id][:months_to_report_results] = calculate_months_to_report_results(study)
+      @calculations[study.nct_id][:actual_duration] = calculate_actual_duration(study)
+      @calculations[study.nct_id][:registered_in_calendar_year] = calculate_registered_year(study)
     end
   end
   
 
-  def self.process_outcome_design_counts(nct_ids, calculations)
-    results = DesignOutcome.count_outcomes_by_type_for(nct_ids)
+  def self.process_outcome_design_counts
+    results = DesignOutcome.count_outcomes_by_type_for(@nct_ids)
     results.each do |nct_id, counts|
-      calculations[nct_id][:number_of_primary_outcomes_to_measure] = counts[:primary]
-      calculations[nct_id][:number_of_secondary_outcomes_to_measure] = counts[:secondary]
-      calculations[nct_id][:number_of_other_outcomes_to_measure] = counts[:other]
+      @calculations[nct_id][:number_of_primary_outcomes_to_measure] = counts[:primary]
+      @calculations[nct_id][:number_of_secondary_outcomes_to_measure] = counts[:secondary]
+      @calculations[nct_id][:number_of_other_outcomes_to_measure] = counts[:other]
     end
   end
 
 
-  def self.process_event_subject_counts(nct_ids, calculations)
-    results = ReportedEvent.sum_subjects_by_event_type_for(nct_ids)
+  def self.process_event_subject_counts
+    results = ReportedEvent.sum_subjects_by_event_type_for(@nct_ids)
     results.each do |nct_id, counts|
-      calculations[nct_id][:number_of_sae_subjects] = counts[:serious]
-      calculations[nct_id][:number_of_nsae_subjects] = counts[:other]
+      @calculations[nct_id][:number_of_sae_subjects] = counts[:serious]
+      @calculations[nct_id][:number_of_nsae_subjects] = counts[:other]
     end
   end
 
