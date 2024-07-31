bc84827240deccb613bea119cac7b384e858fbbb
akabishau <aleksey.kabishau@icloud.com>
2024-07-31 18:03:54 -0400

    improve insert of result group ids in outcome measurement table

--- app/models/outcome_measurement.rb
+++ app/models/outcome_measurement.rb
@@ -3,12 +3,20 @@ class OutcomeMeasurement < StudyRelationship
   belongs_to :result_group
 
   def self.update_result_group_ids(nct_ids)
-    puts "updating result group ids for outcome measurements"
-    where(nct_id: nct_ids).find_each do |outcome_measurement|
-      result_group = ResultGroup.find_by(nct_id: outcome_measurement.nct_id, ctgov_group_code: outcome_measurement.ctgov_group_code, outcome_id: outcome_measurement.outcome_id)
-      if result_group
-        outcome_measurement.update(result_group_id: result_group.id)
-      end
+    Rails.logger.info "updating result group ids for outcome measurements"
+
+
+    updates = []
+    measurements = where(nct_id: nct_ids)
+    groups = ResultGroup.where(nct_id: nct_ids, result_type: "Outcome").index_by do |group|
+      [group.nct_id, group.ctgov_group_code, group.outcome_id]
     end
+
+    measurements.each do |measurement|
+      group = groups[[measurement.nct_id, measurement.ctgov_group_code, measurement.outcome_id]]
+      updates << { id: measurement.id, result_group_id: group.id } if group
+    end
+
+    import updates, on_duplicate_key_update: { conflict_target: [:id], columns: [:result_group_id] }  
   end
 end
