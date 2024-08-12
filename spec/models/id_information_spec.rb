59aee3e4c6a47e1cf29aede59d47ac840ff53717
akabishau <aleksey.kabishau@icloud.com>
2024-08-12 10:16:37 -0400

    refactor spec

--- spec/models/id_information_spec.rb
+++ spec/models/id_information_spec.rb
@@ -2,15 +2,15 @@ require "rails_helper"
   
 RSpec.describe IdInformation, type: :model do
   describe "Mapping" do
-    let(:nct_id) { "NCT000001" }
+    nct_id = "NCT000001"
 
     let(:content) { load_json(described_class) }
     let(:expected_data) do
       [
-        { "nct_id" => "NCT000001", "id_source" => "nct_alias", "id_value" => "NCT000002", "id_type" => nil, "id_type_description" => nil, "id_link" => nil },
-        { "nct_id" => "NCT000001", "id_source" => "org_study_id", "id_value" => "NRG-GY032", "id_type" => nil, "id_type_description" => nil, "id_link" => nil },
-        { "nct_id" => "NCT000001", "id_source" => "secondary_id", "id_value" => "NCI-2023-09355", "id_type" => "REGISTRY", "id_type_description" => "CTRP", "id_link" => nil },
-        { "nct_id" => "NCT000001", "id_source" => "secondary_id", "id_value" => "U10CA180868", "id_type" => "NIH", "id_type_description" => nil, "id_link" => "https://reporter.nih.gov/quickSearch/U10CA180868" },
+        { "nct_id" => nct_id, "id_source" => "nct_alias", "id_value" => "NCT000002", "id_type" => nil, "id_type_description" => nil, "id_link" => nil },
+        { "nct_id" => nct_id, "id_source" => "org_study_id", "id_value" => "NRG-GY032", "id_type" => nil, "id_type_description" => nil, "id_link" => nil },
+        { "nct_id" => nct_id, "id_source" => "secondary_id", "id_value" => "NCI-2023-09355", "id_type" => "REGISTRY", "id_type_description" => "CTRP", "id_link" => nil },
+        { "nct_id" => nct_id, "id_source" => "secondary_id", "id_value" => "U10CA180868", "id_type" => "NIH", "id_type_description" => nil, "id_link" => "https://reporter.nih.gov/quickSearch/U10CA180868" },
       ]
     end
 
@@ -20,10 +20,9 @@ RSpec.describe IdInformation, type: :model do
     end
 
 
-    it "creates an instance of IdInformation", schema: :v2 do
-      imported = IdInformation.all.order(id_source: :asc).map{|x| x.attributes }
-      imported.each{|x| x.delete("id")}
-      expect(imported).to eq(expected_data)
+    it "creates an instance of IdInformation" do
+      imported = IdInformation.where(nct_id: nct_id).map{|x| x.attributes.except("id") }
+      expect(imported).to match_array(expected_data)
     end
   end
 end
