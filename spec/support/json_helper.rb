e9a3e0a4674f1d544e9bdb7cd807577df89896ce
akabishau <aleksey.kabishau@icloud.com>
2024-08-14 09:50:28 -0400

    update stubs file structure and json loader logic

--- spec/support/json_helper.rb
+++ spec/support/json_helper.rb
@@ -1,13 +1,11 @@
 module JsonHelper
   
   def load_json_for(model, folder: "import")
-    file_name = "#{model.name.underscore}.json"
-    file_path = Rails.root.join("spec", "fixtures", "json", "models", folder, file_name)
+    file_path = Rails.root.join("spec", "fixtures", "models", folder, filename(model))
     raise "File not found: #{file_path}" unless File.exist?(file_path)
     parse_json_from(file_path)
   end
 
-  # to support json structure: { "Model 1": [], "Model 2": []} - simplify if not needed
   def load_expected_data_for(model)
     json = load_json_for(model, folder: "output")
     # json[model.name]
@@ -17,12 +15,7 @@ module JsonHelper
   private
 
   def filename(subject)
-    subject.is_a?(Class) ? subject.name.underscore : subject
-  end
-
-  # TODO: Better name?
-  def base_folder(subject)
-    subject.is_a?(Class) ? "models" : "studies"
+    subject.is_a?(Class) ? "#{subject.name.underscore}.json" : subject
   end
 
   def parse_json_from(file_path)
