4314c309fed5704f7791479e9d927b5d36c8cb34
akabishau <aleksey.kabishau@icloud.com>
2024-08-14 15:28:44 -0400

    refactor helpers

--- spec/support/json_helper.rb
+++ spec/support/json_helper.rb
@@ -1,29 +1,44 @@
 module JsonHelper
   
-  def load_json_for(model, folder: "import")
-    file_path = Rails.root.join("spec", "fixtures", "models", folder, filename(model))
-    raise "File not found: #{file_path}" unless File.exist?(file_path)
+  def load_json(filename, path)
+    file = resolve_file_name(filename)
+    file_path = path.join(file)
     parse_json_from(file_path)
   end
 
-  def load_expected_data_for(model)
-    json = load_json_for(model, folder: "output")
-    # json[model.name]
-  end
-
-
   private
 
-  def filename(subject)
-    subject.is_a?(Class) ? "#{subject.name.underscore}.json" : subject
-  end
-
   def parse_json_from(file_path)
+    raise "File not found: #{file_path}" unless File.exist?(file_path)
     content = File.read(file_path)
     JSON.parse(content)
     rescue Errno::ENOENT => e
       raise "Error reading file: #{file_path} - #{e.message}"
     rescue JSON::ParserError => e
       raise "JSON parsing error in file: #{file_path} - #{e.message}"
+  end
+
+  def resolve_file_name(subject)
+    subject.is_a?(Class) ? "#{subject.name.underscore}.json" : subject
+  end
+
+  def spec_root
+    @spec_root ||= Rails.root.join("spec")
+  end
+
+  def models
+    @models ||= spec_root.join("fixtures", "models")
+  end
+
+  def models_import_path
+    @models_import_path ||= models.join("import")
+  end
+
+  def models_expected_path
+    @models_expected_path ||= models.join("expected")
+  end
+
+  def models_support_path
+    @support ||= spec_root.join("support", "json_data")
   end
 end
--- spec/support/test_data_helper.rb
+++ spec/support/test_data_helper.rb
@@ -2,34 +2,27 @@ module TestDataHelper
   include JsonHelper
 
 
-  def setup_json_sample(model, nct_id = "NCT001")
-    content = load_json_for(model)
+  def setup_json_sample(model, path = models_import_path, nct_id = "NCT001")
+    content = load_json(model, path)
     record = StudyJsonRecord.new(nct_id: nct_id, version: "2", content: content)
     StudyJsonRecord::Worker.new.process(1, [record])
   end
 
 
   def imported_data_for(model, nct_id = "NCT001")
-    # byebug
     model.where(nct_id: nct_id).map{ |r| r.attributes.except("id") }
   end
 
-  def expected_data_for(model)
-    json = load_expected_data_for(model)
+  def expected_data_for(model, path = models_expected_path)
+    json = load_json(model, path)
     json[model.name]
   end
 
 
-  # TODO: use subject instead of model
-  # define custom matcher instead of excluding keys in before extraction and comparison
   def compare_imported_with_expected_for(model)
     setup_json_sample(model)
     imported = imported_data_for(model)
     expected = expected_data_for(model)
-
-    puts "expected", expected
-    puts "imported", imported
-
     expect(imported).to match_array(expected)
   end
 end
