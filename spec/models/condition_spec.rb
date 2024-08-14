a58e2f831fd6889cd2ddb581392de4eefa299f02
akabishau <aleksey.kabishau@icloud.com>
2024-08-14 15:30:43 -0400

    refactor legacy spec to use new functionality

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -4,34 +4,24 @@ describe Condition do
   it "should create an instance of Condition", schema: :v2 do
     expected_data = [
       {
-        "nct_id" => "NCT000001",
+        "nct_id" => "NCT001",
         "name" => "Axial Spondyloarthritis",
         "downcase_name" => "axial spondyloarthritis"
       },
       {
-        "nct_id" => "NCT000001",
+        "nct_id" => "NCT001",
         "name" => "Nonradiographic Axial Spondyloarthritis",
         "downcase_name" => "nonradiographic axial spondyloarthritis"
       },
       {
-        "nct_id" => "NCT000001",
+        "nct_id" => "NCT001",
         "name" => "Nr-axSpA",
         "downcase_name" => "nr-axspa"
       }
     ]  
 
-    # load the json
-    content = JSON.parse(File.read('spec/support/json_data/condition.json'))
-    StudyJsonRecord.create(nct_id: "NCT000001", version: '2', content: content) # create a brand new json record
-
-    # process the json
-    StudyJsonRecord::Worker.new.process # import the new json record
-
-    # load the database entries
-    imported = Condition.all.map{|x| x.attributes }
-    imported.each{|x| x.delete("id")}
-
-    # Compare the modified imported data with the expected data
+    setup_json_sample(described_class, models_support_path)
+    imported = imported_data_for(described_class)
     expect(imported).to eq(expected_data)
   end
 end
