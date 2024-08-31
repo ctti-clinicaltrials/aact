b9ce66d481a62b28f6382903d82a24a6a705b209
akabishau <aleksey.kabishau@icloud.com>
2024-08-30 23:45:02 -0400

    add fetch option by date + refactoring

--- app/services/api/ct_gov_client_v2.rb
+++ app/services/api/ct_gov_client_v2.rb
@@ -1,21 +1,20 @@
-# TODO: Would it be better to use DI instead of inheritance here?
 module API
   class CTGovClientV2 < HttpService
-
     BASE_URL = "https://clinicaltrials.gov/api/v2/".freeze
 
     def initialize
-    super(BASE_URL)
+      super(BASE_URL)
     end
 
     def fetch_study(nct_id)
       get("studies/#{nct_id}")
     end
 
-    def fetch_studies(fields: nil, nct_ids: nil, query_term: nil, page_size: 4, limit: nil)
+    def fetch_studies(fields: nil, nct_ids: nil, query_term: nil, page_size: nil, limit: nil)
       items = []
       page_token = nil
 
+      # TODO: optimize this later
       params = { pageSize: page_size }
       params[:fields] = fields
       params["filter.ids"] = nct_ids.join(",") if nct_ids
@@ -26,6 +25,12 @@ module API
         Rails.logger.debug("Params: #{params}")
 
         result = get("studies", params.compact)
+        if result.nil?
+          # TODO: handle this better
+          Rails.logger.warn("Received nil response from API")
+          break
+        end
+
         studies = result["studies"] if result
         break if studies.nil? or studies.empty?
 
@@ -36,6 +41,12 @@ module API
         break if page_token.nil?
       end
       items
+    end
+
+    # start_date is required keyword argument
+    def fetch_studies_by_date(start_date:, end_date: nil, page_size: 100, limit: nil)
+      query_term = "AREA[LastUpdatePostDate]RANGE[#{start_date},#{end_date || 'MAX'}]"
+      fetch_studies(query_term: query_term, page_size: page_size, limit: limit)
     end
   end
 end
