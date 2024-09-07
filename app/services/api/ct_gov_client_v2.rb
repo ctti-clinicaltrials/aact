2af74163dabd64ee24e78518a78db1440f36612b
akabishau <aleksey.kabishau@icloud.com>
2024-09-06 22:30:05 -0400

    refactoring

--- app/services/api/ct_gov_client_v2.rb
+++ app/services/api/ct_gov_client_v2.rb
@@ -10,13 +10,13 @@ module API
       get("studies/#{nct_id}")
     end
 
-    def fetch_studies_paginated(query_term: nil, page_size: nil)
+    # TODO: error handling
+    def fetch_studies(query_term: nil, page_size: nil)
       page_token = nil
       total_count = 0
       total_fetched = 0
 
       params = { pageSize: page_size, countTotal: true }
-      # params[:fields] = fields
       params["query.term"] = query_term
 
       loop do
@@ -45,60 +45,21 @@ module API
       end
     end
 
-    def studies(page_token: nil, query_term: nil, page_size: nil, limit: nil)
-      params = { pageSize: page_size }
-      params[:pageToken] = page_token if page_token
-      params["query.term"] = query_term if query_term
-      # params["query.term"] = "AREA[LastUpdatePostDate]RANGE[2024-08-30,MAX]"
-      params[:countTotal] = true if !page_token # total shown in 1 page response only
-      Rails.logger.debug("Params: #{params}")
-      get("studies", params.compact)
-    end
 
-    def fetch_studies(fields: nil, nct_ids: nil, query_term: nil, page_size: nil, limit: nil)
-      items = []
-      page_token = nil
-
-      # TODO: optimize this later
-      params = { pageSize: page_size, countTotal: true } # total shown in 1 page response only
-      params[:fields] = fields
-      params["filter.ids"] = nct_ids.join(",") if nct_ids
-      params["query.term"] = query_term
-        
-      loop do
-        params[:pageToken] = page_token
-        Rails.logger.debug("Params: #{params}")
-
-        result = get("studies", params.compact)
-        if result.nil?
-          # TODO: handle this better
-          Rails.logger.warn("Received nil response from API")
-          break
-        end
-        Rails.logger.info("Total Count: #{result["totalCount"]}") if result["totalCount"]
-
-        studies = result["studies"] if result
-        break if studies.nil? or studies.empty?
-
-        items.concat(studies)
-        break if items.size >= limit if limit
-
-        page_token = result["nextPageToken"]
-        break if page_token.nil?
+    # start_date is required keyword argument
+    def get_studies_in_date_range(start_date:, end_date: nil, page_size: nil)
+      Rails.logger.info("Fetching studies for range: #{start_date} - #{end_date || Date.today}")
+      query_term = build_date_range_query(start_date: start_date, end_date: end_date)
+      fetch_studies(query_term: query_term, page_size: page_size) do |studies|
+        yield studies
       end
-      items
     end
 
-    def query_term_for(start_date:, end_date: nil)
+    private
+
+    def build_date_range_query(start_date:, end_date: nil)
       "AREA[LastUpdatePostDate]RANGE[#{start_date},#{end_date || 'MAX'}]"
     end
 
-    # start_date is required keyword argument
-    def fetch_studies_by_date(start_date:, end_date: nil, page_size: 5, limit: 10)
-      Rails.logger.info("Fetching studies for range: #{start_date} - #{end_date || Date.today}")
-      query_term = "AREA[LastUpdatePostDate]RANGE[#{start_date},#{end_date || 'MAX'}]"
-      fetch_studies(query_term: query_term, page_size: page_size, limit: limit)
-      # studies(query_term: query_term, page_size: page_size, limit: limit)
-    end
   end
 end
--- app/services/study_sync_service.rb
+++ app/services/study_sync_service.rb
@@ -9,67 +9,18 @@ class StudySyncService
   end
 
   # get a better name - for now mimic study_downloader
-  def download_recently_updated
-    start_date = fetch_start_date
-    studies = @api_client.fetch_studies_by_date(start_date: start_date)
-    studies.each do |study_json|
-      nct_id = study_json["protocolSection"]["identificationModule"]["nctId"]
-      # byebug if nct_id == "NCT06211660"
-      json_record = StudyJsonRecord.find_or_create_by(nct_id: nct_id, version: '2') { |r| r.content = {} }
-      json_record.update(content: study_json, download_date: Date.today)
-      # if content is the same, rails doesn't set new updated_at value
-    end
-
-  end
-
-  def recent_studies
+  def sync_recent_studies
     silence_active_record { StudyJsonRecord.destroy_all } # temporary fix
-    # start_date = fetch_start_date
-    start_date = "2024-09-01"
-
-    query_term = @api_client.query_term_for(start_date: start_date)
-    @api_client.fetch_studies_paginated(query_term: query_term, page_size: 500) do |studies|
-      import_studies(studies)
+    start_date = get_sync_start_date
+    # start_date = "2024-09-04"
+    @api_client.get_studies_in_date_range(start_date: start_date, page_size: 500) do |studies|
+      persist(studies)
     end
-  end
-
-  def recently_updated
-    silence_active_record do
-      StudyJsonRecord.destroy_all # temporary fix
-       Rails.logger.info("Deleted all StudyJsonRecords")
-    end
-    
-    # TODO: capture total count and use the somewhere (load event?
-    # TODO: show progress
-    # TODO - handle errors -> propage up to the caller?
-    total_count = nil
-    page_token = nil
-    start_date = fetch_start_date
-    # start_date = "2024-07-30"
-
-    loop do
-      query_term = @api_client.query_term_for(start_date: start_date)
-      # studies = @api_client.fetch_studies_by_date(start_date: start_date)
-      response = @api_client.studies(page_token: page_token, query_term: query_term, page_size: 500)
-      
-      studies = response["studies"]
-      total_count = response["totalCount"] if response["totalCount"]
-      page_token = response["nextPageToken"]
-      count = studies.size
-
-      Rails.logger.info("Total Count: #{total_count}") if response["totalCount"]
-      Rails.logger.info("Fetched #{count} studies")
-
-      
-      import_studies(response["studies"])
-      break if page_token.nil? 
-    end
-    Rails.logger.info("Importing of #{total_count} StudyJsonRecords is completed")
   end
 
   private
 
-  def import_studies(studies)
+  def persist(studies)
     silence_active_record do
       study_records = studies.map do |study_json|
         nct_id = study_json["protocolSection"]["identificationModule"]["nctId"]
@@ -94,7 +45,9 @@ class StudySyncService
     #   })
   end
 
-  def fetch_start_date
+  private
+
+  def get_sync_start_date
     last_event = Support::LoadEvent
                   .where(event_type: 'incremental', status: 'complete')
                   .order(completed_at: :desc)
@@ -102,7 +55,7 @@ class StudySyncService
 
     if last_event && last_event.completed_at
       # TODO: figure out the timezone, so we don't have to reprocess a day ago
-      last_event.completed_at.to_date.days_ago(1)
+      last_event.completed_at.to_date - 1
     else
       Rails.logger.warn("No complete incremental events found, using default start date.")
       Date.today - 5 # or any default date you want to use
